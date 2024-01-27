6dd2c47eeb3ded8285918a5169ab7b00d56f77b9
ST <workinvs@gmail.com>
2024-01-27 14:42:20 +0200

    feat: add the DropdownMenu

--- src/components/atoms/buttons/DropdownButton.tsx
+++ src/components/atoms/buttons/DropdownButton.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useRef } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 
 import { IButton } from '@components/atoms/buttons';
-import { Button, Icon } from '@components/atoms';
+import { Button, Icon, DropdownMenu } from '@components/atoms';
 
 interface IDropdownButton extends Partial<IButton> {
   iconLeft?: string | React.FC<React.SVGProps<SVGSVGElement>>;
@@ -16,40 +15,14 @@ export const DropdownButton = ({ iconLeft, name, children, disabled, ...rest }: 
   const handleMouseEnter = () => setIsOpen(true);
   const handleMouseLeave = () => setIsOpen(false);
 
-  const dropdownVariants = {
-    opened: {
-      opacity: 1,
-      scale: 1,
-      transition: { duration: 0.3 },
-    },
-    closed: {
-      opacity: 0,
-      scale: 0.95,
-      transition: { duration: 0.3 },
-    },
-  };
-
   return (
     <div className="relative" ref={dropdownRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="absolute w-full left-0 h-2 -bottom-2"></div>
-
       <Button {...rest} disabled={disabled}>
         {iconLeft && <Icon disabled={disabled} src={iconLeft} />}
         {name}
       </Button>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            initial="closed"
-            animate="opened"
-            exit="closed"
-            variants={dropdownVariants}
-            className="absolute left-1/2 !transform -translate-x-1/2 mt-2 p-2 bg-gray-600 text-white rounded-md shadow-xl z-50"
-          >
-            {children}
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <DropdownMenu isOpen={isOpen}>{children}</DropdownMenu>
     </div>
   );
 };
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,5 @@
 export { Icon } from "@components/atoms/Icon";
 export { Badge } from "@components/atoms/Badge";
 export { Link } from "@components/atoms/Link";
+export { DropdownMenu } from "@components/atoms/DropdownMenu";
 export { Button, IconButton, DropdownButton } from "@components/atoms/buttons";
