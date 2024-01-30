963c4684722ba6e0cf888d74d8e1ff078d2c5b13
Ronen Mars <ronen@autokitteh.com>
2024-01-30 12:20:42 +0200

    chore: remove test log

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { ProjectsService } from "./services";
 import { router } from "@routing/routes";
 import { RouterProvider } from "react-router-dom";
 
 export const App = () => {
-	console.log(ProjectsService.list());
 	return <RouterProvider router={router} />;
 };
