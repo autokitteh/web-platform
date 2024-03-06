3047758f4b4b6df421a4cf934ca5a5b98d6da43c
ST <workinvs@gmail.com>
2024-03-06 14:06:36 +0200

    refactor: change display project list in submenu, move modal

--- src/components/molecules/menu/menu.tsx
+++ src/components/molecules/menu/menu.tsx
@@ -1,25 +1,89 @@
-import React from "react";
-import { Button, Icon } from "@components/atoms";
+import React, { useEffect, useState } from "react";
+import { NewProject } from "@assets/image";
+import { Button, Icon, Toast, Input } from "@components/atoms";
+import { Modal } from "@components/molecules";
+import { menuItems } from "@constants";
 import { IMenu, ISubmenuInfo } from "@interfaces/components";
+import { IMenuItem } from "@interfaces/components";
+import { ProjectsService } from "@services";
 import { useUiGlobalStore } from "@store";
 import { cn } from "@utilities";
+import { isEqual } from "lodash";
 
 export const Menu = ({ className, isOpen = false, onSubmenu }: IMenu) => {
-	const { menuItems } = useUiGlobalStore();
+	const { lastMenuUpdate } = useUiGlobalStore();
+	const [menu, setMenu] = useState<IMenuItem[]>(menuItems);
+	const [toast, setToast] = useState({
+		isOpen: false,
+		message: "",
+	});
+	const [isModal, setIsModal] = useState(false);
+
+	useEffect(() => {
+		const fetchMenu = async () => {
+			const { data, error } = await ProjectsService.list();
+			if (error) return setToast({ ...toast, message: (error as Error).message || "Something wrong!" });
+
+			const updatedSubmenu = data?.map(({ projectId, name }) => ({
+				id: projectId,
+				name,
+			}));
+			const currentSubmenu = menu.find(({ id }) => id === 1)?.submenu;
+
+			if (isEqual(currentSubmenu, updatedSubmenu)) return;
+			setMenu(menuItems.map((item) => (item.id === 1 ? { ...item, submenu: updatedSubmenu } : item)));
+		};
+
+		fetchMenu();
+
+		const intervalMenu = setInterval(fetchMenu, 60000);
+		return () => clearInterval(intervalMenu);
+	}, [lastMenuUpdate]);
+
 	const handleMouseEnter = (submenu: ISubmenuInfo["submenu"], e: React.MouseEvent) => {
 		onSubmenu?.({ submenu: submenu || undefined, top: e.currentTarget.getBoundingClientRect().top + 5 });
 	};
+	const handleCloseToast = () => setToast({ ...toast, isOpen: false });
+	const toggleModal = () => setIsModal(!isModal);
 
 	return (
-		<div className={cn(className, "flex flex-col gap-4")}>
-			{menuItems.map(({ icon, name, href, submenu, id }) => (
-				<div key={id} onMouseEnter={(e) => handleMouseEnter(submenu, e)}>
-					<Button className="hover:bg-green-light" href={href}>
-						{icon ? <Icon alt={name} className="w-8 h-8 p-1 " src={icon} /> : null}
-						{isOpen ? name : null}
+		<>
+			<div className={cn(className, "flex flex-col gap-4")}>
+				<div onMouseEnter={(e) => handleMouseEnter(undefined, e)}>
+					<Button className="hover:bg-green-light" onClick={toggleModal}>
+						<Icon alt="New Project" className="w-8 h-8 p-1 " src={NewProject} />
+						{isOpen ? "New Project" : null}
 					</Button>
 				</div>
-			))}
-		</div>
+				{menu.map(({ icon, name, href, submenu, id }) => (
+					<div key={id} onMouseEnter={(e) => handleMouseEnter(submenu, e)}>
+						<Button className="hover:bg-green-light" href={href}>
+							{icon ? <Icon alt={name} className="w-8 h-8 p-1 " src={icon} /> : null}
+							{isOpen ? name : null}
+						</Button>
+					</div>
+				))}
+			</div>
+			<Modal isOpen={isModal} onClose={toggleModal}>
+				<div className="mx-6">
+					<h3 className="text-xl font-bold mb-5">Add New</h3>
+					<form>
+						<Input
+							classInput="placeholder:text-gray-400 hover:placeholder:text-gray-800"
+							className="bg-white border-gray-400 hover:border-gray-700"
+							isRequired
+							placeholder="Name"
+						/>
+						<Button className="font-bold justify-center mt-2 rounded-lg py-2.5" variant="filled">
+							Create
+						</Button>
+					</form>
+				</div>
+			</Modal>
+			<Toast className="border-error" duration={10} isOpen={toast.isOpen} onClose={handleCloseToast}>
+				<h5 className="font-semibold">Error</h5>
+				<p className="mt-1 text-xs">{toast.message}</p>
+			</Toast>
+		</>
 	);
 };
--- src/components/organisms/tabsContent/addCodeAssetsTab.tsx
+++ src/components/organisms/tabsContent/addCodeAssetsTab.tsx
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { PlusCircle } from "@assets/image";
-import { Button, Input } from "@components/atoms";
-import { Modal } from "@components/molecules";
+import { Button } from "@components/atoms";
 
 export const AddCodeAssetsTab = () => {
-	const [isModal, setIsModal] = useState(false);
-	const toggleModal = () => setIsModal(!isModal);
-
 	return (
 		<div className="h-full flex justify-center items-center">
-			<Button className=" group flex flex-col items-center gap-2.5 cursor-pointer" onClick={toggleModal}>
+			<Button className=" group flex flex-col items-center gap-2.5 cursor-pointer">
 				<PlusCircle className="transition stroke-gray-400 group-hover:stroke-green-accent" />
 				<p className="text-center text-lg font-bold uppercase text-white">Add Code & Assets</p>
 			</Button>
-			<Modal isOpen={isModal} onClose={toggleModal}>
-				<div className="mx-6">
-					<h3 className="text-xl font-bold mb-5">Add New</h3>
-					<form>
-						<Input
-							classInput="placeholder:text-gray-400 hover:placeholder:text-gray-800"
-							className="bg-white border-gray-400 hover:border-gray-700"
-							isRequired
-							placeholder="Name"
-						/>
-						<Button className="font-bold justify-center mt-2 rounded-lg py-2.5" variant="filled">
-							Create
-						</Button>
-					</form>
-				</div>
-			</Modal>
 		</div>
 	);
 };
