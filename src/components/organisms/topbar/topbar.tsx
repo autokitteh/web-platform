708d7831d0af7a219cf683ca2c451a6d082e50e5
ST <workinvs@gmail.com>
2024-03-12 14:19:33 +0200

    feat: validation editable name

--- src/components/organisms/topbar/topbar.tsx
+++ src/components/organisms/topbar/topbar.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FullScreen, More } from "@assets/image";
-import { Button, IconButton, IconSvg } from "@components/atoms";
+import { Button, ErrorMessage, IconButton, IconSvg } from "@components/atoms";
 import { DropdownButton } from "@components/molecules";
 import { topbarItems } from "@constants";
 import { ProjectsService } from "@services";
@@ -15,7 +15,13 @@ export const Topbar = () => {
 		name: "Slack Monitor",
 		projectId: "Version 454462",
 	});
+	const [isNameValid, setIsNameValid] = useState<boolean>(true);
+	const spanRef = useRef<HTMLSpanElement>(null);
+
 	const styleIconSreen = cn({ "border-transparent bg-black": isFullScreen });
+	const styleInput = cn("font-semibold p-0 text-2xl leading-6 bg-transparent min-w-3 outline outline-0 rounded", {
+		"outline-error outline-2": !isNameValid,
+	});
 
 	useEffect(() => {
 		if (!newProjectId) return;
@@ -27,29 +33,39 @@ export const Topbar = () => {
 	}, [newProjectId]);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLSpanElement> | React.KeyboardEvent<HTMLSpanElement>) => {
-		const name = (e.target as HTMLSpanElement).textContent || "";
-
+		const nameLength = (e.target as HTMLSpanElement).textContent?.trim().length || 0;
 		if ((e as React.KeyboardEvent<HTMLSpanElement>).key === "Enter") {
 			(e.target as HTMLSpanElement).blur();
 		}
-		if (e.type === "blur") console.log(name);
+
+		if (e.type === "blur" && nameLength > 0) {
+			setIsNameValid(true);
+		}
+	};
+
+	const handleInput = (e: React.ChangeEvent<HTMLSpanElement>) => {
+		const nameLength = e.target.textContent?.trim().length || 0;
+		setIsNameValid(nameLength > 0);
 	};
 
 	return (
 		<div className="flex justify-between items-center bg-gray-700 gap-5 pl-7 pr-3.5 py-3 rounded-b-xl">
-			<div className="flex items-end gap-3">
+			<div className="flex items-end gap-3 relative">
 				<span
-					className="font-semibold p-0 text-2xl leading-6 bg-transparent min-w-3"
-					contentEditable
+					className={styleInput}
+					contentEditable={true}
 					onBlur={handleInputChange}
+					onInput={handleInput}
 					onKeyDown={handleInputChange}
+					ref={spanRef}
 					role="textbox"
+					suppressContentEditableWarning={true}
 					tabIndex={0}
 					title="Rename"
 				>
 					{project.name}
 				</span>
-
+				<ErrorMessage className="-bottom-5 text-xs">{!isNameValid ? "Name is required" : null}</ErrorMessage>
 				<span className="font-semibold text-gray-300 leading-none">{project.projectId}</span>
 			</div>
 			<div className="flex items-stretch gap-3">
