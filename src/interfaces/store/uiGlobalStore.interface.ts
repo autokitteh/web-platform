8eac4fc31f04d6530b97e571e0638068996c4687
ST <workinvs@gmail.com>
2024-03-13 10:42:14 +0200

    feat: display projects in submenu sidebar and some fix

--- src/interfaces/store/uiGlobalStore.interface.ts
+++ src/interfaces/store/uiGlobalStore.interface.ts
@@ -1,4 +1,9 @@
+import { IMenuItem } from "@interfaces/components";
+
 export interface IUIGlobalStore {
+	lastMenuUpdate: number | undefined;
+	menuItems: IMenuItem[];
 	isFullScreen: boolean;
 	toggleFullScreen: () => void;
+	refreshMenu: (forceUpdate?: boolean) => Promise<void>;
 }
