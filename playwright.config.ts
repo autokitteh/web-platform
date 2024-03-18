f9caa31f2840e51d509ec989bcd92132f1a8b813
Ronen Mars <ronen@autokitteh.com>
2024-03-18 12:35:29 +0200

    test: fix playwright config to run in the github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:5174",
+		baseURL: "http://localhost:4173",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -71,7 +71,7 @@ export default defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: "npm run build && npm run preview",
-		port: 5174,
+		port: 4173,
 		timeout: 120000,
 		reuseExistingServer: !process.env.CI,
 	},
