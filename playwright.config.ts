267cf11cf494933ff6f25159e07f6eafd886ca3f
Ronen Mars <ronen@autokitteh.com>
2024-03-27 18:00:21 +0200

    test: e2e tests by playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,10 +32,10 @@ export default defineConfig({
 
 	/* Configure projects for major browsers */
 	projects: [
-		{
-			name: "chromium",
-			use: { ...devices["Desktop Chrome"] },
-		},
+		// {
+		// 	name: "chromium",
+		// 	use: { ...devices["Desktop Chrome"] },
+		// },
 
 		{
 			name: "firefox",
@@ -46,14 +46,14 @@ export default defineConfig({
 			name: "webkit",
 			use: { ...devices["Desktop Safari"] },
 		},
-		{
-			name: "Mobile Chrome",
-			use: { ...devices["Pixel 5"] },
-		},
-		{
-			name: "Mobile Safari",
-			use: { ...devices["iPhone 12"] },
-		},
+		// {
+		// 	name: "Mobile Chrome",
+		// 	use: { ...devices["Pixel 5"] },
+		// },
+		// {
+		// 	name: "Mobile Safari",
+		// 	use: { ...devices["iPhone 12"] },
+		// },
 		{
 			name: "Microsoft Edge",
 			use: { ...devices["Desktop Edge"], channel: "msedge" },
