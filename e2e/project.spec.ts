57a810e243354be277712c9c11513690a7ed4a0c
Ronen Mars <ronen@autokitteh.com>
2024-03-27 17:41:44 +0200

    test: e2e tests by playwright

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -1,40 +1,18 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Project Suite", () => {
-	test("Create project", async ({ page }) => {
-		await page.goto("");
-
-		const button = page.getByRole("button", { name: "New Project" });
-		await button.hover();
-		await button.click();
-		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
-
-		const projectURL = page.url();
-		const projectId = projectURL.split("/").pop();
-
-		if (!projectId) {
-			throw new Error("Project ID not found after creation.");
-		}
-
-		await expect(page.getByText(projectId)).toBeVisible();
-	});
-
 	test("Change project name", async ({ page }) => {
 		await page.goto("");
 
 		const button = page.getByRole("button", { name: "New Project" });
-		await button.hover();
 		await button.click();
 		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
-
 		const projectURL = page.url();
 		const projectId = projectURL.split("/").pop();
 
 		if (!projectId) {
 			throw new Error("Project ID not found after creation.");
 		}
-
-		await expect(page.getByText(projectId)).toBeVisible();
 
 		const renameTextbox = page.getByRole("textbox", { name: "Rename" });
 		await renameTextbox.click();
@@ -46,10 +24,8 @@ test.describe("Project Suite", () => {
 		await page.goto("");
 
 		const button = page.getByRole("button", { name: "New Project" });
-		await button.hover();
 		await button.click();
 		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
-
 		const projectURL = page.url();
 		const projectId = projectURL.split("/").pop();
 
@@ -58,7 +34,6 @@ test.describe("Project Suite", () => {
 		}
 
 		await expect(page.getByText(projectId)).toBeVisible();
-
 		const addNewCodeButton = page.getByRole("button", { name: "Add new code file" });
 		await addNewCodeButton.click();
 		await page.waitForTimeout(500);
--- playwright.config.ts
+++ playwright.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
 	webServer: {
 		command: "npm run build && npm run preview",
 		port: 4173,
-		timeout: 3600000,
+		timeout: 9000000,
 		reuseExistingServer: !process.env.CI,
 	},
 });
