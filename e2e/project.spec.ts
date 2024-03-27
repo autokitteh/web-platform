14c8115248eab3381237765ddd6a4acbf4f300d9
Ronen Mars <ronen@autokitteh.com>
2024-03-27 19:47:07 +0200

    Revert "test: e2e tests by playwright"
    This reverts commit 93fe354f136f940ce4e7140e64287dba8e5f7468.

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -1,34 +1,37 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Project Suite", () => {
-	// Move common setup actions to beforeEach
-	test.beforeEach(async ({ page }) => {
-		await page.goto(""); // Assuming this navigates to the root of your project management app
-		await page.getByRole("button", { name: "New Project" }).click(); // Create a new project
-
-		// Optional: Wait for a specific element that confirms project creation, if necessary.
-		// This could be waiting for a URL change or a specific element to be visible.
-	});
-
 	test("Change project name", async ({ page }) => {
+		await page.goto("");
+		await page.getByRole("button", { name: "New Project" }).click();
 		await page.getByRole("textbox", { name: "Rename" }).click();
 		await page.getByRole("textbox", { name: "Rename" }).fill("Grankie_0121");
-		await expect(page.getByText("Grankie_0121")).toBeVisible(); // Properly wait and assert visibility
+		expect(page.getByText("Grankie_0121")).toBeTruthy();
 	});
 
 	test("Add new file to project", async ({ page }) => {
-		// Extract project ID from URL, assuming project creation redirects to a new URL
-		const projectURL = page.url();
+		await page.goto("");
+		const button = page.getByRole("button", { name: "New Project" });
+		await button.hover();
+		if (await button.isVisible()) {
+			await button.click();
+		} else {
+			test.fail();
+		}
+		const projectURL = await page.url();
 		const projectId = projectURL.split("/").pop();
-		expect(projectId).toBeTruthy(); // Assert projectId is not null or empty
-
-		await expect(page.getByText(projectId!)).toBeVisible(); // Wait and check if projectId is visible
-
+		if (!projectId) {
+			test.fail();
+		}
+		await expect(page.getByText(projectId!)).toBeVisible();
 		const addNewCodeButton = page.getByRole("button", { name: "Add new code file" });
-		await addNewCodeButton.click(); // Implicitly waits for visibility
-		await page.waitForTimeout(500); // Consider replacing with more deterministic wait conditions
-
+		if (await addNewCodeButton.isVisible()) {
+			await addNewCodeButton.click();
+			await page.waitForTimeout(500);
+		} else {
+			test.fail();
+		}
 		const newFileInput = page.getByRole("textbox", { name: "new file name" });
-		await expect(newFileInput).toBeVisible(); // Proper assertion for visibility
+		await newFileInput.isVisible();
 	});
 });
