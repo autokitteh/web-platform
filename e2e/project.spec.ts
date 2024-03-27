4b2ca98b46cdfa882f134a0c30e85442a94998b5
Ronen Mars <ronen@autokitteh.com>
2024-03-27 17:38:19 +0200

    test: e2e tests by playwright

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Project Suite", () => {
-	test.beforeEach(async ({ page }) => {
+	test("Create project", async ({ page }) => {
 		await page.goto("");
 
 		const button = page.getByRole("button", { name: "New Project" });
@@ -20,6 +20,22 @@ test.describe("Project Suite", () => {
 	});
 
 	test("Change project name", async ({ page }) => {
+		await page.goto("");
+
+		const button = page.getByRole("button", { name: "New Project" });
+		await button.hover();
+		await button.click();
+		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
+
+		const projectURL = page.url();
+		const projectId = projectURL.split("/").pop();
+
+		if (!projectId) {
+			throw new Error("Project ID not found after creation.");
+		}
+
+		await expect(page.getByText(projectId)).toBeVisible();
+
 		const renameTextbox = page.getByRole("textbox", { name: "Rename" });
 		await renameTextbox.click();
 		await renameTextbox.fill("Grankie_0121");
@@ -27,6 +43,22 @@ test.describe("Project Suite", () => {
 	});
 
 	test("Add new file to project", async ({ page }) => {
+		await page.goto("");
+
+		const button = page.getByRole("button", { name: "New Project" });
+		await button.hover();
+		await button.click();
+		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
+
+		const projectURL = page.url();
+		const projectId = projectURL.split("/").pop();
+
+		if (!projectId) {
+			throw new Error("Project ID not found after creation.");
+		}
+
+		await expect(page.getByText(projectId)).toBeVisible();
+
 		const addNewCodeButton = page.getByRole("button", { name: "Add new code file" });
 		await addNewCodeButton.click();
 		await page.waitForTimeout(500);
