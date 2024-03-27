8ae702316f08be985c40161b409737d6690652a1
Ronen Mars <ronen@autokitteh.com>
2024-03-27 17:59:59 +0200

    test: e2e tests by playwright

---
+++ e2e/project.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Project Suite", () => {
+	test("Change project name", async ({ page }) => {
+		await page.goto("");
+		await page.getByRole("button", { name: "New Project" }).click();
+		await page.getByRole("textbox", { name: "Rename" }).click();
+		await page.getByRole("textbox", { name: "Rename" }).fill("Grankie_0121");
+		expect(page.getByText("Grankie_0121")).toBeTruthy();
+	});
+
+	test("Add new file to project", async ({ page }) => {
+		await page.goto("");
+		const button = page.getByRole("button", { name: "New Project" });
+		await button.hover();
+		if (await button.isVisible()) {
+			await button.click();
+		} else {
+			test.fail();
+		}
+		const projectURL = await page.url();
+		const projectId = projectURL.split("/").pop();
+		if (!projectId) {
+			test.fail();
+		}
+		await expect(page.getByText(projectId!)).toBeVisible();
+		const addNewCodeButton = page.getByRole("button", { name: "Add new code file" });
+		if (await addNewCodeButton.isVisible()) {
+			await addNewCodeButton.click();
+			await page.waitForTimeout(500);
+		} else {
+			test.fail();
+		}
+		const newFileInput = page.getByRole("textbox", { name: "new file name" });
+		await newFileInput.isVisible();
+	});
+});
