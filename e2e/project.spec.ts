453f622a29531067c46cefff98ec3aa23833764d
Ronen Mars <ronen@autokitteh.com>
2024-03-27 17:32:20 +0200

    test: e2e tests by playwright

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -7,7 +7,7 @@ test.describe("Project Suite", () => {
 		const button = page.getByRole("button", { name: "New Project" });
 		await button.hover();
 		await button.click();
-		await page.waitForURL(/http:\/\/localhost:4173\/(prj_[a-zA-Z0-9]+)/);
+		await page.waitForTimeout(500);
 
 		const projectURL = page.url();
 		const projectId = projectURL.split("/").pop();
