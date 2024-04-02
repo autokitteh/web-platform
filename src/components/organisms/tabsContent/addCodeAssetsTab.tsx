78f90523c222ebb33d26542add4a3f26328ee48c
ST <workinvs@gmail.com>
2024-04-02 18:06:14 +0300

    feat: add frame when dragging

--- src/components/organisms/tabsContent/addCodeAssetsTab.tsx
+++ src/components/organisms/tabsContent/addCodeAssetsTab.tsx
@@ -22,12 +22,23 @@ export const AddCodeAssetsTab = () => {
 		message: "",
 	});
 
+	const resourcesEntries = Object.entries(currentProject.resources);
+	const sortedResources = orderBy(resourcesEntries, ([name]) => name, "asc");
+
 	const styleCircle = cn("transition stroke-gray-400 group-hover:stroke-green-accent", {
 		"stroke-green-accent": isDragOver,
 	});
-
-	const resourcesEntries = Object.entries(currentProject.resources);
-	const sortedResources = orderBy(resourcesEntries, ([name]) => name, "asc");
+	const styleBase = cn("transition rounded-xl relative flex-1", {
+		"mt-auto mb-auto flex justify-center items-center": isEmpty(sortedResources),
+	});
+	const styleFrame = cn(
+		"absolute transition top-0 h-full w-full rounded-lg z-50 flex justify-center items-center",
+		"opacity-0 select-none pointer-events-none",
+		{
+			"bg-white/40 border-2 opacity-1": isDragOver,
+			"opacity-1 pointer-events-auto": isEmpty(sortedResources),
+		}
+	);
 
 	const handleDragOver = (event: React.DragEvent) => {
 		event.preventDefault();
@@ -70,55 +81,56 @@ export const AddCodeAssetsTab = () => {
 				<PlusCircle className="transtion duration-300 stroke-gray-300 group-hover:stroke-white w-5 h-5" />
 				Add new
 			</Button>
-			{!isEmpty(sortedResources) ? (
-				<Table className="mt-5 max-h-80">
-					<THead>
-						<Tr>
-							<Th className="border-r-0 cursor-pointer group font-normal">Name</Th>
-							<Th className="border-r-0 max-8" />
-						</Tr>
-					</THead>
-					<TBody>
-						{sortedResources.map(([name], idx) => (
-							<Tr className={cn({ "bg-black": name === currentProject.activeEditorFileName })} key={idx}>
-								<Td
-									className="font-semibold border-r-0 cursor-pointer"
-									onClick={() => updateActiveEditorFileName(name)}
-								>
-									{name}
-								</Td>
-								<Th className="border-r-0 max-w-8" />
-							</Tr>
-						))}
-					</TBody>
-				</Table>
-			) : null}
-
 			<div
-				className="mt-auto mb-auto flex justify-center items-center"
+				className={styleBase}
 				onDragEnter={handleDragOver}
 				onDragLeave={() => setIsDragOver(false)}
 				onDragOver={handleDragOver}
 				onDrop={handleDrop}
 			>
-				<div className="flex flex-col items-center gap-2.5">
-					<label className="group flex flex-col items-center gap-2.5 cursor-pointer">
-						<input accept=".py, .star" className="hidden" multiple onChange={handleFileSelect} type="file" />
-						<PlusCircle className={styleCircle} />
-						<p className="text-center text-lg font-bold uppercase text-white">Add Code & Assets</p>
-					</label>
+				{!isEmpty(sortedResources) ? (
+					<Table className="max-h-96">
+						<THead>
+							<Tr>
+								<Th className="border-r-0 cursor-pointer group font-normal">Name</Th>
+								<Th className="border-r-0 max-8" />
+							</Tr>
+						</THead>
+						<TBody>
+							{sortedResources.map(([name], idx) => (
+								<Tr className={cn({ "bg-black": name === currentProject.activeEditorFileName })} key={idx}>
+									<Td
+										className="font-semibold border-r-0 cursor-pointer"
+										onClick={() => updateActiveEditorFileName(name)}
+									>
+										{name}
+									</Td>
+									<Th className="border-r-0 max-w-8" />
+								</Tr>
+							))}
+						</TBody>
+					</Table>
+				) : null}
+				<div className={styleFrame}>
+					<div className="flex flex-col items-center gap-2.5">
+						<label className="group flex flex-col items-center gap-2.5 cursor-pointer">
+							<input accept=".py, .star" className="hidden" multiple onChange={handleFileSelect} type="file" />
+							<PlusCircle className={styleCircle} />
+							<p className="text-center text-lg font-bold uppercase text-white">Add Code & Assets</p>
+						</label>
+					</div>
 				</div>
-				<ModalAddCodeAssets onError={(message) => setToast({ isOpen: true, message })} />
-				<Toast
-					className="border-error"
-					duration={10}
-					isOpen={toast.isOpen}
-					onClose={() => setToast({ ...toast, isOpen: false })}
-				>
-					<h5 className="font-semibold">Error</h5>
-					<p className="mt-1 text-xs">{toast.message}</p>
-				</Toast>
 			</div>
+			<ModalAddCodeAssets onError={(message) => setToast({ isOpen: true, message })} />
+			<Toast
+				className="border-error"
+				duration={10}
+				isOpen={toast.isOpen}
+				onClose={() => setToast({ ...toast, isOpen: false })}
+			>
+				<h5 className="font-semibold">Error</h5>
+				<p className="mt-1 text-xs">{toast.message}</p>
+			</Toast>
 		</div>
 	);
 };
