1c48b6e3e29b9d206d96eb9918a1d33cd915c4c0
Ronen Mars <ronen@autokitteh.com>
2024-04-29 13:13:00 +0300

    fix: fix e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
 		// 	name: "chromium",
 		// 	use: { ...devices["Desktop Chrome"] },
 		// },
+
 		// {
 		// 	name: "firefox",
 		// 	use: { ...devices["Desktop Firefox"] },
