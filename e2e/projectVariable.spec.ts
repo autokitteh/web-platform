1c507d35d6bcbdb798c83537d6f30e6d5b406fa7
Ronen Mars <ronen@autokitteh.com>
2024-05-01 11:29:22 +0300

    fix: fix e2e tests

--- e2e/projectVariable.spec.ts
+++ e2e/projectVariable.spec.ts
@@ -9,6 +9,8 @@ test.describe("Project Variable", () => {
 		await button.hover();
 		await button.click();
 
+		await page.waitForTimeout(500);
+
 		await page.getByRole("tab", { name: "Variables" }).click();
 		await page.getByRole("link", { name: "Add new" }).click();
 
