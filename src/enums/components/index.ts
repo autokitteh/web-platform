a3e2c758a916f50bdcd804ad3833c51e8fe2b52f
ST <workinvs@gmail.com>
2024-05-03 09:37:45 +0300

    fix: e2e tests

--- src/enums/components/index.ts
+++ src/enums/components/index.ts
@@ -3,3 +3,4 @@ export { SortDirectionVariant } from "@enums/components/table.enum";
 export { ModalName } from "@enums/components/modal.enum";
 export { OutputTabsVariants, ProjectTabs } from "@enums/components/tabs.enum";
 export { SidebarMenu, SidebarHrefMenu } from "@enums/components/menu.enum";
+export { ReadOnlyFile } from "@enums/components/readOnlyFile.enum";
