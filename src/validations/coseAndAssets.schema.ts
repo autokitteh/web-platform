5302bd98eb9733989a35c04b530564e7e0365cfb
J1za <workinvs@gmail.com>
2024-05-10 11:10:08 +0300

    fix: translation

--- src/validations/coseAndAssets.schema.ts
+++ src/validations/coseAndAssets.schema.ts
@@ -1,14 +1,20 @@
 import i18n from "i18next";
-import { z } from "zod";
+import { z, ZodObject, ZodTypeAny } from "zod";
 
 const selectExtensionSchema = z.object({
 	label: z.string(),
 	value: z.string(),
 });
 
-export const codeAssetsSchema = z.object({
-	name: z.string().min(2, "Name is required"),
-	extension: selectExtensionSchema.refine((value) => value.label, {
-		message: i18n.t("extensionIsRequired", { ns: "validations" }),
-	}),
+let codeAssetsSchema: ZodObject<Record<string, ZodTypeAny>>;
+
+i18n.on("initialized", () => {
+	codeAssetsSchema = z.object({
+		name: z.string().min(2, i18n.t("nameIsRequired", { ns: "validations" })),
+		extension: selectExtensionSchema.refine((value) => value.label, {
+			message: i18n.t("extensionIsRequired", { ns: "validations" }),
+		}),
+	});
 });
+
+export { codeAssetsSchema };
