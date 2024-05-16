2f7b095d3524d75a140cfd84fc1c08050eb1580b
ST <workinvs@gmail.com>
2024-05-16 21:23:05 +0300

    feat: add stopPagination to handle function

--- src/components/organisms/deployments/deploymentsHistory.tsx
+++ src/components/organisms/deployments/deploymentsHistory.tsx
@@ -1,151 +1,195 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import { TrashIcon, ActionActiveIcon, ActionStoppedIcon } from "@assets/image/icons";
 import { IconButton, TBody, THead, Table, Td, Th, Toast, Tr } from "@components/atoms";
 import { SortButton } from "@components/molecules";
 import { DeploymentState, DeploymentSessionStats } from "@components/organisms/deployments";
+import { ModalDeleteDeployment } from "@components/organisms/modals";
 import { DeploymentStateVariant } from "@enums";
-import { ESortDirection } from "@enums/components";
+import { ModalName, SortDirectionVariant } from "@enums/components";
 import { DeploymentsService } from "@services";
+import { useModalStore } from "@store";
 import { SortDirection } from "@type/components";
 import { Deployment } from "@type/models";
 import { orderBy } from "lodash";
 import moment from "moment";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export const DeploymentsHistory = () => {
 	const { t: tErrors } = useTranslation("errors");
 	const { t } = useTranslation("deployments", { keyPrefix: "history" });
+
+	const { openModal, closeModal } = useModalStore();
 	const [deployments, setDeployments] = useState<Deployment[]>([]);
+	const [deploymentId, setDeploymentId] = useState<string>();
+	const [isLoadingDeployments, setIsLoadingDeployments] = useState(true);
+
 	const [sort, setSort] = useState<{
 		direction: SortDirection;
 		column: keyof Deployment;
-	}>({ direction: ESortDirection.ASC, column: "createdAt" });
+	}>({ direction: SortDirectionVariant.ASC, column: "createdAt" });
 	const [toast, setToast] = useState({
 		isOpen: false,
 		message: "",
 	});
+
 	const { projectId } = useParams();
+	const navigate = useNavigate();
 
 	const fetchDeployments = async () => {
 		if (!projectId) return;
 
-		const { data } = await DeploymentsService.listByProjectId(projectId);
-		data && setDeployments(data);
+		const { data, error } = await DeploymentsService.listByProjectId(projectId);
+		setIsLoadingDeployments(false);
+		if (error) {
+			setToast({ isOpen: true, message: (error as Error).message });
+			return;
+		}
+		if (!data) return;
+		setDeployments(data);
 	};
 
 	useEffect(() => {
 		fetchDeployments();
 	}, [projectId]);
 
-	const toggleSortDeployments = (key: keyof Deployment) => {
-		const newDirection =
-			sort.column === key && sort.direction === ESortDirection.ASC ? ESortDirection.DESC : ESortDirection.ASC;
+	const toggleSortDeployments = useCallback(
+		(key: keyof Deployment) => {
+			const newDirection =
+				sort.column === key && sort.direction === SortDirectionVariant.ASC
+					? SortDirectionVariant.DESC
+					: SortDirectionVariant.ASC;
+			setSort({ direction: newDirection, column: key });
+		},
+		[sort]
+	);
 
-		const sortedDeployments = orderBy(deployments, [key], [newDirection]);
-		setSort({ direction: newDirection, column: key });
-		setDeployments(sortedDeployments);
+	const sortedDeployments = useMemo(() => orderBy(deployments, [sort.column], [sort.direction]), [deployments, sort]);
+
+	const handleDeploymentAction = useCallback(
+		async (id: string, action: "activate" | "deactivate" | "delete", event?: React.MouseEvent) => {
+			event?.stopPropagation();
+
+			const { error } = await (action === "activate"
+				? DeploymentsService.activate(id)
+				: action === "deactivate"
+					? DeploymentsService.deactivate(id)
+					: DeploymentsService.delete(id));
+
+			if (error) {
+				setToast({ isOpen: true, message: (error as Error).message });
+				return;
+			}
+			if (action === "delete") closeModal(ModalName.deleteDeployment);
+			fetchDeployments();
+		},
+		[]
+	);
+
+	const showDeleteModal = (event: React.MouseEvent, id: string) => {
+		event.stopPropagation();
+		setDeploymentId(id);
+		openModal(ModalName.deleteDeployment);
 	};
 
-	const handleActiveDeployment = async (id: string) => {
-		await DeploymentsService.activate(id);
-		fetchDeployments();
-	};
-
-	const handleStoppedDeployment = async (id: string) => {
-		await DeploymentsService.deactivate(id);
-		fetchDeployments();
-	};
+	if (isLoadingDeployments)
+		return <div className="mt-10 text-black font-semibold text-xl text-center">{t("loading")}...</div>;
+	if (!sortedDeployments.length)
+		return <div className="mt-10 text-black font-semibold text-xl text-center">{t("noDeployments")}</div>;
 
 	return (
-		<div>
+		<>
 			<div className="flex items-center justify-between">
 				<div className="text-base text-black">
 					{t("tableTitle")} ({deployments.length})
 				</div>
 			</div>
-			{deployments.length ? (
-				<Table className="mt-4">
-					<THead>
-						<Tr>
-							<Th className="cursor-pointer group font-normal" onClick={() => toggleSortDeployments("createdAt")}>
-								{t("table.columns.deploymentTime")}
-								<SortButton
-									className="opacity-0 group-hover:opacity-100"
-									isActive={"createdAt" === sort.column}
-									sortDirection={sort.direction}
-								/>
-							</Th>
+			<Table className="mt-4">
+				<THead>
+					<Tr>
+						<Th className="cursor-pointer group font-normal" onClick={() => toggleSortDeployments("createdAt")}>
+							{t("table.columns.deploymentTime")}
+							<SortButton
+								className="opacity-0 group-hover:opacity-100"
+								isActive={"createdAt" === sort.column}
+								sortDirection={sort.direction}
+							/>
+						</Th>
 
-							<Th className="cursor-pointer group font-normal">{t("table.columns.sessions")}</Th>
-							<Th className="cursor-pointer group font-normal" onClick={() => toggleSortDeployments("buildId")}>
-								{t("table.columns.buildId")}
-								<SortButton
-									className="opacity-0 group-hover:opacity-100"
-									isActive={"buildId" === sort.column}
-									sortDirection={sort.direction}
-								/>
-							</Th>
-							<Th
-								className="cursor-pointer group font-normal border-r-0"
-								onClick={() => toggleSortDeployments("state")}
-							>
-								{t("table.columns.status")}
-								<SortButton
-									className="opacity-0 group-hover:opacity-100"
-									isActive={"state" === sort.column}
-									sortDirection={sort.direction}
-								/>
-							</Th>
-							<Th className="max-w-12 border-0" />
-							<Th className="max-w-12 border-0" />
-						</Tr>
-					</THead>
-					<TBody className="bg-gray-700">
-						{deployments.map(({ deploymentId, createdAt, state, buildId, sessionStats }) => (
-							<Tr className="group cursor-pointer" key={deploymentId}>
-								<Td className="font-semibold">{moment(createdAt).fromNow()}</Td>
-
-								<Td>
-									<DeploymentSessionStats sessionStats={sessionStats} />
-								</Td>
-								<Td>{buildId}</Td>
-								<Td className="border-r-0">
-									<DeploymentState deploymentState={state} />
-								</Td>
-								<Td className="max-w-12 border-0 pr-1.5 justify-end">
-									{state === DeploymentStateVariant.activeDeployment ? (
-										<IconButton className="p-1" onClick={() => handleStoppedDeployment(deploymentId)}>
+						<Th className="cursor-pointer group font-normal">{t("table.columns.sessions")}</Th>
+						<Th className="cursor-pointer group font-normal" onClick={() => toggleSortDeployments("buildId")}>
+							{t("table.columns.buildId")}
+							<SortButton
+								className="opacity-0 group-hover:opacity-100"
+								isActive={"buildId" === sort.column}
+								sortDirection={sort.direction}
+							/>
+						</Th>
+						<Th className="cursor-pointer group font-normal border-r-0" onClick={() => toggleSortDeployments("state")}>
+							{t("table.columns.status")}
+							<SortButton
+								className="opacity-0 group-hover:opacity-100"
+								isActive={"state" === sort.column}
+								sortDirection={sort.direction}
+							/>
+						</Th>
+						<Th className="text-right font-normal max-w-20">Actions</Th>
+					</Tr>
+				</THead>
+				<TBody className="bg-gray-700">
+					{sortedDeployments.map(({ deploymentId, createdAt, state, buildId, sessionStats }) => (
+						<Tr className="group cursor-pointer" key={deploymentId} onClick={() => navigate(deploymentId)}>
+							<Td className="font-semibold">{moment(createdAt).fromNow()}</Td>
+							<Td>
+								<DeploymentSessionStats sessionStats={sessionStats} />
+							</Td>
+							<Td>{buildId}</Td>
+							<Td className="border-r-0">
+								<DeploymentState deploymentState={state} />
+							</Td>
+							<Td className="max-w-20">
+								<div className="flex space-x-1">
+									{state === DeploymentStateVariant.active ? (
+										<IconButton
+											ariaLabel={t("ariaDeactivateDeploy")}
+											className="p-1"
+											onClick={(e) => handleDeploymentAction(deploymentId, "deactivate", e)}
+										>
 											<ActionStoppedIcon className="group-hover:fill-white w-4 h-4 transition" />
 										</IconButton>
 									) : (
-										<IconButton className="p-1" onClick={() => handleActiveDeployment(deploymentId)}>
+										<IconButton
+											ariaLabel={t("ariaActivateDeploy")}
+											className="p-1"
+											onClick={(e) => handleDeploymentAction(deploymentId, "activate", e)}
+										>
 											<ActionActiveIcon className="group-hover:fill-green-accent w-4 h-4 transition" />
 										</IconButton>
 									)}
-								</Td>
-								<Td className="max-w-12 border-0 pr-1.5 justify-end">
-									<IconButton>
+									<IconButton
+										ariaLabel={t("ariaDeleDeploy")}
+										disabled={state === DeploymentStateVariant.active}
+										onClick={(e) => showDeleteModal(e, deploymentId)}
+										title={t("deleteDisabled")}
+									>
 										<TrashIcon className="fill-white w-3 h-3" />
 									</IconButton>
-								</Td>
-							</Tr>
-						))}
-					</TBody>
-				</Table>
-			) : (
-				<div className="mt-10 text-black font-semibold text-xl text-center">{t("noDeployments")}</div>
-			)}
-
+								</div>
+							</Td>
+						</Tr>
+					))}
+				</TBody>
+			</Table>
+			<ModalDeleteDeployment onDelete={() => handleDeploymentAction(deploymentId!, "delete")} />
 			<Toast
-				className="border-error"
 				duration={5}
 				isOpen={toast.isOpen}
 				onClose={() => setToast({ ...toast, isOpen: false })}
+				title={tErrors("error")}
+				type="error"
 			>
-				<p className="font-semibold text-error">{tErrors("error")}</p>
 				<p className="mt-1 text-xs">{toast.message}</p>
 			</Toast>
-		</div>
+		</>
 	);
 };
