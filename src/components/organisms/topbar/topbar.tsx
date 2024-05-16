656de3ce588177df693237084daa4075450f2b1f
ST <workinvs@gmail.com>
2024-05-16 21:25:31 +0300

    feat: disable button in topbar

--- src/components/organisms/topbar/topbar.tsx
+++ src/components/organisms/topbar/topbar.tsx
@@ -9,11 +9,12 @@ import { useProjectStore } from "@store";
 import { Project } from "@type/models";
 import { cn } from "@utilities";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const Topbar = () => {
 	const { t } = useTranslation(["projects", "errors", "buttons"]);
 	const { projectId } = useParams();
+	const location = useLocation();
 	const {
 		getProjectsList,
 		currentProject: { resources },
@@ -138,7 +139,7 @@ export const Topbar = () => {
 				<Button
 					ariaLabel={t("topbar.buttons.ariaBuildProject")}
 					className="px-4 py-2 font-semibold text-white whitespace-nowrap hover:bg-gray-700"
-					disabled={!projectId || loadingButton[TopbarButton.build]}
+					disabled={!projectId || loadingButton[TopbarButton.build] || !location.pathname.endsWith(projectId)}
 					onClick={build}
 					variant="outline"
 				>
@@ -148,7 +149,7 @@ export const Topbar = () => {
 				<Button
 					ariaLabel={t("topbar.buttons.ariaDeployProject")}
 					className="px-4 py-2 font-semibold text-white whitespace-nowrap hover:bg-gray-700"
-					disabled={!projectId || loadingButton[TopbarButton.deploy]}
+					disabled={!projectId || loadingButton[TopbarButton.deploy] || !location.pathname.endsWith(projectId)}
 					onClick={deploy}
 					variant="outline"
 				>
@@ -158,7 +159,7 @@ export const Topbar = () => {
 				<Button
 					ariaLabel={t("topbar.buttons.ariaStats")}
 					className="px-4 py-2 font-semibold text-white whitespace-nowrap hover:bg-gray-700"
-					disabled={!projectId}
+					disabled={!projectId || !location.pathname.endsWith(projectId)}
 					href={`/projects/${projectId}/deployments`}
 					variant="outline"
 				>
@@ -175,9 +176,12 @@ export const Topbar = () => {
 							</Button>
 						</div>
 					}
-					disabled={!projectId}
 				>
-					<Button className="h-full text-white px-4 hover:bg-gray-700" disabled={!projectId} variant="outline">
+					<Button
+						className="h-full text-white px-4 hover:bg-gray-700"
+						disabled={!projectId || !location.pathname.endsWith(projectId)}
+						variant="outline"
+					>
 						<More />
 						{t("more", { ns: "buttons" })}
 					</Button>
