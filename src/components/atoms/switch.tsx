8a0e8dbadc3bcde58d03dd1439a5732c9db2aee3
J1za <workinvs@gmail.com>
2024-05-22 16:30:02 +0300

    fix: position switch

--- src/components/atoms/switch.tsx
+++ src/components/atoms/switch.tsx
@@ -6,7 +6,7 @@ export const Switch = ({ label, className, checked, onChange }: SwitchProps) => 
 	const baseStyle = cn(
 		"relative w-10 h-5 bg-gray-300 peer-focus:outline-none rounded-full peer-checked:after:translate-x-full",
 		"peer-checked:after:border-gray-300 after:content-[''] peer-checked:bg-green-accent after:absolute",
-		"after:top-[2px] after:start-[3px] after:bg-white after:border-gray-300 after:border after:rounded-full",
+		"after:top-[2px] after:start-[4px] after:bg-white after:border after:rounded-full",
 		"after:h-4 after:w-4 after:transition-all",
 		className
 	);
