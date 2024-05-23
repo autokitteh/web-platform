780e61329ee78e423f09fe4c693ddf1a02b77020
J1za <workinvs@gmail.com>
2024-05-23 17:56:26 +0300

    fix: switch

--- src/components/atoms/switch.tsx
+++ src/components/atoms/switch.tsx
@@ -4,8 +4,8 @@ import { cn } from "@utilities";
 
 export const Switch = ({ label, className, checked, onChange }: SwitchProps) => {
 	const baseStyle = cn(
-		"relative w-10 h-5 bg-gray-300 peer-focus:outline-none rounded-full peer-checked:after:translate-x-full",
-		"peer-checked:after:border-gray-300 after:content-[''] peer-checked:bg-green-accent after:absolute",
+		"relative w-10 h-5 bg-gray-300 rounded-full peer-checked:after:translate-x-full peer-checked:after:border-gray-300",
+		"after:content-[''] peer-checked:bg-green-accent after:absolute",
 		"after:top-[2px] after:start-[4px] after:bg-white after:border after:rounded-full",
 		"after:h-4 after:w-4 after:transition-all",
 		className
@@ -13,7 +13,7 @@ export const Switch = ({ label, className, checked, onChange }: SwitchProps) => 
 
 	return (
 		<label className="inline-flex items-center cursor-pointer">
-			<input checked={checked} className="sr-only peer" onChange={(e) => onChange(e.target.checked)} type="checkbox" />
+			<input checked={checked} className="hidden peer" onChange={(e) => onChange(e.target.checked)} type="checkbox" />
 			<div className={baseStyle} />
 			{label ? <span className="ms-3 text-sm font-medium text-white">{label}</span> : null}
 		</label>
