d7c64fa4493f1943620d9763e967bc04d2e06d8a
Vitaliy Stukalo <workinvs@gmail.com>
2024-05-28 10:17:24 +0300

    feat: add fetch interval new sessions (#102)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,5 @@ export { namespaces } from "@constants/namespaces.logger.constants";
 export { baseUrl } from "@constants/api.constants";
 export { menuItems, initialProjectTabs } from "@constants/lists";
 export { getSelectBlackStyles, getSelectWhiteStyles } from "@constants/forms";
-export { fetchMenuInterval, isDevelopment } from "@constants/global.constants";
+export { fetchMenuInterval, fetchSessionsInterval, isDevelopment } from "@constants/global.constants";
 export { monacoLanguages, defalutFileExtension } from "@constants/monacoLanguages.constants.ts";
