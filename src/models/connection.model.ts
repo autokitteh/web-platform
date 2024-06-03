19bb287134434d8ae8fd4962809254b954ae8ca3
J1za <workinvs@gmail.com>
2024-06-03 16:39:42 +0300

    refactor: propo model connection

--- src/models/connection.model.ts
+++ src/models/connection.model.ts
@@ -1,14 +1,29 @@
+import { Status, Status_Code } from "@ak-proto-ts/common/v1/status_pb";
 import { Connection as ProtoConnection } from "@ak-proto-ts/connections/v1/connection_pb";
-import { ConnectionStatus } from "@enums";
 import { Connection, ConnectionStatusType } from "@type/models";
+
+export const mapProtoStatusToConnectionStatus = (protoStatus?: Status): ConnectionStatusType => {
+	if (!protoStatus) return "ok";
+
+	const temporaryStatusMapping = protoStatus.code === Status_Code.UNSPECIFIED ? Status_Code.OK : protoStatus.code;
+
+	switch (temporaryStatusMapping) {
+		case Status_Code.OK:
+			return "ok";
+		case Status_Code.WARNING:
+			return "warning";
+		case Status_Code.ERROR:
+			return "error";
+	}
+};
 
 export const convertConnectionProtoToModel = (protoConnection: ProtoConnection): Connection => {
 	return {
 		connectionId: protoConnection.connectionId,
 		integrationId: protoConnection.integrationId,
 		name: protoConnection.name,
-		initUrl: protoConnection.links.init_url,
-		status: ConnectionStatus[protoConnection.status!.code] as ConnectionStatusType,
-		statusInfoMessage: protoConnection.status!.message,
+		initUrl: protoConnection.links?.init_url || "",
+		status: mapProtoStatusToConnectionStatus(protoConnection.status),
+		statusInfoMessage: protoConnection.status?.message || "",
 	};
 };
