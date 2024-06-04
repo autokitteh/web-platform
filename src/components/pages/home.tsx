dbe97db21c978c5f709262d0011c7ba35a4f5406
Ronen Mars <ronen@autokitteh.com>
2024-06-04 14:44:35 +0300

    feat: css loader

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppWrapper, MapMenuFrameLayout } from "@components/templates";
+
+export const Home = () => {
+	return (
+		<AppWrapper>
+			<MapMenuFrameLayout />
+		</AppWrapper>
+	);
+};
