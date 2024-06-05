e643a3e9cebbe1daed37b02b9dcf8258594ec005
Ronen Mars <ronen@autokitteh.com>
2024-06-05 12:29:09 +0300

    fix: fix descope app page

--- src/constants/global.constants.ts
+++ src/constants/global.constants.ts
@@ -4,4 +4,4 @@ export const fetchDeploymentsInterval = 30000;
 
 export const isDevelopment = import.meta.env.NODE_ENV === "development";
 export const isAuthEnabled: boolean = import.meta.env.VITE_AUTH_ENABLED === "true";
-export const descopeProjectId: string = import.meta.env.VITE_DESCOPE_PROJECT_ID || "descopeProjectId";
+export const descopeProjectId: string = import.meta.env.VITE_DESCOPE_PROJECT_ID;
