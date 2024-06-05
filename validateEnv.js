ab17bf7a986fd63b76f2f989297e96e60651d09e
Ronen Mars <ronen@autokitteh.com>
2024-06-05 16:48:09 +0300

    fix: fix descope auth

--- validateEnv.js
+++ validateEnv.js
@@ -2,10 +2,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.VITE_AUTH_ENABLED) {
-	throw new Error("Environment variable - VITE_AUTH_ENABLED not defined");
-} else if (!process.env.VITE_DESCOPE_PROJECT_ID && process.env.VITE_AUTH_ENABLED === "true") {
+if (!process.env.VITE_DESCOPE_PROJECT_ID && process.env.VITE_AUTH_ENABLED === "true") {
 	throw new Error("Environment variable - VITE_DESCOPE_PROJECT_ID not defined for descope authentication");
-} else {
-	console.log("Environment variables validated successfully.");
 }
