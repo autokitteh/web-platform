beb6127c9700ccc9f73dca2d35298c6744f9be7d
Ronen Mars <ronen@autokitteh.com>
2024-06-06 07:57:20 +0300

    fix: fix descope auth

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DescopeWrapper } from "@components/templates";
 import { descopeProjectId, isAuthEnabled } from "@constants";
 import { router } from "@routing/routes";
+import { useProjectStore } from "@store/useProjectStore";
 import { RouterProvider } from "react-router-dom";
 
 export const App: React.FC = () => {
+	const { getProjectsList } = useProjectStore();
+
+	useEffect(() => {
+		getProjectsList();
+	}, [getProjectsList]);
+
 	return (
 		<div>
 			{isAuthEnabled && descopeProjectId ? (
