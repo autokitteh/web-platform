8144ad5d7e39b215c594fd8140c19ba66ea0a787
Ronen Mars <ronen@autokitteh.com>
2024-06-07 17:20:25 +0300

    refactor: system refactor

--- src/constants/lists/index.ts
+++ src/constants/lists/index.ts
@@ -1,4 +1,3 @@
 export { menuItems } from "@constants/lists/menuItems.constants";
-// export { initialProjectTabs } from "@constants/lists/tabsProject.constants";
 export { selectConnectionApp, selectIntegrationGithub } from "@constants/lists/connections";
 export { infoGithubLinks } from "@constants/lists/connections";
--- src/constants/lists/tabsValue.constants.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import { AddCodeAssetsTab, TriggersContent, VariablesContent } from "@components/organisms";
-// import { ProjectTabs } from "@enums/components";
-
-// export const tabsMainFrame = [
-// 	{ title: ProjectTabs.codeAndAssets, count: 2, content: () => <AddCodeAssetsTab /> },
-// 	{ title: ProjectTabs.triggers, count: 4, content: () => <TriggersContent /> },
-// 	{ title: ProjectTabs.variables, count: 4, content: () => <VariablesContent /> },
-// ];
