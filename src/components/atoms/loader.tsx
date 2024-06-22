b4c0dd7a6a2961a74fc2688d2dd18013384d8b26
Ronen Mars <ronen@autokitteh.com>
2024-06-22 16:44:17 +0300

    refactor: refactor logger for easier use

--- src/components/atoms/loader.tsx
+++ src/components/atoms/loader.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { LoaderProps } from "@interfaces/components/loader.interface";
 import { cn } from "@utilities";
 
-export const Loader = ({ size = "xl", firstColor = "dark-gray", secondColor = "gray" }: LoaderProps) => {
-	const sizeClass = cn("loader-cycle-disks", {
-		"before:w-2 before:h-2 after:w-2 after:h-2": size === "sm",
-		"before:w-4 before:h-4 after:w-4 after:h-4": size === "md",
-		"before:w-6 before:h-6 after:w-6 after:h-6": size === "lg",
-		"before:w-8 before:h-8 after:w-8 after:h-8": size === "xl",
+export const Loader = ({ size = "md", firstColor = "dark-gray", secondColor = "gray" }: LoaderProps) => {
+	const sizeClass = cn("loader-cycle-disks flex items-center", {
+		"w-4 before:w-2 before:h-2 after:w-2 after:h-2": size === "sm",
+		"w-8 before:w-4 before:h-4 after:w-4 after:h-4": size === "md",
+		"w-6 before:w-6 before:h-6 after:w-6 after:h-6": size === "lg",
+		"w-16 before:w-8 before:h-8 after:w-8 after:h-8": size === "xl",
 	});
 
 	const spinnerClass = cn(sizeClass, {
