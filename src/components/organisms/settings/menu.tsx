bfe953bcca161043390511a514065fd152b45c95
Ronen Mars <ronen@autokitteh.com>
2024-06-23 01:31:51 +0300

    feat: settings page - wip - progress

--- src/components/organisms/settings/menu.tsx
+++ src/components/organisms/settings/menu.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { Bell, Receipt, Security, Sliders, User } from "@assets/image/icons";
 import { IconSvg } from "@components/atoms";
+import { useTranslation } from "react-i18next";
 
-export const SettingsMenu = () => (
-	<div className="flex bg-black flex-1 rounded-tl-lg rounded-bl-lg flex-col pt-10 pl-6 h-full text-lg">
-		<div className="flex mb-4 cursor-pointer group" role="link">
-			<div className="p-2 bg-gray-500 rounded-full mr-2">
-				<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={User} />
+export const SettingsMenu = () => {
+	const { t } = useTranslation("settings");
+
+	return (
+		<div className="flex bg-black flex-1 rounded-tl-lg rounded-bl-lg flex-col pt-10 pl-6 h-full text-lg">
+			<div className="flex mb-4 cursor-pointer group" role="link">
+				<div className="p-2 bg-gray-500 rounded-full mr-2">
+					<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={User} />
+				</div>
+				{t("menu.myProfile")}
 			</div>
-			My Profile
-		</div>
-		<div className="flex mb-4 cursor-pointer group" role="link">
-			<div className="p-2 bg-gray-500 rounded-full mr-2">
-				<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Security} />
+			<div className="flex mb-4 cursor-pointer group" role="link">
+				<div className="p-2 bg-gray-500 rounded-full mr-2">
+					<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Security} />
+				</div>
+				{t("menu.security")}
 			</div>
-			Security
-		</div>
-		<div className="flex mb-4 cursor-pointer group" role="link">
-			<div className="p-2 bg-gray-500 rounded-full mr-2">
-				<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Bell} />
+			<div className="flex mb-4 cursor-pointer group" role="link">
+				<div className="p-2 bg-gray-500 rounded-full mr-2">
+					<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Bell} />
+				</div>
+				{t("menu.notifications")}
 			</div>
-			Notifications
-		</div>
-		<div className="flex mb-4 cursor-pointer group" role="link">
-			<div className="p-2 bg-gray-500 rounded-full mr-2">
-				<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Sliders} />
+			<div className="flex mb-4 cursor-pointer group" role="link">
+				<div className="p-2 bg-gray-500 rounded-full mr-2">
+					<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Sliders} />
+				</div>
+				{t("menu.advanced")}
 			</div>
-			Advanced
-		</div>
-		<div className="flex mb-4 cursor-pointer group" role="link">
-			<div className="p-2 bg-gray-500 rounded-full mr-2">
-				<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Receipt} />
+			<div className="flex mb-4 cursor-pointer group" role="link">
+				<div className="p-2 bg-gray-500 rounded-full mr-2">
+					<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={Receipt} />
+				</div>
+				{t("menu.billing")}
 			</div>
-			Billing
 		</div>
-	</div>
-);
+	);
+};
