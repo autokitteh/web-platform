7f3e320dbc3bd22c198f5ef146c420215f2cb0f1
Ronen Mars <ronen@autokitteh.com>
2024-06-24 10:13:55 +0300

    feat: handle bad urls - 404

--- src/components/pages/notFound404.tsx
+++ src/components/pages/notFound404.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Error404 } from "@assets/image";
+import { Icon } from "@components/atoms";
+import { Link } from "react-router-dom";
 
 export const NotFound404: React.FC = () => (
-	<div>
-		<div className="flex w-full">
-			<h1 className="text-black w-full text-2xl font-averta-bold mt-6">Page not found 404</h1>
-		</div>
+	<div className="flex flex-col w-full h-full justify-center items-center">
+		<div className="text-black mb-8 text-lg">Error: Page doesn&apos;t exist</div>
+		<Icon className="w-1/3" src={Error404} />
+		<Link className="text-black text-2xl mt-8" to="/">
+			Back to Home
+		</Link>
 	</div>
 );
