fead335878cb17b170367ff07137d7a1cbc05431
Ronen Mars <ronen@autokitteh.com>
2024-06-24 08:18:43 +0300

    feat: settings page - wip - progress

--- src/components/organisms/settings/topbar.tsx
+++ src/components/organisms/settings/topbar.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { cn } from "@utilities";
+import { useTranslation } from "react-i18next";
 
 export const SettingsTopbar = () => {
+	const { t } = useTranslation("settings", { keyPrefix: "topbar" });
+
 	const styleInput = cn(
 		"font-bold p-0 text-xl leading-6 bg-transparent min-w-3 outline outline-0 rounded leading-tight"
 	);
@@ -9,7 +12,7 @@ export const SettingsTopbar = () => {
 	return (
 		<div className="flex justify-between items-center bg-gray-800 gap-5 pl-7 pr-3.5 py-3 rounded-b-xl">
 			<div className="flex items-end gap-3 relative font-fira-code text-gray-300">
-				<span className={styleInput}>Settings</span>
+				<span className={styleInput}>{t("title")}</span>
 			</div>
 		</div>
 	);
