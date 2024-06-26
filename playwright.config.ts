813c7f63ead8229856623e3c89cc0292ea47b3e2
Ronen Mars <ronen@autokitteh.com>
2024-06-26 13:36:13 +0300

    ci: add trace to successfull tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 		baseURL: "http://localhost:4173",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: "retain-on-failure",
+		trace: "on",
 		video: "retain-on-failure",
 	},
 
