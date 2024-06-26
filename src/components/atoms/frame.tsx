716a42745a2ecec149848b1da8b1111d56e55de8
J1za <workinvs@gmail.com>
2024-06-26 16:23:40 +0300

    refactor: frame

--- src/components/atoms/frame.tsx
+++ src/components/atoms/frame.tsx
@@ -3,7 +3,7 @@ import { FrameProps } from "@interfaces/components";
 import { cn } from "@utilities";
 
 export const Frame = ({ className, children }: FrameProps) => {
-	const frameStyle = cn("px-8 py-10 rounded-2xl w-full bg-black relative flex flex-col h-[85vh]", className);
+	const frameStyle = cn("px-8 py-10 rounded-2xl w-full bg-black relative flex flex-col", className);
 
 	return <div className={frameStyle}>{children}</div>;
 };
--- src/components/organisms/splitFrame.tsx
+++ src/components/organisms/splitFrame.tsx
@@ -7,31 +7,33 @@ import { cn } from "@utilities";
 
 export const SplitFrame = ({ children }: SplitFrameProps) => {
 	const [leftSideWidth] = useResize({ min: 35, max: 70, initial: 50, direction: "horizontal" });
-	const [outputHeight] = useResize({ min: 5, max: 95, initial: 30, direction: "vertical" });
+	const [outputHeight] = useResize({ min: 20, max: 90, initial: 30, direction: "vertical" });
 
-	const mainFrameStyle = cn("rounded-l-none pb-0 overflow-hidden", { "rounded-2xl": !children });
+	const heightFrameClass = cn("h-[80vh] xl:h-[86vh] 2xl:h-[90vh]");
+	const rightFrameClass = cn(`rounded-l-none pb-0 overflow-hidden`, {
+		[heightFrameClass]: true,
+		"rounded-2xl": !children,
+	});
+
+	const leftFrameClass = cn(`flex-auto bg-gray-800 border-r border-gray-600 rounded-r-none ${heightFrameClass}`);
 
 	return (
-		<div className="flex justify-end w-full ">
-			<div className="flex" style={{ width: `${leftSideWidth}%` }}>
-				{children ? (
-					<Frame className="flex-auto bg-gray-800 border-r border-gray-600 rounded-r-none">{children}</Frame>
-				) : null}
+		<div className="flex justify-end w-full">
+			<div className="flex items-center" style={{ width: `${leftSideWidth}%` }}>
+				{children ? <Frame className={leftFrameClass}>{children}</Frame> : null}
 			</div>
 			<div className="z-10 w-2 -ml-2 resize-handle-horizontal cursor-ew-resize" />
-			<div style={{ width: `${100 - (leftSideWidth as number)}%` }}>
-				<Frame className={mainFrameStyle}>
-					<div className="flex flex-col h-full">
-						<div style={{ height: `${100 - (outputHeight as number)}%` }}>
-							<EditorTabs />
-						</div>
-						<Button className="p-1 -mx-8 cursor-ns-resize resize-handle-vertical bg-gray-700 z-40 rounded-none" />
-						<div
-							className="px-8 -mx-8 border-0 border-t pt-7 border-t-gray-600"
-							style={{ height: `${outputHeight as number}%` }}
-						>
-							<OutputTabs />
-						</div>
+			<div className="flex items-center" style={{ width: `${100 - (leftSideWidth as number)}%` }}>
+				<Frame className={rightFrameClass}>
+					<div style={{ height: `${100 - (outputHeight as number)}%` }}>
+						<EditorTabs />
+					</div>
+					<Button className="z-0 p-0.5 -mx-8 transition bg-gray-700 rounded-none cursor-ns-resize resize-handle-vertical hover:bg-gray-400" />
+					<div
+						className="z-0 px-8 pt-5 -mx-8 bg-black border-0 border-t border-t-gray-600"
+						style={{ height: `${outputHeight as number}%` }}
+					>
+						<OutputTabs />
 					</div>
 				</Frame>
 				<LogoCatLarge />
