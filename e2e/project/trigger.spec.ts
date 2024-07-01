282848c33ee412f82befb036a0a8fb4b1fbc7d4e
J1za <workinvs@gmail.com>
2024-07-01 17:39:09 +0300

    test: e2e trigger

--- e2e/project/trigger.spec.ts
+++ e2e/project/trigger.spec.ts
@@ -1,43 +1,4 @@
 import { test, expect } from "@e2e/fixtures";
-import { Page } from "@playwright/test";
-
-async function createTrigger(page: Page, name: string, cronExpression: string, fileName: string, functionName: string) {
-	await page.getByRole("link", { name: "Add new" }).click();
-
-	await page.getByRole("combobox", { name: "Select trigger type", exact: true }).click();
-	await page.getByRole("option", { name: "Scheduler" }).click();
-
-	const nameInput = page.getByRole("textbox", { name: "Name", exact: true });
-	await nameInput.click();
-	await nameInput.fill(name);
-
-	const cronInput = page.getByRole("textbox", { name: "Cron expression" });
-	await cronInput.click();
-	await cronInput.fill(cronExpression);
-
-	await page.getByRole("combobox", { name: "Select file", exact: true }).click();
-	await page.getByRole("option", { name: fileName }).click();
-
-	const functionNameInput = page.getByRole("textbox", { name: "Function name" });
-	await functionNameInput.click();
-	await functionNameInput.fill(functionName);
-
-	await page.getByRole("button", { name: "Save" }).click();
-}
-
-async function modifyTrigger(page: Page, name: string, cronExpression: string, functionName: string) {
-	await page.getByRole("button", { name: `Modify ${name} trigger` }).click();
-
-	const cronInput = page.getByRole("textbox", { name: "Cron expression" });
-	await cronInput.click();
-	await cronInput.fill(cronExpression);
-
-	const functionNameInput = page.getByRole("textbox", { name: "Function name" });
-	await functionNameInput.click();
-	await functionNameInput.fill(functionName);
-
-	await page.getByRole("button", { name: "Save" }).click();
-}
 
 test.beforeEach(async ({ page, dashboardPage }) => {
 	await dashboardPage.createProjectFromMenu();
@@ -54,36 +15,31 @@ test.beforeEach(async ({ page, dashboardPage }) => {
 
 test.describe("Project Triggers Suite", () => {
 	test("Create trigger with cron expression", async ({ page }) => {
-		await createTrigger(page, "triggerName", "5 4 * * *", "newFile.star", "functionName");
+		await page.getByRole("link", { name: "Add new" }).click();
 
+		await page.getByRole("combobox", { name: "Select trigger type" }).click();
+		await page.getByRole("option", { name: "Scheduler" }).click();
+
+		const nameInput = page.getByRole("textbox", { name: "Name", exact: true });
+		await nameInput.click();
+		await nameInput.fill("triggerName");
+
+		const cronInput = page.getByRole("textbox", { name: "Cron expression" });
+		await cronInput.click();
+		await cronInput.fill("5 4 * * *");
+
+		await page.getByRole("combobox", { name: "Select file" }).click();
+		await page.getByRole("option", { name: "newFile.star" }).click();
+
+		const functionNameInput = page.getByRole("textbox", { name: "Function name" });
+		await functionNameInput.click();
+		await functionNameInput.fill("functionName");
+
+		await page.getByRole("button", { name: "Save" }).click();
 		const newRowInTable = page.getByRole("row", { name: "triggerName" });
 		await expect(newRowInTable).toHaveCount(1);
 
 		const newCallInTable = page.getByRole("cell", { name: "newFile.star:functionName" });
 		await expect(newCallInTable).toBeVisible();
-	});
-
-	test("Modify trigger with cron expression", async ({ page }) => {
-		await createTrigger(page, "triggerName", "5 4 * * *", "newFile.star", "functionName");
-
-		const newRowInTable = page.getByRole("cell", { name: "triggerName", exact: true });
-		await expect(newRowInTable).toBeVisible();
-
-		await modifyTrigger(page, "triggerName", "4 4 * * *", "newFunctionName");
-
-		const newCallInTable = page.getByRole("cell", { name: "newFile.star:newFunctionName" });
-		await expect(newCallInTable).toBeVisible();
-	});
-
-	test("Delete trigger", async ({ page }) => {
-		await createTrigger(page, "triggerName", "5 4 * * *", "newFile.star", "functionName");
-
-		const newRowInTable = page.getByRole("cell", { name: "triggerName", exact: true });
-		await expect(newRowInTable).toBeVisible();
-
-		await page.getByRole("button", { name: "Delete triggerName trigger" }).click();
-		await page.getByRole("button", { name: "Yes, delete" }).click();
-		const newVariableInTable = page.getByRole("cell", { name: "triggerName", exact: true });
-		await expect(newVariableInTable).not.toBeVisible();
 	});
 });
