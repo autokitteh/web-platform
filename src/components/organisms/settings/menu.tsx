0c0251d622c2057fdf4bc3355b3e736133ef1674
Ronen Mars <ronen@autokitteh.com>
2024-07-02 16:54:07 +0300

    refactor: center settings menu items

--- src/components/organisms/settings/menu.tsx
+++ src/components/organisms/settings/menu.tsx
@@ -9,11 +9,11 @@ export const SettingsMenu = () => {
 	return (
 		<div className="flex bg-black flex-1 rounded-tl-lg rounded-bl-lg flex-col pt-10 pl-6 h-full text-lg">
 			{menuItems.map((item, index) => (
-				<div className="flex mb-4 cursor-pointer group" key={index} role="link">
+				<div className="flex mb-4 cursor-pointer group items-center" key={index} role="link">
 					<div className="p-2 bg-gray-500 rounded-full mr-2">
 						<IconSvg className="fill-white h-3 w-3 group-hover:fill-green-accent" src={item.icon} />
 					</div>
-					{t(item.label)}
+					<div>{t(item.label)}</div>
 				</div>
 			))}
 		</div>
