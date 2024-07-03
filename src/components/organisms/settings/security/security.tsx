c4dcdb7e3ddd1905951207fab1e5f9224f44a0f2
Ronen Mars <ronen@autokitteh.com>
2024-07-03 12:21:56 +0300

    feat: security-token

--- src/components/organisms/settings/security/security.tsx
+++ src/components/organisms/settings/security/security.tsx
@@ -2,39 +2,104 @@ import { useState } from "react";
 import React from "react";
 import { DisplayConnectionTokenModal } from "./displayTokenModal";
 import { NewProject } from "@assets/image";
-import { Button, IconSvg, Loader, Typography } from "@components/atoms";
-import { ModalName } from "@enums/components";
+import { CopyIcon } from "@assets/image/icons";
+import { Button, IconSvg, Input, Loader, Typography } from "@components/atoms";
 import { HttpService } from "@services";
-import { useModalStore } from "@store";
-import { useTranslation } from "react-i18next";
+import { useToastStore } from "@store";
+import { Trans, useTranslation } from "react-i18next";
 
 export const Security = () => {
-	const { t } = useTranslation("settings");
+	const { t: tSettings } = useTranslation("settings");
+	const { t } = useTranslation("modals", { keyPrefix: "getToken" });
+	const { t: tErrors } = useTranslation("errors");
 	const [isLoading, setIsLoading] = useState(false);
-	const { openModal } = useModalStore();
+	const addToast = useToastStore((state) => state.addToast);
+	const [token, setToken] = useState<string>("");
 
 	const createToken = async () => {
 		setIsLoading(true);
 		const { data: jwtToken } = await HttpService.post("/auth/tokens");
+		setToken(jwtToken);
 		setIsLoading(false);
-		openModal(ModalName.getToken, jwtToken);
 	};
+
+	const copyToClipboard = async (text: string) => {
+		try {
+			await navigator.clipboard.writeText(text);
+
+			addToast({
+				id: Date.now().toString(),
+				message: t("copySuccess"),
+				title: "Success",
+				type: "success",
+			});
+		} catch (err) {
+			addToast({
+				id: Date.now().toString(),
+				message: t("copyFailure"),
+				title: tErrors("error"),
+				type: "error",
+			});
+		}
+	};
+
 	return (
 		<>
 			<Typography className="mb-4 text-settings-title font-bold" element="h1" size="large">
-				{t("security.title")}
+				{tSettings("security.title")}
 			</Typography>
 			<div>
-				<p className="mb-4">{t("security.howToUseTokenText")}</p>
-				<Button className="border-2 border-black" onClick={createToken} variant="light">
-					<div className="ml-1 flex items-center">
-						{!isLoading ? (
-							<IconSvg alt="New Project" className="w-4 before:w-2 before:h-2 after:w-2 after:h-2" src={NewProject} />
-						) : null}
-						{isLoading ? <Loader size="sm" /> : null}
-					</div>
-					<div className="flex mr-1">{t("security.creatTokenButton")}</div>
-				</Button>
+				<p className="mb-4 w-2/3">
+					<Trans
+						components={{
+							anchor: (
+								<a
+									href="https://marketplace.visualstudio.com/items?itemName=autokitteh.autokitteh"
+									key="extensionLink"
+									rel="noreferrer"
+									target="_blank"
+									title="AutoKitteh Extension"
+								/>
+							),
+						}}
+						i18nKey={t("line")}
+					/>
+				</p>
+				<div className="flex flex-row w-2/3">
+					{!token ? (
+						<Button className="flex border-2 border-black" onClick={createToken} variant="light">
+							<div className="flex ml-1 items-center">
+								{!isLoading ? (
+									<IconSvg
+										alt="New Project"
+										className="w-4 before:w-2 before:h-2 after:w-2 after:h-2"
+										src={NewProject}
+									/>
+								) : null}
+								{isLoading ? <Loader size="sm" /> : null}
+							</div>
+							<div className="flex mr-1">{tSettings("security.creatTokenButton")}</div>
+						</Button>
+					) : null}
+					{token ? (
+						<div className="flex w-full">
+							<Input
+								aria-label={t("copyInputAriaLabel")}
+								className="flex-1 hover:border-gray-700 rounded-3xl"
+								disabled
+								value={token}
+							/>
+							<Button
+								aria-label={t("copyButton")}
+								className="px-3 font-semibold bg-white border-black rounded-md hover:bg-gray-300 ml-4"
+								onClick={() => copyToClipboard(token)}
+								variant="outline"
+							>
+								<CopyIcon className="w-4.1 h-6 fill-black" />
+							</Button>
+						</div>
+					) : null}
+				</div>
 			</div>
 			<DisplayConnectionTokenModal />
 		</>
