85672690cc9849c4f666e2ae71dc97d8fce0beb4
J1za <workinvs@gmail.com>
2024-07-03 15:01:06 +0300

    style: update table colors

--- src/components/atoms/table/tR.tsx
+++ src/components/atoms/table/tR.tsx
@@ -3,7 +3,7 @@ import { TableProps } from "@interfaces/components";
 import { cn } from "@utilities";
 
 export const Tr = ({ className, children, onClick, style }: TableProps) => {
-	const tRStyle = cn("flex border-b-2 border-gray-600 transition hover:bg-black", className);
+	const tRStyle = cn("flex border-b-2 border-gray-600 transition hover:bg-gray-800", className);
 
 	return (
 		<tr className={tRStyle} onClick={onClick} style={style}>
