580f5c9215d3164e4058094e14a9ecc942b74941
J1za <workinvs@gmail.com>
2024-07-04 11:06:30 +0300

    chore: update font size for settings title

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,14 +4,14 @@ import plugin from "tailwindcss/plugin";
 export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		fontSize: {
-			"settings-title": "1.3rem",
-		},
 		fontFamily: {
 			"fira-sans": ["Fira Sans", "sans-serif"],
 			"fira-code": ["Fira Code", "sans-serif"],
 		},
 		extend: {
+			fontSize: {
+				"settings-title": "1.3rem",
+			},
 			flex: {
 				5: "5 5 0%",
 			},
