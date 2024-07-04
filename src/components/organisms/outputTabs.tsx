06f34ad33d1b903b0b202817e9e8673ba2b21193
J1za <workinvs@gmail.com>
2024-07-04 15:24:20 +0300

    refactor: height outputTabs

--- src/components/organisms/outputTabs.tsx
+++ src/components/organisms/outputTabs.tsx
@@ -16,18 +16,18 @@ export const OutputTabs: React.FC = () => {
 	} as const;
 
 	return (
-		<div className="flex flex-col flex-1">
+		<div className="flex flex-col h-full pb-3">
 			<div
 				className={
 					`static top-4 h-8 uppercase flex items-center gap-1 xl:gap-2 2xl:gap-4 3xl:gap-5 select-none ` +
-					`overflow-x-auto overflow-y-hidden whitespace-nowrap scrollbar mb-2`
+					`overflow-x-auto overflow-y-hidden whitespace-nowrap scrollbar`
 				}
 			>
 				<Tab activeTab={OutputTabsVariants.output} value={OutputTabsVariants.output}>
 					output
 				</Tab>
 			</div>
-			<div className="flex-auto h-48 pt-6 mt-4 overflow-auto scrollbar">
+			<div className="flex-auto h-48 pt-6 overflow-auto scrollbar">
 				{logs.map(({ id, timestamp, message, status }) => (
 					<div className="mb-4" key={id}>
 						<p className="font-medium text-gray-200">{timestamp}</p>
