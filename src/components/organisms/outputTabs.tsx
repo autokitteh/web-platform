ff827ed7fdbb2abfe75d59e012f763ec50b419da
Ronen Mars <ronen@autokitteh.com>
2024-07-21 08:25:39 +0300

    feat: clear logs button

--- src/components/organisms/outputTabs.tsx
+++ src/components/organisms/outputTabs.tsx
@@ -7,8 +7,11 @@ import { useLoggerStore } from "@store";
 
 import { Tab } from "@components/atoms";
 
+import { TrashCanIcon } from "@assets/image/icons";
+
 export const OutputTabs: React.FC = () => {
 	const logs = useLoggerStore((state) => state.logs);
+	const clearLogs = useLoggerStore((state) => state.clearLogs);
 
 	const ouputTextStyle = {
 		[LoggerLevel.debug]: "",
@@ -23,12 +26,21 @@ export const OutputTabs: React.FC = () => {
 			<div
 				className={
 					`static top-4 flex h-8 select-none items-center gap-1 uppercase xl:gap-2 2xl:gap-4 3xl:gap-5 ` +
-					`scrollbar overflow-x-auto overflow-y-hidden whitespace-nowrap`
+					`scrollbar w-full overflow-x-auto overflow-y-hidden whitespace-nowrap`
 				}
 			>
 				<Tab activeTab={OutputTabsVariants.output} value={OutputTabsVariants.output}>
 					output
 				</Tab>
+
+				<div className="mb-1 flex w-full items-center justify-end">
+					<button
+						className="h-5 w-5 cursor-pointer fill-white hover:h-6 hover:w-6 hover:rounded-full hover:border-4 hover:border-white hover:bg-gray-black-200 hover:fill-gray-black-500"
+						onClick={() => clearLogs()}
+					>
+						<TrashCanIcon className="h-4 w-4" />
+					</button>
+				</div>
 			</div>
 
 			<div className="scrollbar h-48 flex-auto overflow-auto pt-6">
