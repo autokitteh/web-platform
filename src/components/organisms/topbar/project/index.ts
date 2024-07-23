2aec540e693b57b4b0c87b98807e3ade678817cf
J1za <workinvs@gmail.com>
2024-07-23 10:45:16 +0300

    refactor: project topbar component

--- src/components/organisms/topbar/project/index.ts
+++ src/components/organisms/topbar/project/index.ts
@@ -1,2 +1,3 @@
 export { ProjectTopbar } from "@components/organisms/topbar/project/project";
 export { DeleteProjectModal } from "@components/organisms/topbar/project/deleteProjectModal";
+export { ProjectTopbarButtons } from "@components/organisms/topbar/project/buttons";
--- src/components/organisms/topbar/project/project.tsx
+++ src/components/organisms/topbar/project/project.tsx
@@ -1,31 +1,22 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useTranslation } from "react-i18next";
-import { redirect, useNavigate, useParams } from "react-router-dom";
+import { redirect, useParams } from "react-router-dom";
 
-import { ModalName, TopbarButton } from "@enums/components";
 import { ProjectsService } from "@services";
 import { Project } from "@type/models";
 import { cn } from "@utilities";
 
-import { useModalStore, useProjectStore, useToastStore } from "@store";
+import { useProjectStore, useToastStore } from "@store";
 
-import { Button, ErrorMessage, IconSvg, Spinner } from "@components/atoms";
-import { DropdownButton } from "@components/molecules";
-import { DeleteProjectModal } from "@components/organisms";
-
-import { BuildIcon, DeployIcon, MoreIcon, StatsIcon } from "@assets/image";
-import { TrashIcon } from "@assets/image/icons";
+import { ErrorMessage } from "@components/atoms";
+import { ProjectTopbarButtons } from "@components/organisms/topbar/project";
 
 export const ProjectTopbar = () => {
 	const { t } = useTranslation(["projects", "buttons"]);
-	const { t: tError } = useTranslation("errors");
 	const { projectId } = useParams();
-	const navigate = useNavigate();
-	const { closeModal, openModal } = useModalStore();
-	const { getProject, getProjectsList, renameProject, resources } = useProjectStore();
+	const { getProject, renameProject } = useProjectStore();
 	const [isNameValid, setIsNameValid] = useState<boolean>(true);
-	const [loadingButton, setLoadingButton] = useState<Record<string, boolean>>({});
 	const [project, setProject] = useState<Project>();
 	const addToast = useToastStore((state) => state.addToast);
 	const inputClass = cn(
@@ -104,81 +95,6 @@ export const ProjectTopbar = () => {
 		setIsNameValid(validateName(newName));
 	};
 
-	const build = async () => {
-		if (!Object.keys(resources).length) {
-			return <div />;
-		}
-
-		setLoadingButton((prev) => ({ ...prev, [TopbarButton.build]: true }));
-
-		const { error } = await ProjectsService.build(projectId!, resources);
-		if (error) {
-			addToast({
-				id: Date.now().toString(),
-				message: (error as Error).message,
-				type: "error",
-			});
-		} else {
-			addToast({
-				id: Date.now().toString(),
-				message: t("topbar.buildProjectSuccess"),
-				type: "success",
-			});
-		}
-
-		setLoadingButton((prev) => ({ ...prev, [TopbarButton.build]: false }));
-	};
-
-	const deploy = async () => {
-		if (!Object.keys(resources).length) {
-			return <div />;
-		}
-
-		setLoadingButton((prev) => ({ ...prev, [TopbarButton.deploy]: true }));
-
-		const { error } = await ProjectsService.run(projectId!, resources);
-		if (error) {
-			addToast({
-				id: Date.now().toString(),
-				message: (error as Error).message,
-				type: "error",
-			});
-		} else {
-			addToast({
-				id: Date.now().toString(),
-				message: t("topbar.deployedProjectSuccess"),
-				type: "success",
-			});
-		}
-
-		setLoadingButton((prev) => ({ ...prev, [TopbarButton.deploy]: false }));
-	};
-
-	const handleDeleteProject = async () => {
-		if (!projectId) {
-			return;
-		}
-
-		const { error } = await ProjectsService.delete(projectId);
-		closeModal(ModalName.deleteProject);
-		if (error) {
-			addToast({
-				id: Date.now().toString(),
-				message: tError("projectRemoveFailed"),
-				type: "error",
-			});
-
-			return;
-		}
-		await getProjectsList();
-		navigate("/");
-	};
-
-	const handleOpenModalDeletePrject = useCallback(() => {
-		openModal(ModalName.deleteProject);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
 	return (
 		<div className="flex items-center justify-between gap-5 rounded-b-xl bg-gray-1250 py-3 pl-7 pr-3.5">
 			<div className="relative flex items-end gap-3 font-fira-code text-gray-500">
@@ -203,65 +119,7 @@ export const ProjectTopbar = () => {
 				<span className="text-sm font-semibold leading-tight">{project?.id}</span>
 			</div>
 
-			<div className="flex items-stretch gap-3">
-				<Button
-					ariaLabel={t("topbar.buttons.ariaBuildProject")}
-					className="whitespace-nowrap px-4 py-2 font-semibold text-white hover:bg-gray-1100"
-					disabled={loadingButton[TopbarButton.build]}
-					onClick={build}
-					variant="outline"
-				>
-					{loadingButton[TopbarButton.build] ? <Spinner /> : <IconSvg size="lg" src={BuildIcon} />}
-
-					{t("topbar.buttons.build")}
-				</Button>
-
-				<Button
-					ariaLabel={t("topbar.buttons.ariaDeployProject")}
-					className="whitespace-nowrap px-4 py-2 font-semibold text-white hover:bg-gray-1100"
-					disabled={loadingButton[TopbarButton.deploy]}
-					onClick={deploy}
-					variant="outline"
-				>
-					{loadingButton[TopbarButton.deploy] ? <Spinner /> : <IconSvg size="lg" src={DeployIcon} />}
-
-					{t("topbar.buttons.deploy")}
-				</Button>
-
-				<Button
-					ariaLabel={t("topbar.buttons.ariaStats")}
-					className="whitespace-nowrap px-4 py-2 font-semibold text-white hover:bg-gray-1100"
-					href={`/projects/${projectId}/deployments`}
-					variant="outline"
-				>
-					<IconSvg size="lg" src={StatsIcon} />
-
-					{t("topbar.buttons.stats")}
-				</Button>
-
-				<DropdownButton
-					className="font-semibold text-white"
-					contentMenu={
-						<Button
-							className="whitespace-nowrap px-4 py-2 font-semibold text-white"
-							onClick={handleOpenModalDeletePrject}
-							variant="outline"
-						>
-							<IconSvg className="fill-white" size="md" src={TrashIcon} />
-
-							{t("topbar.buttons.delete")}
-						</Button>
-					}
-				>
-					<Button className="h-full px-4 text-white hover:bg-gray-700" variant="outline">
-						<IconSvg size="lg" src={MoreIcon} />
-
-						{t("more", { ns: "buttons" })}
-					</Button>
-				</DropdownButton>
-			</div>
-
-			<DeleteProjectModal onDelete={handleDeleteProject} />
+			<ProjectTopbarButtons />
 		</div>
 	);
 };
