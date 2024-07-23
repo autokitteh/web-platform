95222a84e4b736de19a54531a9d8ec6123a92109
J1za <workinvs@gmail.com>
2024-07-23 10:55:53 +0300

    fix: delete unused file

--- src/interfaces/store/codeAssetsStore.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface CodeAssetsStore {
-	content?: string;
-	name?: string;
-	setCodeAsset: (content: File | string) => void;
-}
--- src/interfaces/store/index.ts
+++ src/interfaces/store/index.ts
@@ -1,5 +1,4 @@
 export type { Toast, ToastStore } from "@interfaces/components/toast.interface";
-export type { CodeAssetsStore } from "@interfaces/store/codeAssetsStore.interface.ts";
 export type { LoggerStore } from "@interfaces/store/loggerStore.interface";
 export type { ModalStore } from "@interfaces/store/modalStore.interface";
 export type { ProjectStore } from "@interfaces/store/projectStore.interface";
