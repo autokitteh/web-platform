a3d1ba27ffadc29b3a587b8557a0ddfc8d5e26fb
Ronen Mars <ronen@autokitteh.com>
2024-07-30 14:38:24 +0300

    fix: husky on commit (#235)

--- lint-staged.config.cjs
+++
@@ -1,6 +0,0 @@
-/* eslint-disable unicorn/filename-case */
-/* eslint-disable @typescript-eslint/naming-convention */
-// eslint-disable-next-line no-undef
-module.exports = {
-	"**/*.{js,jsx,ts,tsx}": () => ["npm run type-check", "npm run lint"],
-};
