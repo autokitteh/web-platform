6fdc8da1a9fd5b10a6730e349e35bc57fce204ca
Ronen Mars <ronen@autokitteh.com>
2024-08-05 15:20:59 +0300

    feat: logout cookie

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,6 +1,26 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import Cookies from "js-cookie";
 
-import { apiBaseUrl, apiRequestTimeout } from "@constants";
+import { apiAuthCookieName, apiBaseUrl, apiRequestTimeout } from "@constants";
+
+const addResponseInterceptor = (http: AxiosInstance) => {
+	http.interceptors.response.use(
+		function (response: AxiosResponse) {
+			return response;
+		},
+		function (error: AxiosError) {
+			const status = error?.response?.status || 0;
+			if (status === 401) {
+				Cookies.remove(apiAuthCookieName);
+
+				localStorage.clear();
+				window.location.reload();
+			}
+
+			return Promise.reject(error);
+		}
+	);
+};
 
 const createAxiosInstance = (baseAddress: string, withCredentials = false) =>
 	axios.create({
@@ -18,5 +38,5 @@ const httpClient = createAxiosInstance(apiBaseUrl, import.meta.env.VITE_AUTH_ENA
 // Axios instance for local domain requests (same domain as the app)
 const localDomainHttpClient = createAxiosInstance("/");
 
-export const HttpService = httpClient;
+export const HttpService = addResponseInterceptor(httpClient);
 export const LocalDomainHttpService = localDomainHttpClient;
