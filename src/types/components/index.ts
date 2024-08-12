9a6b6d2805a81b731c072a2997c66cacdeb9fde2
J1za <workinvs@gmail.com>
2024-08-12 12:22:27 +0300

    refactor: remove unused type

--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -1,5 +1,5 @@
 export type { ButtonType } from "@type/components/button.type";
-export type { IntegrationType, IntegrationGoogleType } from "@type/components/connection.type";
+export type { IntegrationType } from "@type/components/connection.type";
 export type { InputType } from "@type/components/input.type";
 export type { SortDirection } from "@type/components/table.type";
 export type { ToastType } from "@type/components/toast.type";
