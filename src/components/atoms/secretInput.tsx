795160ab44d5da29fa74a53e255e6c569b5a1beb
Ronen Mars <ronen@autokitteh.com>
2024-08-12 14:08:26 +0300

    refactor: edit github - secret input fix

--- src/components/atoms/secretInput.tsx
+++ src/components/atoms/secretInput.tsx
@@ -119,7 +119,8 @@ export const SecretInput = forwardRef<HTMLInputElement, SecretInputProps>((props
 		{ "-top-2 left-3 px-1 text-xs text-white before:bg-gray-950": isFocused || hasValue },
 		{ "text-gray-900": variant === InputVariant.light },
 		{ "-top-2 left-3 px-1 text-xs before:bg-white": (isFocused || hasValue) && variant === InputVariant.light },
-		{ hidden: !isFocused && resetOnFirstFocus && isFirstFocus }
+		{ "text-black": variant === InputVariant.light },
+		{ "left-3 top-0 px-1 text-xs": !isFocused && resetOnFirstFocus && isFirstFocus }
 	);
 
 	const borderOverlayLabelClass = cn("absolute left-0 top-1/2 z-0 h-0.5 w-full -translate-y-1/2 bg-black", {
