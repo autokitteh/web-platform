c54f9ccd7e7d68b08e81ff748a7c25f08ef9690f
Ronen Mars <ronen@autokitteh.com>
2024-08-14 15:40:16 +0300

    refactor: optimize connections field extraction code

--- src/hooks/utils/filterConnectionsFormFields.ts
+++ src/hooks/utils/filterConnectionsFormFields.ts
@@ -1,28 +1,30 @@
 import { FieldValues } from "react-hook-form";
-import type { ZodSchema, z } from "zod";
+import { ZodSchema, z } from "zod";
 
 import { Integrations } from "@src/enums/components";
 
-export function getZodSchemaFieldsShallow(schema: ZodSchema) {
-	const fields: string[] = [];
-	const proxy = new Proxy(fields, {
-		get(_, key) {
-			if (key === "then" || typeof key !== "string") {
-				return;
-			}
-			fields.push(key);
-		},
-	});
-	schema.safeParse(proxy);
+export const getZodKeys = <T extends z.ZodTypeAny>(schema: T): string[] => {
+	if (schema === null || schema === undefined) return [];
+	if (schema instanceof z.ZodNullable || schema instanceof z.ZodOptional) return getZodKeys(schema.unwrap());
+	if (schema instanceof z.ZodArray) return getZodKeys(schema.element);
+	if (schema instanceof z.ZodObject) {
+		const entries = Object.entries(schema.shape);
 
-	return fields;
-}
+		return entries.flatMap(([key, value]) => {
+			const nested = value instanceof z.ZodType ? getZodKeys(value).map((subKey) => `${key}.${subKey}`) : [];
+
+			return nested.length ? nested : key;
+		});
+	}
+
+	return [];
+};
 
 export const filterConnectionValues = (
 	connectionData: FieldValues,
-	validationSchema: z.ZodTypeAny
+	validationSchema: ZodSchema
 ): Partial<Record<keyof typeof Integrations, any>> => {
-	const connectionKeys = getZodSchemaFieldsShallow(validationSchema);
+	const connectionKeys = getZodKeys(validationSchema);
 	const filteredValues: Partial<Record<keyof typeof Integrations, any>> = {};
 
 	connectionKeys?.forEach((field) => {
