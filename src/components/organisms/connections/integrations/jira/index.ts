7e4fba918405a451604a4ba447c6f44374b24ff0
J1za <workinvs@gmail.com>
2024-08-20 22:08:20 +0300

    feat: add jira edit form component

--- src/components/organisms/connections/integrations/jira/index.ts
+++ src/components/organisms/connections/integrations/jira/index.ts
@@ -1,1 +1,2 @@
 export { JiraIntegrationAddForm } from "@components/organisms/connections/integrations/jira/add";
+export { JiraIntegrationEditForm } from "@components/organisms/connections/integrations/jira/edit";
--- src/constants/connections/editComponentsMapping.constants.ts
+++ src/constants/connections/editComponentsMapping.constants.ts
@@ -2,8 +2,10 @@ import { Integrations } from "@src/enums/components";
 
 import { DiscordIntegrationEditForm } from "@components/organisms/connections/integrations/discord";
 import { GithubIntegrationEditForm } from "@components/organisms/connections/integrations/github";
+import { JiraIntegrationEditForm } from "@components/organisms/connections/integrations/jira";
 
 export const integrationToEditComponent: Partial<Record<Integrations, React.ComponentType<any>>> = {
 	[Integrations.github]: GithubIntegrationEditForm,
 	[Integrations.discord]: DiscordIntegrationEditForm,
+	[Integrations.jira]: JiraIntegrationEditForm,
 };
