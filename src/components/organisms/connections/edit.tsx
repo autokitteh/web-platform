1e8b8f8c8e5c700c31df9d4ba0b22324a7d5062a
Ronen Mars <ronen@autokitteh.com>
2024-08-21 10:49:23 +0300

    feat: fix google add functionality

--- src/components/organisms/connections/edit.tsx
+++ src/components/organisms/connections/edit.tsx
@@ -31,7 +31,7 @@ export const EditConnection = () => {
 	}, [connectionId]);
 
 	const SelectedIntegrationComponent = selectedIntegration
-		? integrationToEditComponent[selectedIntegration.value as keyof typeof Integrations]
+		? integrationToEditComponent[selectedIntegration.value as Integrations]
 		: null;
 
 	return (
