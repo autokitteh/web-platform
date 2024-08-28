2a744932b3abc2068931506fd834cea097d31c63
J1za <workinvs@gmail.com>
2024-08-28 12:17:49 +0300

    feat: add auth_type field to google edit

--- src/components/organisms/connections/integrations/google/edit.tsx
+++ src/components/organisms/connections/integrations/google/edit.tsx
@@ -15,7 +15,7 @@ export const GoogleIntegrationEditForm = () => {
 	const { t } = useTranslation("integrations");
 
 	const { connectionType, errors, handleSubmit, isLoading, onSubmitEdit, register, setValue } = useConnectionForm(
-		{ json: "" },
+		{ json: "", auth_type: "json" },
 		googleIntegrationSchema,
 		"edit"
 	);
