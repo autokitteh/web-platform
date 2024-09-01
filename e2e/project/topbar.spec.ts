bb116308e31c05bce72bd89026eccb7494de58cd
Ronen Mars <ronen@autokitteh.com>
2024-09-01 13:13:04 +0300

    test: fix e2e tests with the recent change

--- e2e/project/topbar.spec.ts
+++ e2e/project/topbar.spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from "@e2e/fixtures";
 
 test.beforeEach(async ({ dashboardPage, page }) => {
 	await dashboardPage.createProjectFromMenu();
-	await expect(page.getByRole("link", { name: "View project stats" })).toBeVisible();
-	await expect(page.getByRole("link", { name: "View project stats" })).not.toBeDisabled();
+	await expect(page.getByRole("link", { name: "Deployments" })).toBeVisible();
+	await expect(page.getByRole("link", { name: "Deployments" })).not.toBeDisabled();
 });
 
 test.describe("Project Topbar Suite", () => {
 	test("Changed deployments topbar", async ({ page }) => {
-		await page.getByRole("link", { name: "View project stats" }).click();
+		await page.getByRole("link", { name: "Deployments" }).click();
 		await expect(page.getByRole("link", { name: "Go to project" })).toBeVisible();
 	});
 });
