7cb66e6943d3dea0502da30a2b583e8b490c9bd9
Ronen Mars <ronen@autokitteh.com>
2024-09-03 12:25:16 +0300

    feat: session viewer

--- src/components/organisms/deployments/sessions/tabs/activities/list.tsx
+++ src/components/organisms/deployments/sessions/tabs/activities/list.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 import JsonView from "@uiw/react-json-view";
 import { githubDarkTheme } from "@uiw/react-json-view/githubDark";
+import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import ReactTimeAgo from "react-time-ago";
 
@@ -19,6 +20,7 @@ export const SessionActivitiesList = () => {
 	const { sessionId } = useParams();
 	const [activities, setActivities] = React.useState<any[]>([]);
 	const addToast = useToastStore((state) => state.addToast);
+	const { t } = useTranslation("deployments", { keyPrefix: "activities" });
 
 	const fetchActivities = async () => {
 		const { data: sessionExecution, error } = await SessionsService.getLogRecordsBySessionId(sessionId!);
@@ -40,107 +42,111 @@ export const SessionActivitiesList = () => {
 
 	return (
 		<div className="h-full overflow-y-scroll pt-5">
-			{(activities || []).map((activity, index) => (
-				<div key={activity.key}>
-					{index !== 0 ? (
-						<div className="flex w-full items-center justify-center pt-2">
-							<IconSvg className="fill-white" size="sm" src={ArrowUpFaIcon} />
-						</div>
-					) : null}
+			{activities?.length ? (
+				activities.map((activity, index) => (
+					<div key={activity.key}>
+						{index !== 0 ? (
+							<div className="flex w-full items-center justify-center pt-2">
+								<IconSvg className="fill-white" size="sm" src={ArrowUpFaIcon} />
+							</div>
+						) : null}
 
-					<div className="mt-2">
-						<Accordion
-							className="mt-2 rounded-md bg-gray-1000 px-2 py-1"
-							title={
-								<div className="flex w-full gap-3">
-									<div className="mt-0.5">{activity.startTime.toTimeString().split(" ")[0]}</div>
+						<div className="mt-2">
+							<Accordion
+								className="mt-2 rounded-md bg-gray-1000 px-2 py-1"
+								title={
+									<div className="flex w-full gap-3">
+										<div className="mt-0.5">{activity.startTime.toTimeString().split(" ")[0]}</div>
 
-									<div>
-										<div className="text-left font-bold"> {activity.functionName}</div>
+										<div>
+											<div className="text-left font-bold"> {activity.functionName}</div>
 
-										<div className="flex items-center gap-1">
-											<span>Status: </span>
+											<div className="flex items-center gap-1">
+												<span>Status: </span>
 
-											{activity.status === "error" || activity.status === "completed" ? (
-												<>
-													<ActivityStatus activityState={activity.status} /> -
-													<ReactTimeAgo date={activity.endTime} locale="en-US" />
-												</>
-											) : (
-												<>
-													<ActivityStatus activityState={activity.status} /> -
-													<ReactTimeAgo date={activity.startTime} locale="en-US" />
-												</>
-											)}
+												{activity.status === "error" || activity.status === "completed" ? (
+													<>
+														<ActivityStatus activityState={activity.status} /> -
+														<ReactTimeAgo date={activity.endTime} locale="en-US" />
+													</>
+												) : (
+													<>
+														<ActivityStatus activityState={activity.status} /> -
+														<ReactTimeAgo date={activity.startTime} locale="en-US" />
+													</>
+												)}
+											</div>
 										</div>
 									</div>
+								}
+							>
+								<div className="mx-7">
+									{activity.args.length ? (
+										<>
+											<div className="font-bold">Arguments:</div>
+											<Table>
+												<THead>
+													<Tr>
+														<Th>Value</Th>
+													</Tr>
+												</THead>
+
+												<TBody>
+													{activity.args.map((argument: string) => (
+														<Tr key={argument}>
+															<Td>{argument}</Td>
+														</Tr>
+													))}
+												</TBody>
+											</Table>
+										</>
+									) : null}
+
+									{Object.keys(activity.kwargs).length ? (
+										<>
+											<div className="mt-4 font-bold">KW Arguments:</div>
+											<Table>
+												<THead>
+													<Tr>
+														<Th>Key</Th>
+
+														<Th>Value</Th>
+													</Tr>
+												</THead>
+
+												<TBody>
+													{Object.keys(activity.kwargs).map((argumentKey) => (
+														<Tr key={argumentKey}>
+															<Td>{argumentKey}</Td>
+
+															<Td>{activity.kwargs[argumentKey]}</Td>
+														</Tr>
+													))}
+												</TBody>
+											</Table>
+										</>
+									) : null}
+
+									{activity.returnValue ? (
+										<Accordion
+											className="mt-2"
+											title={<div className="font-bold underline">Returned Value</div>}
+										>
+											<JsonView
+												className="scrollbar max-h-72 overflow-auto"
+												style={githubDarkTheme}
+												value={activity.returnValue}
+											/>
+										</Accordion>
+									) : null}
 								</div>
-							}
-						>
-							<div className="mx-7">
-								{activity.args.length ? (
-									<>
-										<div className="font-bold">Arguments:</div>
-										<Table>
-											<THead>
-												<Tr>
-													<Th>Value</Th>
-												</Tr>
-											</THead>
-
-											<TBody>
-												{activity.args.map((argument: string) => (
-													<Tr key={argument}>
-														<Td>{argument}</Td>
-													</Tr>
-												))}
-											</TBody>
-										</Table>
-									</>
-								) : null}
-
-								{Object.keys(activity.kwargs).length ? (
-									<>
-										<div className="mt-4 font-bold">KW Arguments:</div>
-										<Table>
-											<THead>
-												<Tr>
-													<Th>Key</Th>
-
-													<Th>Value</Th>
-												</Tr>
-											</THead>
-
-											<TBody>
-												{Object.keys(activity.kwargs).map((argumentKey) => (
-													<Tr key={argumentKey}>
-														<Td>{argumentKey}</Td>
-
-														<Td>{activity.kwargs[argumentKey]}</Td>
-													</Tr>
-												))}
-											</TBody>
-										</Table>
-									</>
-								) : null}
-
-								{activity.returnValue ? (
-									<Accordion
-										className="mt-2"
-										title={<div className="font-bold underline">Returned Value</div>}
-									>
-										<JsonView
-											className="scrollbar max-h-72 overflow-auto"
-											style={githubDarkTheme}
-											value={activity.returnValue}
-										/>
-									</Accordion>
-								) : null}
-							</div>
-						</Accordion>
+							</Accordion>
+						</div>
 					</div>
-				</div>
-			))}
+				))
+			) : (
+				<div className="mt-10 text-center text-xl font-semibold">{t("notData")}</div>
+			)}
 		</div>
 	);
 };
