62bf45de7cec1d813300970fc35c833f6aa2cd9a
Ronen Mars <ronen@autokitteh.com>
2024-09-05 18:15:33 +0300

    fix: refactor connection add form and fix google oauth

--- src/utilities/stripGoogleConnectionName.utils.ts
+++ src/utilities/stripGoogleConnectionName.utils.ts
@@ -1,9 +1,13 @@
 import { Integrations } from "@src/enums/components";
 
 export const stripGoogleConnectionName = (connectionName: string) => {
-	if (connectionName === Integrations.google) return connectionName;
+	if (!connectionName) return "";
 
-	if (!connectionName.includes(Integrations.google) || connectionName.includes(Integrations.googlegemini)) {
+	if (
+		!connectionName.includes(Integrations.google) ||
+		connectionName.includes(Integrations.googlegemini) ||
+		connectionName === Integrations.google
+	) {
 		return connectionName;
 	}
 
