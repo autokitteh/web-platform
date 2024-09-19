8e159028a6997d6dbcab6df339ea41e8cf284beb
Ronen Mars <ronen@autokitteh.com>
2024-09-19 18:35:45 +0300

    fix: seconds bug

--- src/utilities/convertTimestampToDate.utils.ts
+++ src/utilities/convertTimestampToDate.utils.ts
@@ -8,7 +8,7 @@ import { ProtoTimestamp } from "@type/utilities";
 export const convertTimestampToDate = (timestamp: unknown): Date => {
 	const timestampConverted = timestamp as ProtoTimestamp;
 
-	if (timestampConverted?.seconds) {
+	if (!timestampConverted?.seconds) {
 		return new Date();
 	}
 	const milliseconds = timestampConverted.seconds * BigInt(1000);
