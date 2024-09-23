a2cf7f03a8a22833f136d7d76b341183c08674eb
Ronen Mars <ronen@autokitteh.com>
2024-09-23 08:54:29 +0300

    refactor: add missing lagnuages to add new code modal

--- src/constants/monacoLanguages.constants.ts
+++ src/constants/monacoLanguages.constants.ts
@@ -1,6 +1,8 @@
-export const defalutFileExtension = ".py";
+export const defalutFileExtension = ".star";
 
 export const monacoLanguages = {
+	".json": "json",
+	".jsonc": "jsonc",
 	".py": "python",
 	".star": "starlark",
 	".txt": "text",
