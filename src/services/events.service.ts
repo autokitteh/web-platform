87f0fab0435242fa4b6ac98c31b035cfb109a80a
Ronen Mars <ronen@autokitteh.com>
2024-09-29 08:15:28 +0300

    fix: 'invalid prefix' error

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 
 import { eventsClient } from "@api/grpc/clients.grpc.api";
 import { namespaces } from "@constants";
-import { LoggerService } from "@services";
+import { ConnectionService, LoggerService, TriggersService } from "@services";
 import { convertEventProtoToModel } from "@src/models/event.model";
 import { Event } from "@src/types/models/event.type";
 import { ServiceResponse } from "@type";
@@ -17,6 +17,74 @@ export class EventsService {
 			const eventConverted = convertEventProtoToModel(event);
 
 			return { data: eventConverted, error: undefined };
+		} catch (error) {
+			LoggerService.error(
+				namespaces.eventsService,
+				i18n.t("fetchFailedForEvent", {
+					eventId,
+					error: (error as Error).message,
+					ns: "services",
+				})
+			);
+
+			return { data: undefined, error };
+		}
+	}
+
+	static async getDestinationName(eventId: string): Promise<ServiceResponse<string>> {
+		try {
+			const { event } = await eventsClient.get({ eventId });
+			if (!event) {
+				return { data: undefined, error: undefined };
+			}
+			const eventConverted = convertEventProtoToModel(event);
+
+			if (!eventConverted.destinationId) {
+				const errorMessage = i18n.t("eventNoDestinationId", {
+					eventId,
+					ns: "services",
+				});
+
+				LoggerService.error(namespaces.eventsService, errorMessage);
+
+				return { data: "", error: undefined };
+			}
+
+			if (eventConverted.destinationId.startsWith("trg_")) {
+				const trigger = await TriggersService.get(eventConverted.destinationId);
+				if (trigger.error) {
+					const errorMessage = i18n.t("eventNoDestinationIdExtended", {
+						eventId,
+						ns: "services",
+						error: trigger.error,
+					});
+
+					return { data: "", error: errorMessage };
+				} else return { data: trigger.data?.connectionName, error: undefined };
+			}
+
+			if (eventConverted.destinationId.startsWith("con_")) {
+				const connection = await ConnectionService.get(eventConverted.destinationId);
+				if (connection.error) {
+					const errorMessage = i18n.t("eventNoDestinationIdExtended", {
+						eventId,
+						ns: "services",
+						error: connection.error,
+					});
+
+					LoggerService.error(namespaces.eventsService, errorMessage);
+
+					return { data: "", error: connection.error };
+				} else return { data: connection.data?.name, error: undefined };
+			}
+			const errorMessage = i18n.t("eventUnknownDestinationId", {
+				eventId,
+				ns: "services",
+			});
+
+			LoggerService.error(namespaces.eventsService, errorMessage);
+
+			return { data: "", error: errorMessage };
 		} catch (error) {
 			LoggerService.error(
 				namespaces.eventsService,
