b141b9aa9f1bcf4935f97b64401794ad2cee4122
Ronen Mars <ronen@autokitteh.com>
2024-09-29 08:01:16 +0300

    feat: convert wrapped objects from the backend

--- src/utilities/convertWrappedJson.utils.ts
+++ src/utilities/convertWrappedJson.utils.ts
@@ -1,18 +1,29 @@
-import { WrappedJsonObject } from "@src/interfaces/utilities";
+import i18n from "i18next";
 
-export const parseNestedJson = (object: WrappedJsonObject): Record<string, any> => {
+import { LoggerService } from "@services/index";
+import { Value } from "@src/autokitteh/proto/gen/ts/autokitteh/values/v1/values_pb";
+import { namespaces } from "@src/constants";
+import { WrappedJsonObject } from "@src/interfaces/utilities";
+import { convertValue } from "@src/models";
+
+export const parseNestedJson = (object: WrappedJsonObject): Record<string, string> => {
 	if (!object) return {};
 	const result: Record<string, any> = {};
 
 	for (const key in object) {
 		if (Object.prototype.hasOwnProperty.call(object, key)) {
-			const value = object[key];
+			const value = convertValue(object[key] as Value);
 			if (value && typeof value === "object" && "string" in value && value && value.string) {
 				try {
 					result[key] = JSON.parse(value.string);
 				} catch (error) {
-					console.error(`Error parsing JSON for key "${key}":`, error);
-					result[key] = value.string;
+					const errorMessage = i18n.t("convertWrappedJsonError", {
+						error: (error as Error).message,
+						ns: "errors",
+						key,
+					});
+					LoggerService.error(namespaces.sessionsService, errorMessage);
+					result[key] = errorMessage;
 				}
 			} else {
 				result[key] = value;
