f0acd3012fe42aaad1be748785cde3c648c339f7
Ronen Mars <ronen@autokitteh.com>
2024-09-29 14:44:15 +0300

    fix: logs fetch (#408)

--- src/hooks/useVirtualizedList.tsx
+++ src/hooks/useVirtualizedList.tsx
@@ -59,8 +59,7 @@ export function useVirtualizedList<T extends SessionOutput | SessionActivity>(
 			return;
 		}
 		await loadLogs(sessionId, pageSize * 2);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [sessionId, shouldLoadMore, loadLogs]);
+	}, [sessionId, shouldLoadMore, loadLogs, pageSize]);
 
 	const handleScroll = useCallback(({ scrollTop }: { scrollTop: number }): void => {
 		if (scrollTop !== 0) setScrollPosition(scrollTop);
@@ -82,12 +81,21 @@ export function useVirtualizedList<T extends SessionOutput | SessionActivity>(
 
 		if (!session) {
 			reset(sessionId);
-			const newPageSize = calculatePageSize();
-			setPageSize(newPageSize);
+			if (!pageSize) {
+				setPageSize(calculatePageSize());
+
+				return;
+			}
 			loadMoreRows();
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [sessionId, type, session, reset, loadMoreRows, itemHeight]);
+
+	useEffect(() => {
+		if (!pageSize) return;
+		loadMoreRows();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [pageSize]);
 
 	useEffect(() => {
 		return () => {
