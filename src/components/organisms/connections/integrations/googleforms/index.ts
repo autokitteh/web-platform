cb060f447a2413f1e58cf9a8b1a5e9aecc213cd1
Ronen Mars <ronen@autokitteh.com>
2024-09-29 16:54:19 +0300

    feat: add optional form id field to google forms connection form

---
+++ src/components/organisms/connections/integrations/googleforms/index.ts
@@ -0,0 +1,2 @@
+export { GoogleFormsIntegrationAddForm } from "@components/organisms/connections/integrations/googleforms/add";
+export { GoogleFormsIntegrationEditForm } from "@components/organisms/connections/integrations/googleforms/edit";
