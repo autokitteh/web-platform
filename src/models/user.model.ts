5c832c9e67064c55ab7e5f49409a55ee5c44fb57
Ronen Mars <ronen@autokitteh.com>
2024-09-30 17:04:55 +0300

    refactor: conditional param in user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,7 +1,7 @@
 import { User as ProtoUser } from "@ak-proto-ts/users/v1/user_pb";
 import { User } from "@type/models";
 
-export function convertUserProtoToModel(protoUser: ProtoUser | undefined): User {
+export function convertUserProtoToModel(protoUser?: ProtoUser): User {
 	return {
 		email: protoUser?.data?.email || "",
 		name: protoUser?.data?.name || "",
