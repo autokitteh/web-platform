1e41732b2a1edca1aaf6f7bb4bf149ea9d7cb8ba
Ronen Mars <ronen@autokitteh.com>
2024-09-30 08:29:24 +0300

    refactor: deployments table adjustments

--- src/components/organisms/deployments/sessionStats.tsx
+++ src/components/organisms/deployments/sessionStats.tsx
@@ -1,20 +1,48 @@
 import React from "react";
 
+import { useTranslation } from "react-i18next";
+
 import { SessionStateType } from "@enums";
 import { DeploymentSession } from "@type/models";
 import { cn } from "@utilities";
 
 export const DeploymentSessionStats = ({ sessionStats }: { sessionStats?: DeploymentSession[] }) => {
+	const { t } = useTranslation("deployments", { keyPrefix: "sessionStats" });
 	const countStyle = (state?: SessionStateType) =>
 		cn("2xl:w-22 inline-block w-14 border-0 p-0 text-sm font-medium", {
-			"hidden": SessionStateType.created === state,
-			"text-gray-650": SessionStateType.stopped === state,
-			"text-green-800": SessionStateType.completed === state,
-			"text-red": SessionStateType.error === state,
+			"text-white": state === SessionStateType.created,
+			"text-gray-650": state === SessionStateType.stopped,
+			"text-green-800": state === SessionStateType.completed,
+			"text-red": state === SessionStateType.error,
 		});
 
-	return sessionStats?.map(({ count, state }) => (
-		<span aria-label={state} className={countStyle(state)} key={state} role="status" title={state}>
+	const sessionStatsOrdered = [
+		sessionStats?.find(({ state }) => state === SessionStateType.created) || {
+			count: 0,
+			state: SessionStateType.created,
+		},
+		sessionStats?.find(({ state }) => state === SessionStateType.stopped) || {
+			count: 0,
+			state: SessionStateType.stopped,
+		},
+		sessionStats?.find(({ state }) => state === SessionStateType.completed) || {
+			count: 0,
+			state: SessionStateType.completed,
+		},
+		sessionStats?.find(({ state }) => state === SessionStateType.error) || {
+			count: 0,
+			state: SessionStateType.error,
+		},
+	];
+
+	return sessionStatsOrdered.map(({ count, state }) => (
+		<span
+			aria-label={state}
+			className={countStyle(state)}
+			key={state}
+			role="status"
+			title={t(state?.toString() || "")}
+		>
 			{count}
 		</span>
 	));
