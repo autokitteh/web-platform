389cafc9276b3f9d788ea3bc267b2ea772479ab1
Ronen Mars <ronen@autokitteh.com>
2024-09-30 14:28:12 +0300

    refactor: design the empty state button so it wont hide the rest of the screen, it fixes e2e

--- src/components/molecules/emptyTableAddButton.tsx
+++ src/components/molecules/emptyTableAddButton.tsx
@@ -7,27 +7,26 @@ import { Button } from "@components/atoms";
 import { PlusCircle } from "@assets/image";
 
 export const EmptyTableAddButton = ({ buttonText, onClick }: { buttonText: string; onClick: () => void }) => {
-	const styleFrame = cn(
-		"absolute top-0 z-10 flex h-full w-full items-center justify-center rounded-lg duration-300",
-		"pointer-events-none select-none opacity-1 pointer-events-auto"
-	);
+	const styleFrame = cn("relative rounded-lg");
 
 	return (
-		<Button onClick={onClick}>
-			<div className={styleFrame}>
-				<div className="flex flex-col items-center gap-2.5">
-					<label
-						className={cn(
-							"group flex cursor-pointer flex-col items-center gap-2.5",
-							"text-center text-lg font-bold uppercase text-white"
-						)}
-					>
-						<PlusCircle className="stroke-gray-750 duration-300 group-hover:stroke-green-800" />
+		<div className="flex size-full items-center justify-center">
+			<Button className="hover:bg-transparent" onClick={onClick}>
+				<div className={styleFrame}>
+					<div className="flex flex-col items-center gap-2.5">
+						<label
+							className={cn(
+								"group flex cursor-pointer flex-col items-center gap-2.5",
+								"text-center text-lg font-bold uppercase text-white"
+							)}
+						>
+							<PlusCircle className="stroke-gray-750 duration-300 group-hover:stroke-green-800" />
 
-						{buttonText}
-					</label>
+							{buttonText}
+						</label>
+					</div>
 				</div>
-			</div>
-		</Button>
+			</Button>
+		</div>
 	);
 };
