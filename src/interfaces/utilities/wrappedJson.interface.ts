29ea7f572232b343790fe352e85387af3fbe2d1d
Ronen Mars <ronen@autokitteh.com>
2024-10-01 13:43:00 +0300

    refactor: wrapped json interface code optimization

--- src/interfaces/utilities/wrappedJson.interface.ts
+++ src/interfaces/utilities/wrappedJson.interface.ts
@@ -1,26 +1,26 @@
 export interface WrappedJsonObject {
-	[key: string]: {
-		boolean?: boolean;
-		bytes?: Uint8Array;
-		dict?: { items: Array<{ k: any; v: any }> };
-		duration?: { nanos: number; seconds: number };
-		float?: number;
-		function?: {
+	[key: string]: Partial<{
+		boolean: boolean;
+		bytes: Uint8Array;
+		dict: { items: Array<{ k: any; v: any }> };
+		duration: { nanos: number; seconds: number };
+		float: number;
+		function: {
 			data: Uint8Array;
 			// eslint-disable-next-line @liferay/no-abbreviations
-			desc?: any;
+			desc: any;
 			executorId: string;
 			flags: string[];
 			name: string;
 		};
-		integer?: bigint;
-		list?: { vs: any[] };
-		module?: { members: { [key: string]: any }; name: string };
-		nothing?: null;
-		set?: { vs: any[] };
-		string?: string;
-		struct?: { ctor: any; fields: { [key: string]: any } };
-		symbol?: { name: string };
-		time?: { nanos: number; seconds: number };
-	};
+		integer: bigint;
+		list: { vs: any[] };
+		module: { members: { [key: string]: any }; name: string };
+		nothing: null;
+		set: { vs: any[] };
+		string: string;
+		struct: { ctor: any; fields: { [key: string]: any } };
+		symbol: { name: string };
+		time: { nanos: number; seconds: number };
+	}>;
 }
