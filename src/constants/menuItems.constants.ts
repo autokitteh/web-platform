fea3e417989320f1b394d2801e04c6e8c470c6a4
Ronen Mars <ronen@autokitteh.com>
2024-10-06 07:38:27 +0300

    refactor: remove inactive settings menu items

--- src/constants/menuItems.constants.ts
+++ src/constants/menuItems.constants.ts
@@ -1,9 +1,3 @@
-import { BellIcon, ReceiptIcon, SecurityIcon, SlidersIcon, UserIcon } from "@assets/image/icons";
+import { SecurityIcon } from "@assets/image/icons";
 
-export const menuItems = [
-	{ icon: UserIcon, label: "menu.myProfile" },
-	{ icon: SecurityIcon, label: "menu.security" },
-	{ icon: BellIcon, label: "menu.notifications" },
-	{ icon: SlidersIcon, label: "menu.advanced" },
-	{ icon: ReceiptIcon, label: "menu.billing" },
-];
+export const menuItems = [{ icon: SecurityIcon, label: "menu.security" }];
