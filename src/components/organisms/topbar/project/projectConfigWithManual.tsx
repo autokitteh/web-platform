8eaf2064f92b37631e1213fe9207fa2b2639a411
J1za <workinvs@gmail.com>
2024-10-08 18:48:56 +0300

    refactor: topbar configs

--- src/components/organisms/topbar/project/projectConfigWithManual.tsx
+++ src/components/organisms/topbar/project/projectConfigWithManual.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { useParams } from "react-router-dom";
 
 import { useCacheStore } from "@src/store";
-
-import { useFileOperations } from "@hooks";
 
 import {
 	ManualRunButtons,
@@ -15,17 +13,7 @@ import {
 
 export const ProjectConfigWithManualTopbar = () => {
 	const { projectId } = useParams();
-	const { openProjectId, setOpenProjectId } = useFileOperations(projectId!);
 	const { fetchDeployments } = useCacheStore();
-
-	useEffect(() => {
-		if (!projectId) return;
-
-		if (projectId !== openProjectId) {
-			setOpenProjectId(projectId);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [projectId]);
 
 	return (
 		<div className="flex justify-between rounded-b-xl bg-gray-1250 pl-7 pr-3">
--- src/components/organisms/topbar/project/projectConfigWithoutButtons.tsx
+++ src/components/organisms/topbar/project/projectConfigWithoutButtons.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { useParams } from "react-router-dom";
 
 import { useCacheStore } from "@src/store";
-
-import { useFileOperations } from "@hooks";
 
 import {
 	ManualRunSettingsDrawer,
@@ -14,17 +12,7 @@ import {
 
 export const ProjectConfigWithoutButtonsTopbar = () => {
 	const { projectId } = useParams();
-	const { openProjectId, setOpenProjectId } = useFileOperations(projectId!);
 	const { fetchDeployments } = useCacheStore();
-
-	useEffect(() => {
-		if (!projectId) return;
-
-		if (projectId !== openProjectId) {
-			setOpenProjectId(projectId);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [projectId]);
 
 	return (
 		<div className="flex justify-between rounded-b-xl bg-gray-1250 pl-7 pr-3">
