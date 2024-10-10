7682bf93e1e00facba2e3d9a7b51b8f6e974f3e1
Ronen Mars <ronen@autokitteh.com>
2024-10-10 08:19:23 +0300

    refactor: add project tabs divider (#457)

--- src/components/pages/project.tsx
+++ src/components/pages/project.tsx
@@ -5,7 +5,7 @@ import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 
 import { defaultProjectTab, projectTabs } from "@constants/project.constants";
 import { useProjectStore, useProjectValidationStore } from "@src/store";
-import { calculatePathDepth } from "@utilities";
+import { calculatePathDepth, cn } from "@utilities";
 
 import { IconSvg, PageTitle, Tab } from "@components/atoms";
 import { SplitFrame } from "@components/organisms";
@@ -33,6 +33,7 @@ export const Project = () => {
 		loadProject(projectId!);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [projectId]);
+
 	const activeTab = useMemo(() => {
 		const pathParts = location.pathname.split("/").filter(Boolean);
 
@@ -52,41 +53,42 @@ export const Project = () => {
 				{displayTabs ? (
 					<div className="flex h-full flex-1 flex-col">
 						<div className="sticky -top-8 z-20 -mt-5 bg-gray-1100 pb-0 pt-3">
-							<div className="xl:gap-2 scrollbar flex shrink-0 select-none items-center gap-1 overflow-x-auto overflow-y-hidden whitespace-nowrap py-2 pb-5 2xl:gap-4 3xl:gap-5">
-								{projectTabs.map((tabKey) => {
+							<div className="scrollbar flex shrink-0 select-none items-center overflow-x-auto overflow-y-hidden whitespace-nowrap pb-5 pt-1">
+								{projectTabs.map((tabKey, index) => {
 									const tabState =
 										projectValidationState[tabKey.value as keyof typeof projectValidationState];
 									const warning = tabState.level === "warning" ? tabState.message : "";
 									const error = tabState.level === "error" ? tabState.message : "";
+									const tabClass = cn("py-1 pr-1", { "ml-2": index !== 0 });
 
 									return (
-										<Tab
-											activeTab={activeTab}
-											ariaLabel={tabState?.message || tabKey.label}
-											key={tabKey.value}
-											onClick={() => goTo(tabKey.value)}
-											title={tabState?.message || tabKey.label}
-											value={tabKey.value}
-										>
-											<div className="flex items-center">
-												<div className="tracking-wide">{tabKey.label}</div>
-
-												{error ? (
-													<div className="mb-0.5 ml-1.5 size-3 rounded-full bg-error" />
-												) : null}
-
-												{warning ? (
-													<div className="relative mb-1.5 ml-1.5 size-3 rounded-full">
-														<IconSvg src={WarningTriangleIcon} />
-													</div>
-												) : null}
-											</div>
-										</Tab>
+										<div className="flex items-center" key={tabKey.value}>
+											{index > 0 ? <div className="mx-3 h-5 w-px bg-gray-700" /> : null}
+											<Tab
+												activeTab={activeTab}
+												ariaLabel={tabState?.message || tabKey.label}
+												className={tabClass}
+												onClick={() => goTo(tabKey.value)}
+												title={tabState?.message || tabKey.label}
+												value={tabKey.value}
+											>
+												<div className="flex items-center">
+													<div className="tracking-wide">{tabKey.label}</div>
+													{error ? (
+														<div className="mb-0.5 ml-2 size-3 rounded-full bg-error" />
+													) : null}
+													{warning ? (
+														<div className="relative mb-1.5 ml-2 size-3 rounded-full">
+															<IconSvg src={WarningTriangleIcon} />
+														</div>
+													) : null}
+												</div>
+											</Tab>
+										</div>
 									);
 								})}
 							</div>
 						</div>
-
 						<div className="h-full">
 							<Outlet />
 						</div>
