d246cd326ddede8b3642770bf4f01a7ae91a7327
Vitaliy Stukalo <workinvs@gmail.com>
2024-10-10 09:48:12 +0300

    feat: cache variables (#432)

--- src/interfaces/store/cacheStore.interface.ts
+++ src/interfaces/store/cacheStore.interface.ts
@@ -1,15 +1,19 @@
-import { Deployment, Trigger } from "@src/types/models";
+import { Deployment, Trigger, Variable } from "@src/types/models";
 
 interface LoadingState {
 	deployments: boolean;
 	triggers: boolean;
+	variables: boolean;
 }
 
 export interface CacheStore {
 	deployments?: Deployment[];
 	triggers: Trigger[];
+	variables: Variable[];
 	loading: LoadingState;
 	currentProjectId?: string;
+	envId?: string;
 	fetchDeployments: (projectId: string, force?: boolean) => Promise<void | Deployment[]>;
 	fetchTriggers: (projectId: string, force?: boolean) => Promise<void | Trigger[]>;
+	fetchVariables: (projectId: string, force?: boolean) => Promise<void | Variable[]>;
 }
