e56e9b084b363fc94fe1656b740c914b7598a5e6
Ronen Mars <ronen@autokitteh.com>
2024-10-13 07:47:26 +0300

    refactor: connections - place oauth in integration type first

--- src/constants/lists/connections/options.constants.ts
+++ src/constants/lists/connections/options.constants.ts
@@ -11,18 +11,18 @@ export const integrationIcons: Record<string, React.ComponentType<React.SVGProps
 );
 
 export const githubIntegrationAuthMethods: SelectOption[] = [
-	{ label: "Personal Access Token (PAT)", value: ConnectionAuthType.Pat },
 	{ label: "OAuth", value: ConnectionAuthType.Oauth },
+	{ label: "Personal Access Token (PAT)", value: ConnectionAuthType.Pat },
 ];
 
 export const selectIntegrationGoogle: SelectOption[] = [
-	{ label: "Service Account (JSON Key)", value: ConnectionAuthType.JsonKey },
 	{ label: "User (OAuth v2)", value: ConnectionAuthType.Oauth },
+	{ label: "Service Account (JSON Key)", value: ConnectionAuthType.JsonKey },
 ];
 
 export const selectIntegrationSlack: SelectOption[] = [
-	{ label: "Socket Mode", value: ConnectionAuthType.Socket },
 	{ label: "OAuth v2", value: ConnectionAuthType.Oauth },
+	{ label: "Socket Mode", value: ConnectionAuthType.Socket },
 ];
 
 export const selectIntegrationAws: SelectOption[] = [
