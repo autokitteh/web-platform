9b5d3437c2e68352881a4018869dd550b844193c
Ronen Mars <ronen@autokitteh.com>
2024-10-22 16:04:25 +0300

    feat: file scanner - optimize

--- templateFilesGeneratorPostScript.cjs
+++ templateFilesGeneratorPostScript.cjs
@@ -6,7 +6,6 @@ const fs = require("fs/promises");
 const prettier = require("prettier");
 
 const CONSTANTS_FILE = "src/constants/dashboard.constants.ts";
-const PRETTIER_CONFIG_FILE = ".prettierrc";
 
 async function refineDashboardConstants() {
     console.log("Starting to refine dashboard.constants.ts...");
@@ -16,15 +15,19 @@ async function refineDashboardConstants() {
 
     try {
         // Run Prettier
-        const prettierConfig = await prettier.resolveConfig(PRETTIER_CONFIG_FILE);
         const formatted = await prettier.format(constantsContent, {
-            ...prettierConfig,
             parser: "typescript",
             filepath: CONSTANTS_FILE,
             printWidth: 120,
             useTabs: true,
             tabWidth: 4,
-            singleQuote: false
+            singleQuote: false,
+            arrowParens: "always",
+            bracketSpacing: true,
+            jsxSingleQuote: false,
+            quoteProps: "consistent",
+            semi: true,
+            trailingComma: "es5",
         });
 
         await fs.writeFile(CONSTANTS_FILE, formatted, "utf-8");
