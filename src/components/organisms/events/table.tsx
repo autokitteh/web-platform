2341c091686634cd41760536f62ea67fdd36c3e1
Ronen Mars <ronen@autokitteh.com>
2024-10-22 14:06:40 +0300

    fix: events viewer

--- src/components/organisms/events/table.tsx
+++ src/components/organisms/events/table.tsx
@@ -1,13 +1,14 @@
-import React, { useCallback, useEffect, useId, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useId, useMemo } from "react";
 
 import moment from "moment";
 import { useTranslation } from "react-i18next";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
-import { AutoSizer } from "react-virtualized";
+import { AutoSizer, List } from "react-virtualized";
 
 import { dateTimeFormat, defaultEventsTableRowHeight } from "@src/constants";
 import { useResize, useSort } from "@src/hooks";
 import { useCacheStore } from "@src/store";
+import { SortConfig } from "@src/types";
 import { BaseEvent } from "@src/types/models";
 import { cn } from "@src/utilities";
 
@@ -15,6 +16,47 @@ import { Frame, Loader, ResizeButton, TBody, THead, Table, Td, Th, Tr } from "@c
 import { SortButton } from "@components/molecules";
 
 import { CatImage } from "@assets/image";
+
+const TableHeader = ({
+	onSort,
+	sortConfig,
+}: {
+	onSort: (key: keyof BaseEvent) => (event: React.MouseEvent | React.KeyboardEvent) => void;
+	sortConfig: SortConfig<BaseEvent>;
+}) => {
+	const { t } = useTranslation("events");
+
+	const renderSortableHeader = useCallback(
+		(columnKey: keyof BaseEvent, columnLabel: string) => {
+			return (
+				<div
+					className="group cursor-pointer font-normal outline-none focus:ring-2 focus:ring-blue-500"
+					onClick={onSort(columnKey)}
+					onKeyDown={onSort(columnKey)}
+					role="button"
+					tabIndex={0}
+				>
+					{columnLabel}
+					<SortButton
+						className="opacity-0 group-hover:opacity-100 group-focus:opacity-100"
+						isActive={columnKey === sortConfig?.key}
+						sortDirection={sortConfig.direction}
+					/>
+				</div>
+			);
+		},
+		[onSort, sortConfig]
+	);
+
+	return (
+		<THead>
+			<Th>
+				<Td>{renderSortableHeader("createdAt", t("table.columns.createdAt"))}</Td>
+				<Td>{renderSortableHeader("eventId", t("table.columns.eventId"))}</Td>
+			</Th>
+		</THead>
+	);
+};
 
 export const EventsTable = () => {
 	const { t } = useTranslation("events");
@@ -28,14 +70,11 @@ export const EventsTable = () => {
 	const [leftSideWidth] = useResize({ direction: "horizontal", initial: 50, max: 90, min: 10, id: resizeId });
 	const { eventId } = useParams();
 	const { items: sortedEvents, requestSort, sortConfig } = useSort<BaseEvent>(events || []);
-
 	const navigate = useNavigate();
-	const [visibleRows, setVisibleRows] = useState({ start: 0, end: 20 });
 
 	useEffect(() => {
 		fetchEvents();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [fetchEvents]);
 
 	const frameClass = useMemo(
 		() =>
@@ -56,106 +95,70 @@ export const EventsTable = () => {
 		[requestSort]
 	);
 
-	const renderSortableHeader = useCallback(
-		(columnKey: keyof BaseEvent, columnLabel: string) => {
+	const rowRenderer = useCallback(
+		({ index, key, style }: { index: number; key: string; style: React.CSSProperties }) => {
+			const event = sortedEvents[index];
+
 			return (
-				<div
-					className="group cursor-pointer font-normal outline-none focus:ring-2 focus:ring-blue-500"
-					onClick={handleSort(columnKey)}
-					onKeyDown={handleSort(columnKey)}
-					role="button"
-					tabIndex={0}
-				>
-					{columnLabel}
-					<SortButton
-						className="opacity-0 group-hover:opacity-100 group-focus:opacity-100"
-						isActive={columnKey === sortConfig.key}
-						sortDirection={sortConfig.direction}
-					/>
-				</div>
-			);
-		},
-		[handleSort, sortConfig]
-	);
-
-	const onScroll = useCallback(
-		(event: React.UIEvent<HTMLDivElement>, height: number) => {
-			const { scrollTop } = event.currentTarget;
-			const start = Math.floor(scrollTop / defaultEventsTableRowHeight);
-			const end = Math.min(
-				sortedEvents.length,
-				Math.ceil((scrollTop + height) / defaultEventsTableRowHeight) + 1
-			);
-			setVisibleRows({ start, end });
-		},
-		[sortedEvents.length]
-	);
-
-	const renderRows = useCallback(
-		({ height }: { height: number }) => {
-			const start = visibleRows.start;
-			const end = Math.min(visibleRows.end, Math.ceil(height / defaultEventsTableRowHeight) + start);
-
-			return sortedEvents.slice(start, end).map((event) => (
 				<Tr
 					className="cursor-pointer hover:bg-gray-750"
-					key={event.eventId}
+					key={key}
 					onClick={() => navigate(`events/${event.eventId}`)}
+					style={style}
 				>
 					<Td>{moment(event.createdAt).format(dateTimeFormat)}</Td>
 					<Td>{event.eventId}</Td>
 				</Tr>
-			));
+			);
 		},
-		[sortedEvents, visibleRows, navigate]
+		[sortedEvents, navigate]
 	);
+
+	const renderContent = () => {
+		if (loadingEvents) {
+			return <Loader isCenter size="xl" />;
+		}
+
+		if (!sortedEvents?.length) {
+			return <div className="mt-10 text-center text-xl font-semibold">{t("history.noEvents")}</div>;
+		}
+
+		return (
+			<div className="mt-4 h-full">
+				<Table className="relative w-full overflow-visible">
+					<TableHeader onSort={handleSort} sortConfig={sortConfig} />
+					<TBody>
+						<div className="h-[calc(100vh-200px)]">
+							{" "}
+							{/* Adjust height as needed */}
+							<AutoSizer>
+								{({ height, width }) => (
+									<List
+										height={height}
+										overscanRowCount={5}
+										rowCount={sortedEvents.length}
+										rowHeight={defaultEventsTableRowHeight}
+										rowRenderer={rowRenderer}
+										width={width}
+									/>
+								)}
+							</AutoSizer>
+						</div>
+					</TBody>
+				</Table>
+			</div>
+		);
+	};
 
 	return (
 		<div className="my-2 flex size-full">
 			<div style={{ width: `${leftSideWidth}%` }}>
-				<Frame className={frameClass}>
-					{loadingEvents ? <Loader isCenter size="xl" /> : null}
-
-					{!loadingEvents && !sortedEvents?.length ? (
-						<div className="mt-10 text-center text-xl font-semibold">{t("history.noEvents")}</div>
-					) : null}
-
-					{!loadingEvents && !!sortedEvents?.length ? (
-						<div className="mt-4 h-full">
-							<AutoSizer>
-								{({ height, width }) => (
-									<div
-										className="overflow-auto"
-										onScroll={(event) => onScroll(event, height)}
-										style={{ width, height }}
-									>
-										<Table className="relative w-full overflow-visible">
-											<THead>
-												<Th>
-													<Td>
-														{renderSortableHeader(
-															"createdAt",
-															t("table.columns.createdAt")
-														)}
-													</Td>
-													<Td>
-														{renderSortableHeader("eventId", t("table.columns.eventId"))}
-													</Td>
-												</Th>
-											</THead>
-											<TBody>{renderRows({ height })}</TBody>
-										</Table>
-									</div>
-								)}
-							</AutoSizer>
-						</div>
-					) : null}
-				</Frame>
+				<Frame className={frameClass}>{renderContent()}</Frame>
 			</div>
 
 			<ResizeButton className="hover:bg-white" direction="horizontal" resizeId={resizeId} />
 
-			<div className="flex" style={{ width: `${100 - (leftSideWidth as number)}%` }}>
+			<div className="flex" style={{ width: `${100 - leftSideWidth}%` }}>
 				{eventId ? (
 					<Outlet />
 				) : (
