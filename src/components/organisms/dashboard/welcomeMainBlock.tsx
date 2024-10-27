7e9c1c52df9e366029eafb3cd399825bb3c41a6d
Ronen Mars <ronen@autokitteh.com>
2024-10-27 19:48:44 +0200

    refactor: intro & welcome page

--- src/components/organisms/dashboard/welcomeMainBlock.tsx
+++ src/components/organisms/dashboard/welcomeMainBlock.tsx
@@ -104,7 +104,7 @@ export const DashboardWelcomeMainBlock = () => {
 					<ul className="font-averta font-semibold leading-normal">
 						{infoCardPythonCode.map(({ href, text }, index) => (
 							<li aria-label={text} key={index}>
-								<Link className="font-semibold text-green-800 underline" to={href}>
+								<Link className="font-semibold text-green-800 underline hover:text-green-200" to={href}>
 									{text}
 								</Link>
 							</li>
