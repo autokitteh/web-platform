06cdbdc401788140b5ba87c8ab18e64be8db9c8e
J1za <workinvs@gmail.com>
2024-10-28 13:55:23 +0200

    feat: add new features

--- src/components/organisms/dashboard/templates/tabs/createModal.tsx
+++ src/components/organisms/dashboard/templates/tabs/createModal.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { Editor } from "@monaco-editor/react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 import { ModalName } from "@enums/components";
@@ -22,6 +23,16 @@ export const ProjectTemplateCreateModal = ({ cardTemplate, category }: CreatePro
 	const { closeModal } = useModalStore();
 	const { createProjectFromTemplate } = useCreateProjectFromTemplate();
 
+	const {
+		formState: { errors },
+		handleSubmit,
+		register,
+	} = useForm<{ projectName: string }>({
+		defaultValues: {
+			projectName: "",
+		},
+	});
+
 	const fetchManifestData = async () => {
 		if (!assetDirectory) return;
 		const content = await fetchFileContent(`/assets/templates/${assetDirectory}/README.md`);
@@ -33,76 +44,90 @@ export const ProjectTemplateCreateModal = ({ cardTemplate, category }: CreatePro
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [assetDirectory]);
 
-	const createProjectFromAsset = async () => {
-		if (!assetDirectory) return;
+	const onSubmit = async (data: { projectName: string }) => {
+		const { projectName } = data;
+		if (!assetDirectory || !projectName) return;
 		setIsCreating(true);
-		await createProjectFromTemplate(assetDirectory);
+		await createProjectFromTemplate(assetDirectory, projectName);
 		setIsCreating(false);
 		closeModal(ModalName.templateCreateProject);
 	};
 
 	return (
 		<Modal className="p-5" hideCloseButton name={ModalName.templateCreateProject}>
-			<div className="mb-3 flex items-center justify-end gap-5">
-				<Status>{category}</Status>
-				<div className="flex gap-3">
-					{integrations.map(({ icon, label }, index) => (
-						<div
-							className="relative flex size-8 items-center justify-center rounded-full bg-gray-1400 p-1"
-							key={index}
-							title={label}
-						>
-							<IconSvg className="z-10 rounded-full p-1" size="xl" src={icon} />
-							{index < integrations.length - 1 ? (
-								<PipeCircleDarkIcon className="absolute -right-4 top-1/2 -translate-y-1/2 fill-gray-500" />
-							) : null}
-						</div>
-					))}
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="mb-3 flex items-center justify-end gap-5">
+					<Status>{category}</Status>
+					<div className="flex gap-3">
+						{integrations.map(({ icon, label }, index) => (
+							<div
+								className="relative flex size-8 items-center justify-center rounded-full bg-gray-1400 p-1"
+								key={index}
+								title={label}
+							>
+								<IconSvg className="z-10 rounded-full p-1" size="xl" src={icon} />
+								{index < integrations.length - 1 ? (
+									<PipeCircleDarkIcon className="absolute -right-4 top-1/2 -translate-y-1/2 fill-gray-500" />
+								) : null}
+							</div>
+						))}
+					</div>
 				</div>
-			</div>
-			<Input label={t("projectName")} variant="light" />
-			<Typography className="mt-2 font-semibold" element="h4" size="medium">
-				{description}
-			</Typography>
-			<div className="mt-4 flex items-center gap-1 text-base uppercase">
-				<ReadmeIcon className="size-4" /> {t("readme")}
-			</div>
-			<Editor
-				className="h-96"
-				defaultLanguage="markdown"
-				options={{
-					fontFamily: "monospace, sans-serif",
-					fontSize: 14,
-					minimap: { enabled: false },
-					renderLineHighlight: "none",
-					scrollBeyondLastLine: false,
-					wordWrap: "on",
-					readOnly: true,
-					lineNumbers: "off",
-				}}
-				theme="vs-dark"
-				value={manifestData || ""}
-			/>
-			<div className="mt-8 flex w-full justify-end gap-2">
-				<Button
-					ariaLabel={t("cancelButton")}
-					className="px-4 py-3 font-semibold hover:bg-gray-1100 hover:text-white"
-					onClick={() => closeModal(ModalName.templateCreateProject)}
-					variant="outline"
-				>
-					{t("cancelButton")}
-				</Button>
-				<Button
-					ariaLabel={t("createButton")}
-					className="bg-gray-1100 px-4 py-3 font-semibold"
-					disabled={isCreating}
-					onClick={createProjectFromAsset}
-					variant="filled"
-				>
-					{isCreating ? <Loader className="mr-2" size="sm" /> : null}
-					{t("createButton")}
-				</Button>
-			</div>
+
+				<Input
+					label={t("projectName")}
+					variant="light"
+					{...register("projectName", { required: true })}
+					isError={!!errors.projectName}
+				/>
+				{errors.projectName ? (
+					<Typography className="mt-1 text-error" element="p">
+						{t("projectNameRequired")}
+					</Typography>
+				) : null}
+				<Typography className="mt-2 font-semibold" element="h4" size="medium">
+					{description}
+				</Typography>
+				<div className="mt-4 flex items-center gap-1 text-base uppercase">
+					<ReadmeIcon className="size-4" /> {t("readme")}
+				</div>
+				<Editor
+					className="h-96"
+					defaultLanguage="markdown"
+					options={{
+						fontFamily: "monospace, sans-serif",
+						fontSize: 14,
+						minimap: { enabled: false },
+						renderLineHighlight: "none",
+						scrollBeyondLastLine: false,
+						wordWrap: "on",
+						readOnly: true,
+						lineNumbers: "off",
+					}}
+					theme="vs-dark"
+					value={manifestData || ""}
+				/>
+				<div className="mt-8 flex w-full justify-end gap-2">
+					<Button
+						ariaLabel={t("cancelButton")}
+						className="px-4 py-3 font-semibold hover:bg-gray-1100 hover:text-white"
+						onClick={() => closeModal(ModalName.templateCreateProject)}
+						variant="outline"
+					>
+						{t("cancelButton")}
+					</Button>
+					<Button
+						ariaLabel={t("createButton")}
+						className="bg-gray-1100 px-4 py-3 font-semibold"
+						disabled={isCreating}
+						type="submit"
+						variant="filled"
+					>
+						{isCreating ? <Loader className="mr-2" size="sm" /> : null}
+						{t("createButton")}
+					</Button>
+				</div>
+			</form>
 		</Modal>
 	);
 };
