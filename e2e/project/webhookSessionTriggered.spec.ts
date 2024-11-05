83fcd7fb7e50ed8665e724d309a630f443041425
Ronen Mars <ronen@autokitteh.com>
2024-11-05 11:12:11 +0200

    feat: e2e full flow - test

--- e2e/project/webhookSessionTriggered.spec.ts
+++ e2e/project/webhookSessionTriggered.spec.ts
@@ -20,7 +20,10 @@ async function waitForFirstCompletedSession(page: Page, timeoutMs = 60000) {
 			await page.waitForTimeout(500);
 		}
 
-		const hasErrorStatus = await page.getByRole("status", { name: "error" }).filter({ hasText: "1" }).isVisible();
+		const hasErrorStatus = await page
+			.getByRole("status", { name: "completed" })
+			.filter({ hasText: "1" })
+			.isVisible();
 
 		expect(hasErrorStatus).toBe(true);
 
@@ -39,7 +42,9 @@ test.describe("Session triggered with webhook", () => {
 	test("should successfully deploy project and execute session via webhook", async ({ page }: { page: Page }) => {
 		const deploymentTableRow = page.getByRole("cell", { name: /bld_*/ });
 		await expect(deploymentTableRow).toHaveCount(1);
-		const completedSessionDeploymentColumn = page.getByRole("status", { name: "error" }).filter({ hasText: "1" });
+		const completedSessionDeploymentColumn = page
+			.getByRole("status", { name: "completed" })
+			.filter({ hasText: "1" });
 		completedSessionDeploymentColumn.click();
 
 		await page
