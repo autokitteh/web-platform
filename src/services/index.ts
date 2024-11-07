65e950ca416a4e6935549fa2d53b36424063389d
Ronen Mars <ronen@autokitteh.com>
2024-11-07 14:20:42 +0200

    feat: fetch templates from github

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,15 @@
+// eslint-disable-next-line @liferay/sort-exports
+export { IndexedDBService } from "@services/indexedDb.service";
+export { TemplateStorageService } from "@services/templatesIndexedDb.service";
 export { AuthService } from "@services/auth.service";
 export { BuildsService } from "@services/builds.service";
 export { ConnectionService } from "@services/connection.service";
 export { DeploymentsService } from "@services/deployments.service";
 export { EventsService } from "@services/events.service";
 export { HttpService, LocalDomainHttpService } from "@services/http.service";
-export { IndexedDBService } from "@services/indexedDb.service";
 export { IntegrationsService } from "@services/integrations.service";
 export { LoggerService } from "@services/logger.service";
 export { ProjectsService } from "@services/projects.service";
 export { SessionsService } from "@services/sessions.service";
-export { TemplateStorageService } from "@services/templatesIndexedDb.service";
 export { TriggersService } from "@services/triggers.service";
 export { VariablesService } from "@services/variables.service";
