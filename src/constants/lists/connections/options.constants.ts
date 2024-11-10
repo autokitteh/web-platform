0ae414bc7b5773e2679ee0d0163ddad702936898
Ronen Mars <ronen@autokitteh.com>
2024-11-10 14:04:17 +0200

    fix: update aws connection regions list

--- src/constants/lists/connections/options.constants.ts
+++ src/constants/lists/connections/options.constants.ts
@@ -32,23 +32,142 @@ export const selectIntegrationSlack: SelectOption[] = featureFlags.displaySlackS
 	: baseSelectIntegrationSlack;
 
 export const selectIntegrationAws: SelectOption[] = [
-	{ value: "ap-northeast-1", label: "ap-northeast-1" },
-	{ value: "ap-northeast-2", label: "ap-northeast-2" },
-	{ value: "ap-northeast-3", label: "ap-northeast-3" },
-	{ value: "ap-south-1", label: "ap-south-1" },
-	{ value: "ap-southeast-1", label: "ap-southeast-1" },
-	{ value: "ap-southeast-2", label: "ap-southeast-2" },
-	{ value: "ca-central-1", label: "ca-central-1" },
-	{ value: "eu-central-1", label: "eu-central-1" },
-	{ value: "eu-north-1", label: "eu-north-1" },
-	{ value: "eu-west-1", label: "eu-west-1" },
-	{ value: "eu-west-2", label: "eu-west-2" },
-	{ value: "eu-west-3", label: "eu-west-3" },
-	{ value: "sa-east-1", label: "sa-east-1" },
-	{ value: "us-east-1", label: "us-east-1" },
-	{ value: "us-east-2", label: "us-east-2" },
-	{ value: "us-west-1", label: "us-west-1" },
-	{ value: "us-west-2", label: "us-west-2" },
+	{
+		label: "Africa (Cape Town)",
+		value: "af-south-1",
+	},
+	{
+		label: "Asia Pacific (Hong Kong)",
+		value: "ap-east-1",
+	},
+	{
+		label: "Asia Pacific (Tokyo)",
+		value: "ap-northeast-1",
+	},
+	{
+		label: "Asia Pacific (Seoul)",
+		value: "ap-northeast-2",
+	},
+	{
+		label: "Asia Pacific (Osaka)",
+		value: "ap-northeast-3",
+	},
+	{
+		label: "Asia Pacific (Mumbai)",
+		value: "ap-south-1",
+	},
+	{
+		label: "Asia Pacific (Hyderabad)",
+		value: "ap-south-2",
+	},
+	{
+		label: "Asia Pacific (Singapore)",
+		value: "ap-southeast-1",
+	},
+	{
+		label: "Asia Pacific (Sydney)",
+		value: "ap-southeast-2",
+	},
+	{
+		label: "Asia Pacific (Jakarta)",
+		value: "ap-southeast-3",
+	},
+	{
+		label: "Asia Pacific (Melbourne)",
+		value: "ap-southeast-4",
+	},
+	{
+		label: "Asia Pacific (Malaysia)",
+		value: "ap-southeast-5",
+	},
+	{
+		label: "Canada (Central)",
+		value: "ca-central-1",
+	},
+	{
+		label: "Canada West (Calgary)",
+		value: "ca-west-1",
+	},
+	{
+		label: "China (Beijing)",
+		value: "cn-north-1",
+	},
+	{
+		label: "China (Ningxia)",
+		value: "cn-northwest-1",
+	},
+	{
+		label: "Europe (Frankfurt)",
+		value: "eu-central-1",
+	},
+	{
+		label: "Europe (Zurich)",
+		value: "eu-central-2",
+	},
+	{
+		label: "Europe (Stockholm)",
+		value: "eu-north-1",
+	},
+	{
+		label: "Europe (Milan)",
+		value: "eu-south-1",
+	},
+	{
+		label: "Europe (Spain)",
+		value: "eu-south-2",
+	},
+	{
+		label: "Europe (Ireland)",
+		value: "eu-west-1",
+	},
+	{
+		label: "Europe (London)",
+		value: "eu-west-2",
+	},
+	{
+		label: "Europe (Paris)",
+		value: "eu-west-3",
+	},
+	{
+		label: "Israel (Tel Aviv)",
+		value: "il-central-1",
+	},
+	{
+		label: "Middle East (UAE)",
+		value: "me-central-1",
+	},
+	{
+		label: "Middle East (Bahrain)",
+		value: "me-south-1",
+	},
+	{
+		label: "South America (Sao Paulo)",
+		value: "sa-east-1",
+	},
+	{
+		label: "US East (N. Virginia)",
+		value: "us-east-1",
+	},
+	{
+		label: "US East (Ohio)",
+		value: "us-east-2",
+	},
+	{
+		label: "AWS GovCloud (US-East)",
+		value: "us-gov-east-1",
+	},
+	{
+		label: "AWS GovCloud (US-West)",
+		value: "us-gov-west-1",
+	},
+	{
+		label: "US West (N. California)",
+		value: "us-west-1",
+	},
+	{
+		label: "US West (Oregon)",
+		value: "us-west-2",
+	},
 ];
 
 export const selectIntegrationHttp: SelectOption[] = [
