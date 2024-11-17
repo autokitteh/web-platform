81144eeac58619f2947c2a513b92814d463446ea
Ronen Mars <ronen@autokitteh.com>
2024-11-17 20:48:33 +0200

    feat: extra hard code to copy to clipboard

--- src/utilities/copyToClipboard.utils.ts
+++ src/utilities/copyToClipboard.utils.ts
@@ -4,6 +4,14 @@ export const copyToClipboard = async (text: string): Promise<{ isError: boolean;
 	try {
 		await navigator.clipboard.writeText(text);
 
+		const tempInput = document.createElement("input");
+		tempInput.style.cssText = "position: absolute; left: -1000px; top: -1000px";
+		tempInput.value = text;
+		document.body.appendChild(tempInput);
+		tempInput.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempInput);
+
 		return { isError: false, message: i18n.t("copySuccess", { ns: "global" }) };
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (error) {
