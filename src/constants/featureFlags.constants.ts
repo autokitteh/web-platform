35e3845fb190e7b5d492c43152ae5715e2e27fc7
Ronen Mars <ronen@autokitteh.com>
2024-11-20 16:51:09 +0200

    revert: feature flag

--- src/constants/featureFlags.constants.ts
+++ src/constants/featureFlags.constants.ts
@@ -1,5 +1,5 @@
 export const featureFlags = {
 	displayDiscordIntegration: import.meta.env.DISPLAY_DISCORD_INTEGRATION,
 	displaySlackSocketIntegration: import.meta.env.DISPLAY_SLACK_SOCKET_INTEGRATION,
-	displayComboxInTriggersForm: true,
+	displayComboxInTriggersForm: import.meta.env.DISPLAY_COMBOX_IN_TRIGGERS_FORM,
 };
