f84d5e88a68e502c283f630004279b3e3463014b
Vitaliy Stukalo <workinvs@gmail.com>
2024-11-20 16:25:41 +0200

    feat: event viewer style (#669)

--- src/components/organisms/events/viewer.tsx
+++ src/components/organisms/events/viewer.tsx
@@ -1,13 +1,13 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import JsonView from "@uiw/react-json-view";
 import { githubDarkTheme } from "@uiw/react-json-view/githubDark";
-import { omit } from "lodash";
+import moment from "moment";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { EventsService, LoggerService } from "@services";
-import { namespaces } from "@src/constants";
+import { dateTimeFormat, namespaces } from "@src/constants";
 import { useToastStore } from "@src/store";
 import { BaseEvent } from "@src/types/models";
 
@@ -55,37 +55,45 @@ export const EventViewer = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [eventId]);
 
-	const eventInputs = useMemo(() => omit(eventInfo, "data"), [eventInfo]);
-
 	return isLoading ? (
 		<Loader size="xl" />
 	) : (
 		<Frame className="overflow-y-auto overflow-x-hidden rounded-l-none pb-3 font-fira-code">
 			<div className="flex items-center justify-between border-b border-gray-950 pb-3.5">
-				<div className="flex gap-2 font-fira-sans text-base text-gray-500">
-					<span className="font-semibold">{t("event")}:</span>
-					{eventInfo?.eventId}
-					<CopyButton className="p-0" size="xs" text={eventInfo?.eventId || ""} />
-				</div>
-				<div className="flex items-center gap-3">
-					<IconButton className="size-7 bg-gray-1100 p-0.5" onClick={closeViewer}>
-						<Close className="size-3 fill-white" />
-					</IconButton>
-				</div>
+				<IconButton className="ml-auto size-7 bg-gray-1100 p-0.5" onClick={closeViewer}>
+					<Close className="size-3 fill-white" />
+				</IconButton>
 			</div>
 			{eventInfo ? (
-				<div className="mt-3 border-b border-gray-950 pb-3.5">
-					<Typography className="mb-2 mt-1 font-fira-sans">{t("inputs")}</Typography>
-					<JsonView
-						className="scrollbar max-h-72 overflow-auto"
-						style={githubDarkTheme}
-						value={eventInputs}
-					/>
+				<div className="mt-3 flex justify-between border-b border-gray-950 pb-3.5">
+					<div className="flex flex-col gap-0.5 leading-6">
+						<div className="flex items-center gap-4">
+							<div className="w-32 text-gray-1550">{t("eventType")}</div>
+							{eventInfo.eventType}
+						</div>
+						<div className="flex items-center gap-4">
+							<div className="w-32 text-gray-1550" title="Start Time">
+								{t("created")}
+							</div>
+							<div className="flex flex-row items-center">
+								{moment(eventInfo.createdAt).local().format(dateTimeFormat)}
+							</div>
+						</div>
+					</div>
+
+					<div className="flex flex-col gap-0.5">
+						<div className="flex items-center justify-end gap-4">
+							<div className="leading-6">{t("eventId")}</div>
+							<CopyButton className="p-0" size="xs" text={eventInfo.eventId} />
+						</div>
+						<div className="flex items-center justify-end gap-4">
+							<div className="leading-6">{t("destinationId")}</div>
+							<CopyButton className="p-0" size="xs" text={eventInfo.destinationId || ""} />
+						</div>
+					</div>
 				</div>
 			) : null}
-			<div className="mt-5 flex items-center justify-between">
-				<Typography className="font-fira-sans">{t("output")}</Typography>
-			</div>
+			<Typography className="mt-5 font-fira-sans">{t("payload")}</Typography>
 			{eventInfo?.data ? (
 				<JsonView className="scrollbar mt-3 overflow-auto" style={githubDarkTheme} value={eventInfo.data} />
 			) : null}
