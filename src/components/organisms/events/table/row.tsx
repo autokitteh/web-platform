18a2e29d7ea501ba605a86c162ce2dec0f0f4df0
Ronen Mars <ronen@autokitteh.com>
2024-11-21 08:02:28 +0200

    feat(UI-941): change id columns to copy buttons

--- src/components/organisms/events/table/row.tsx
+++ src/components/organisms/events/table/row.tsx
@@ -6,14 +6,27 @@ import { dateTimeFormat } from "@src/constants";
 import { BaseEvent } from "@src/types/models";
 
 import { Td, Tr } from "@components/atoms";
+import { IdCopyButton } from "@components/molecules";
 
 export const EventRow = memo(
-	({ event, onClick, style }: { event: BaseEvent; onClick: () => void; style: CSSProperties }) => (
+	({
+		event: { createdAt, destinationId, eventId, eventType },
+		onClick,
+		style,
+	}: {
+		event: BaseEvent;
+		onClick: () => void;
+		style: CSSProperties;
+	}) => (
 		<Tr className="cursor-pointer pl-3 hover:bg-gray-750" onClick={onClick} style={style}>
-			<Td>{moment(event.createdAt).local().format(dateTimeFormat)}</Td>
-			<Td>{event.eventId}</Td>
-			<Td>{event.destinationId}</Td>
-			<Td className="w-56">{event.eventType}</Td>
+			<Td>{moment(createdAt).local().format(dateTimeFormat)}</Td>
+			<Td>
+				<IdCopyButton id={eventId} />
+			</Td>
+			<Td>
+				<IdCopyButton id={destinationId || ""} />
+			</Td>
+			<Td className="w-56">{eventType}</Td>
 		</Tr>
 	)
 );
