fb2fca0cd2befa6cc11aab2a710e783f8dfda6c4
Ronen Mars <ronen@autokitteh.com>
2024-12-01 11:27:27 +0200

    fix(UI-1018): fix google gemini addition in connections

--- src/components/organisms/connections/integrations/googleGemini/add.tsx
+++ src/components/organisms/connections/integrations/googleGemini/add.tsx
@@ -29,7 +29,7 @@ export const GoogleGeminiIntegrationAddForm = ({
 
 	useEffect(() => {
 		if (connectionId) {
-			createConnection(connectionId, ConnectionAuthType.BotToken, Integrations.discord);
+			createConnection(connectionId, ConnectionAuthType.BotToken, Integrations.googlegemini);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [connectionId]);
