818c8fe5e3059131c916788811fc155b127990b1
Ronen Mars <ronen@autokitteh.com>
2024-12-01 08:57:04 +0200

    feat(UI-977): add user feedback bubble - custom

--- src/components/organisms/userFeedbackModal.tsx
+++ src/components/organisms/userFeedbackModal.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import * as Sentry from "@sentry/react";
+import html2canvas from "html2canvas-pro";
 import { useForm } from "react-hook-form";
 
 import { ModalName } from "@enums/components";
+import { cn } from "@src/utilities";
 
 import { Button, ErrorMessage, Input } from "@components/atoms";
 import { Modal } from "@components/molecules";
@@ -22,6 +24,8 @@ export const UserFeedbackModal = () => {
 		},
 	});
 
+	const [shown, setShown] = React.useState(true);
+
 	const onSubmit = async (data: { comments: string; email: string; name: string }) => {
 		const { comments, email, name } = data;
 		const sentryId = Sentry.captureMessage("Message that needs user feedback");
@@ -36,8 +40,29 @@ export const UserFeedbackModal = () => {
 		Sentry.captureFeedback(userFeedback);
 	};
 
+	const takeScreenshot = async () => {
+		const screenshotSmall = await html2canvas(document.body);
+		screenshotSmall.style.width = "100%";
+		screenshotSmall.style.height = "100%";
+		document.getElementById("screenshot")!.appendChild(screenshotSmall);
+	};
+
 	return (
 		<Modal className="w-1/2 min-w-550 bg-black p-5" hideCloseButton name={ModalName.userFeedback}>
+			<button
+				className="text-white"
+				onClick={() => {
+					takeScreenshot();
+
+					setShown(true);
+				}}
+			>
+				Add screenshot
+			</button>
+			<div className={cn({ hidden: !shown })}>
+				<div id="screenshot" />
+			</div>
+
 			<form className="mb-8" onSubmit={handleSubmit(onSubmit)}>
 				<Input
 					label="Name"
