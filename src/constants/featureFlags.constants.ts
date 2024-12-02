c15743f632ad25d0563806c1aa40a876eddb40d4
Ronen Mars <ronenmars@gmail.com>
2024-12-02 13:35:16 +0200

    refactor(UI-929): remove events list in triggers combobox feature flag (#708)

--- src/constants/featureFlags.constants.ts
+++ src/constants/featureFlags.constants.ts
@@ -1,5 +1,4 @@
 export const featureFlags = {
 	displayDiscordIntegration: import.meta.env.DISPLAY_DISCORD_INTEGRATION,
 	displaySlackSocketIntegration: import.meta.env.DISPLAY_SLACK_SOCKET_INTEGRATION,
-	displayComboxInTriggersForm: import.meta.env.DISPLAY_COMBOX_IN_TRIGGERS_FORM,
 };
