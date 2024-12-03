e720293bbd7f8485655e2599362b3e0122213531
Ronen Mars <ronenmars@gmail.com>
2024-12-03 15:33:13 +0200

    fix(UI-1029): display an error message when copy button fails (#718)

--- src/utilities/copyToClipboard.utils.ts
+++ src/utilities/copyToClipboard.utils.ts
@@ -7,6 +7,6 @@ export const copyToClipboard = async (text: string): Promise<{ isError: boolean;
 		return { isError: false, message: i18n.t("copySuccess", { ns: "global" }) };
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (error) {
-		return { isError: false, message: i18n.t("copyFailure", { ns: "global" }) };
+		return { isError: true, message: i18n.t("copyFailure", { ns: "global" }) };
 	}
 };
