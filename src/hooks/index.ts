a11d9923c77a0688c632572b85dc7856666a4e4b
Ronen Mars <ronen@autokitteh.com>
2024-12-03 21:38:14 +0200

    feat(UI-1031): add google analytics - refactor

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ export { useFetchTrigger } from "@hooks/triggers/useFetchTrigger";
 export { useConnectionForm } from "@hooks/useConnectionForm";
 export { useCreateProjectFromTemplate } from "@hooks/useCreateProjectFromTemplate";
 export { useFileOperations } from "@hooks/useFileOperations";
-export { useGoogleAnalytics } from "@hooks/useGoogleAnalytics";
 export { useInterval } from "@hooks/useInterval";
 export { useResize } from "@hooks/useResize";
 export { useSort } from "@hooks/useSort";
