e45c5cfbadd750353577f2887159dd90fda06835
J1za <workinvs@gmail.com>
2024-12-04 11:00:14 +0200

    feat: add import project feature

--- src/components/organisms/topbar/dashboard.tsx
+++ src/components/organisms/topbar/dashboard.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { useTranslation } from "react-i18next";
 
-import { ModalName } from "@src/enums/components";
-
 import { useModalStore } from "@store";
 
 import { Button, IconSvg, Typography } from "@components/atoms";
@@ -13,6 +11,10 @@ import { PlusAccordionIcon } from "@assets/image/icons";
 export const DashboardTopbar = () => {
 	const { t } = useTranslation("dashboard", { keyPrefix: "topbar" });
 	const { openModal } = useModalStore();
+
+	const triggerFileInput = () => {
+		fileInputRef.current?.click();
+	};
 
 	return (
 		<div className="z-10 flex flex-wrap">
@@ -24,15 +26,36 @@ export const DashboardTopbar = () => {
 					{t("welcome")}
 				</Typography>
 
-				<Button
-					className="hidden gap-2.5 whitespace-nowrap rounded-full border border-gray-750 py-2.5 pl-3 pr-4 font-averta text-base font-semibold md:flex"
-					onClick={() => openModal(ModalName.newProject)}
-					variant="filled"
-				>
-					<IconSvg className="fill-white" size="lg" src={PlusAccordionIcon} />
-
-					{t("buttons.newProject")}
-				</Button>
+				<div className="flex rounded-full border border-gray-750 font-averta text-base font-semibold">
+					<Button
+						className="gap-2.5 whitespace-nowrap rounded-none rounded-l-full border-r border-gray-750 px-3 py-2.5"
+						disabled={loadingNewProject}
+						onClick={handleCreateProject}
+						variant="filled"
+					>
+						<IconSvg
+							className="fill-white"
+							size="lg"
+							src={!loadingNewProject ? PlusAccordionIcon : Spinner}
+						/>
+						{t("buttons.newProject")}
+					</Button>
+					<Button
+						className="gap-2.5 whitespace-nowrap rounded-none rounded-r-full px-3 py-2.5"
+						disabled={loadingNewProject}
+						onClick={triggerFileInput}
+						variant="filled"
+					>
+						{t("buttons.import")}
+					</Button>
+					<input
+						accept=".zip"
+						className="hidden"
+						onChange={handleImportFile}
+						ref={fileInputRef}
+						type="file"
+					/>
+				</div>
 			</div>
 		</div>
 	);
