de795b5b17b83d451dbecdaf09e095504c43f151
Ronen Mars <ronen@autokitteh.com>
2024-12-16 10:04:07 +0200

    refactor(UI-1057): update trigger scheduler info links

--- src/constants/triggers.constants.ts
+++ src/constants/triggers.constants.ts
@@ -15,12 +15,12 @@ export let extraTriggerTypes = [
 i18n.on("initialized", () => {
 	infoCronExpressionsLinks = [
 		{
-			text: i18n.t("triggers.form.info.aboutCron", { ns: "tabs" }),
-			url: "https://docs.oracle.com/cd/E12058_01/doc/doc.1014/e12030/cron_expressions.htm",
+			text: i18n.t("triggers.form.info.firstLink", { ns: "tabs" }),
+			url: "https://crontab.guru/",
 		},
 		{
-			text: i18n.t("triggers.form.info.schedules", { ns: "tabs" }),
-			url: "https://docs.temporal.io/workflows#robfig-predefined-schedules-and-intervals",
+			text: i18n.t("triggers.form.info.secondLink", { ns: "tabs" }),
+			url: "https://github.com/autokitteh/kittehub/tree/main/samples/scheduler#api-documentation",
 		},
 	];
 
