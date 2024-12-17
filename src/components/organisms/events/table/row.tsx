5c84cc5af73779c3caf9e8638c146e423be66bbf
Ronen Mars <ronen@autokitteh.com>
2024-12-17 14:44:49 +0200

    feat(UI-1081): mark selected event in events table (#776)

--- src/components/organisms/events/table/row.tsx
+++ src/components/organisms/events/table/row.tsx
@@ -1,9 +1,11 @@
 import React, { CSSProperties, memo } from "react";
 
 import moment from "moment";
+import { useParams } from "react-router-dom";
 
 import { dateTimeFormat } from "@src/constants";
 import { BaseEvent } from "@src/types/models";
+import { cn } from "@src/utilities";
 
 import { Td, Tr } from "@components/atoms";
 import { IdCopyButton } from "@components/molecules";
@@ -17,22 +19,29 @@ export const EventRow = memo(
 		event: BaseEvent;
 		onClick: () => void;
 		style: CSSProperties;
-	}) => (
-		<Tr className="cursor-pointer pl-3 hover:bg-gray-750" onClick={onClick} style={style}>
-			<Td className="w-1/4" title={moment(createdAt).local().format(dateTimeFormat)}>
-				{moment(createdAt).local().format(dateTimeFormat)}
-			</Td>
-			<Td className="w-1/4" title={eventId}>
-				<IdCopyButton id={eventId} />
-			</Td>
-			<Td className="w-1/4" title={destinationId || ""}>
-				<IdCopyButton id={destinationId || ""} />
-			</Td>
-			<Td className="w-1/4" title={eventType}>
-				{eventType}
-			</Td>
-		</Tr>
-	)
+	}) => {
+		const { eventId: paramEventId } = useParams();
+		const rowClass = cn("cursor-pointer pl-3 hover:bg-gray-750", {
+			"bg-black": paramEventId === eventId,
+		});
+
+		return (
+			<Tr className={rowClass} onClick={onClick} style={style}>
+				<Td className="w-1/4" title={moment(createdAt).local().format(dateTimeFormat)}>
+					{moment(createdAt).local().format(dateTimeFormat)}
+				</Td>
+				<Td className="w-1/4" title={eventId}>
+					<IdCopyButton id={eventId} />
+				</Td>
+				<Td className="w-1/4" title={destinationId || ""}>
+					<IdCopyButton id={destinationId || ""} />
+				</Td>
+				<Td className="w-1/4" title={eventType}>
+					{eventType}
+				</Td>
+			</Tr>
+		);
+	}
 );
 
 EventRow.displayName = "EventRow";
