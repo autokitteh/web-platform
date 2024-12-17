5f6c91b9fcaf13ddc4d46fbab3dd0917770fec9f
Ronen Mars <ronen@autokitteh.com>
2024-12-17 21:42:58 +0200

    fix(UI-1088): remove redundant hook

--- src/components/organisms/dashboard/projectsTable.tsx
+++ src/components/organisms/dashboard/projectsTable.tsx
@@ -11,7 +11,7 @@ import { ModalName, SidebarHrefMenu } from "@src/enums/components";
 import { cn } from "@src/utilities";
 import { DashboardProjectWithStats, Project } from "@type/models";
 
-import { useProjectActions, useSort, useWindowDimensions } from "@hooks";
+import { useProjectActions, useSort } from "@hooks";
 import { useModalStore, useProjectStore } from "@store";
 
 import { Button, IconButton, IconSvg, Loader, StatusBadge, TBody, THead, Table, Td, Th, Tr } from "@components/atoms";
@@ -37,7 +37,6 @@ export const DashboardProjectsTable = () => {
 	const { openModal } = useModalStore();
 	const { deleteProject, downloadProjectExport, isDeleting } = useProjectActions();
 	const [selectedProjectForDeletion, setSelectedProjectForDeletion] = useState<string>();
-	const { isMobileDevice } = useWindowDimensions();
 
 	const loadProjectsData = async (projectsList: Project[]) => {
 		const projectsStats = {} as Record<string, DashboardProjectWithStats>;
@@ -286,26 +285,22 @@ export const DashboardProjectsTable = () => {
 				<div>{t("table.noProjectsFound")}</div>
 			)}
 
-			<div className="mt-10 flex flex-col items-center justify-center">
-				{isMobileDevice ? null : (
-					<>
-						<Button
-							className="gap-2.5 whitespace-nowrap rounded-full border border-gray-750 py-2.5 pl-3 pr-4 font-averta text-base font-semibold"
-							onClick={() => openModal(ModalName.newProject)}
-							variant="filled"
-						>
-							<IconSvg className="fill-white" size="lg" src={PlusAccordionIcon} />
+			<div className="mt-10 hidden flex-col items-center justify-center sm:flex">
+				<Button
+					className="gap-2.5 whitespace-nowrap rounded-full border border-gray-750 py-2.5 pl-3 pr-4 font-averta text-base font-semibold"
+					onClick={() => openModal(ModalName.newProject)}
+					variant="filled"
+				>
+					<IconSvg className="fill-white" size="lg" src={PlusAccordionIcon} />
 
-							{t("buttons.startNewProject")}
-						</Button>
+					{t("buttons.startNewProject")}
+				</Button>
 
-						<div className="relative ml-5 mt-4">
-							<OrStartFromTemplateImage />
+				<div className="relative ml-5 mt-4">
+					<OrStartFromTemplateImage />
 
-							<ArrowStartTemplateIcon className="absolute -right-10 bottom-4" />
-						</div>
-					</>
-				)}
+					<ArrowStartTemplateIcon className="absolute -right-10 bottom-4" />
+				</div>
 			</div>
 			<DeleteProjectModal isDeleting={isDeleting} onDelete={handleProjectDelete} />
 		</div>
