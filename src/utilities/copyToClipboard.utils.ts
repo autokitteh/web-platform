550c252762602cc51b991e2cf6657151c77485f4
Ronen Mars <ronen@autokitteh.com>
2024-12-17 18:59:43 +0200

    fix(UI-1086): fix copy button on not secured urls

--- src/utilities/copyToClipboard.utils.ts
+++ src/utilities/copyToClipboard.utils.ts
@@ -2,7 +2,18 @@ import i18n from "i18next";
 
 export const copyToClipboard = async (text: string): Promise<{ isError: boolean; message: string }> => {
 	try {
-		await navigator.clipboard.writeText(text);
+		if (navigator.clipboard && window.isSecureContext) {
+			await navigator.clipboard.writeText(text);
+		} else {
+			const textArea = document.createElement("textarea");
+			textArea.value = text;
+			textArea.style.position = "fixed";
+			textArea.style.left = "-999999px";
+			document.body.appendChild(textArea);
+			textArea.select();
+			document.execCommand("copy");
+			document.body.removeChild(textArea);
+		}
 
 		return { isError: false, message: i18n.t("copySuccess", { ns: "global" }) };
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
