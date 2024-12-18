a1050e55b20196959ccdb4cf8d7f2a788d1f9633
Ronen Mars <ronen@autokitteh.com>
2024-12-18 11:17:45 +0200

    test(UI-1089): fix variable modification test after react-router upgrade

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,9 +75,9 @@ export default defineConfig({
 		trace: "on",
 		video: "on",
 		screenshot: "on",
-		// extraHTTPHeaders: {
-		// 	Authorization: `Bearer ${process.env.TESTS_JWT_AUTH_TOKEN}`,
-		// },
+		extraHTTPHeaders: {
+			Authorization: `Bearer ${process.env.TESTS_JWT_AUTH_TOKEN}`,
+		},
 	},
 
 	webServer: {
