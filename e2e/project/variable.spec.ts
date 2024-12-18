74d5d4359f224b4be1ca1815aa90039fbeabec53
Ronen Mars <ronen@autokitteh.com>
2024-12-18 10:43:41 +0200

    test(UI-1089): fix variable modification test after react-router upgrade

--- e2e/project/variable.spec.ts
+++ e2e/project/variable.spec.ts
@@ -9,7 +9,7 @@ test.beforeEach(async ({ dashboardPage, page }) => {
 
 	await page.getByLabel("Name").click();
 	await page.getByLabel("Name").fill("nameVariable");
-	await page.getByLabel("Value").click();
+	await page.getByLabel("Value", { exact: true }).click();
 	await page.getByLabel("Value").fill("valueVariable");
 	await page.getByRole("button", { name: "Save", exact: true }).click();
 
