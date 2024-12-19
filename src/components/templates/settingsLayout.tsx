858612985f8ff9cbceeed22ee25036778ed3b001
J1za <workinvs@gmail.com>
2024-12-19 11:02:01 +0200

    refactor(UI-1059): user profile page

--- src/components/templates/settingsLayout.tsx
+++ src/components/templates/settingsLayout.tsx
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 
+import { useUserStore } from "@src/store";
+
 import { LogoCatLarge, PageTitle } from "@components/atoms";
 import { Sidebar, TitleTopbar } from "@components/organisms";
-import { SettingsMenu } from "@components/organisms/settings";
 
 export const SettingsLayout = () => {
 	const { t } = useTranslation("global", { keyPrefix: "pageTitles" });
 	const [pageTitle, setPageTitle] = useState<string>(t("base"));
-	const { t: tSettings } = useTranslation("settings", { keyPrefix: "topbar" });
+	const { user } = useUserStore();
 
 	useEffect(() => {
 		setPageTitle(t("template", { page: t("settings") }));
@@ -28,12 +29,10 @@ export const SettingsLayout = () => {
 					<Sidebar />
 
 					<div className="flex flex-1 flex-col">
-						<TitleTopbar title={tSettings("title")} />
+						<TitleTopbar title={user?.name || ""} />
 
 						<div className="relative flex size-full overflow-hidden py-2">
-							<SettingsMenu />
-
-							<div className="scrollbar flex h-full flex-5 flex-col overflow-y-auto rounded-r-2xl border-l bg-gray-1100 pl-9 pt-6">
+							<div className="scrollbar flex h-full flex-5 flex-col overflow-y-auto rounded-2xl border-l bg-gray-1100 pl-6 pt-6">
 								<Outlet />
 
 								<div className="absolute !-bottom-5 !-right-5">
