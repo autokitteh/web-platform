2d58281cfe5b29c22e7353f18aa533c3a3711f22
Vitaliy Stukalo <workinvs@gmail.com>
2024-12-22 14:38:03 +0200

    fix(UI-1113): fix id-copy-button to be visible on deployments table (#800)

--- src/components/molecules/idCopyButton.tsx
+++ src/components/molecules/idCopyButton.tsx
@@ -10,12 +10,10 @@ import { CopyButton } from "@components/molecules/copyButton";
 export const IdCopyButton = ({
 	buttonClassName,
 	id,
-	onIdClick,
 	variant,
 }: {
 	buttonClassName?: string;
 	id: string;
-	onIdClick?: (id?: string) => void;
 	variant?: ButtonType;
 }) => {
 	const { t } = useTranslation("components", { keyPrefix: "buttons" });
@@ -24,8 +22,7 @@ export const IdCopyButton = ({
 
 	return (
 		<div className="flex flex-row items-center">
-			<Button className={buttonClassName} onClick={() => onIdClick?.(id)} variant={variant}>
-				{" "}
+			<Button className={buttonClassName} variant={variant}>
 				{id.substring(0, 8)}...
 			</Button>
 			<CopyButton className="mb-0.5" size="sm" successMessage={successMessage} text={id} />
