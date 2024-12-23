9ec39c3fa3196d1586dcd8a763e3ba5564b89c66
Ronen Mars <ronen@autokitteh.com>
2024-12-23 19:04:59 +0200

    feat(UI-1042): add missing ui fields in webhook trigger form - refactore

--- src/components/organisms/triggers/formParts/webhook.tsx
+++ src/components/organisms/triggers/formParts/webhook.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Controller, useFormContext, useWatch } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
+import { anyHttpMethod, httpMethodOptions } from "@src/constants/triggers";
 import { cn, getApiBaseUrl } from "@src/utilities";
 import { TriggerFormData } from "@validations";
 
@@ -10,8 +11,6 @@ import { ErrorMessage, Input, Link } from "@components/atoms";
 import { CopyButton, Select } from "@components/molecules";
 
 import { ExternalLinkIcon } from "@assets/image/icons";
-
-const httpMethods = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"];
 
 export const WebhookFields = ({
 	connectionId,
@@ -47,57 +46,56 @@ export const WebhookFields = ({
 	}, []);
 
 	useEffect(() => {
-		setValue("eventTypeSelect", undefined);
+		setValue("eventTypeSelect", anyHttpMethod);
 
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [connectionId]);
 
 	const webhookClassName = cn("w-full", { "shadow-sm shadow-green-200/80": highlight });
 
-	const selectHTTPMethods = httpMethods.map((method) => ({
-		value: method,
-		label: method,
-	}));
-
 	const watchedFilter = useWatch({ control, name: "filter" });
 
 	return (
 		<>
-			<div className="flex flex-col gap-2">
-				<Controller
-					control={control}
-					name="eventTypeSelect"
-					render={({ field }) => (
-						<Select
-							{...field}
-							aria-label={t("placeholders.httpMethod")}
-							defaultValue={selectedHTTPMethodOption}
-							isError={!!errors.eventTypeSelect}
-							label={t("placeholders.httpMethod")}
-							noOptionsLabel={t("placeholders.httpMethodSelect")}
-							options={selectHTTPMethods}
-							placeholder={t("placeholders.httpMethodSelect")}
-							value={watchedEventTypeSelect}
-						/>
-					)}
-				/>
-				<ErrorMessage>{errors.eventTypeSelect?.message as string}</ErrorMessage>
-				<Link
-					className="group inline-flex items-center gap-2.5 pl-1 text-green-800"
-					target="_blank"
-					to={t("webhookInfo.firstLink")}
-				>
-					{t("webhookInfo.firstLinkText")}
-					<ExternalLinkIcon className="size-3.5 fill-green-800 duration-200" />
-				</Link>
-				<Link
-					className="group inline-flex items-center gap-2.5 pl-1 text-green-800"
-					target="_blank"
-					to={t("webhookInfo.secondLink")}
-				>
-					{t("webhookInfo.secondLinkText")}
-					<ExternalLinkIcon className="size-3.5 fill-green-800 duration-200" />
-				</Link>
+			<div className="flex flex-row gap-2">
+				<div className="flex-1">
+					<Controller
+						control={control}
+						name="eventTypeSelect"
+						render={({ field }) => (
+							<Select
+								{...field}
+								aria-label={t("placeholders.httpMethod")}
+								defaultValue={selectedHTTPMethodOption}
+								isError={!!errors.eventTypeSelect}
+								label={t("placeholders.httpMethod")}
+								noOptionsLabel={t("placeholders.httpMethodSelect")}
+								options={httpMethodOptions}
+								placeholder={t("placeholders.httpMethodSelect")}
+								value={watchedEventTypeSelect}
+							/>
+						)}
+					/>
+					<ErrorMessage>{errors.eventTypeSelect?.message as string}</ErrorMessage>
+				</div>
+				<div className="ml-4 flex flex-1 flex-col gap-2">
+					<Link
+						className="group mt-1 inline-flex items-center gap-2.5 pl-1 text-green-800"
+						target="_blank"
+						to={t("webhookInfo.firstLink")}
+					>
+						{t("webhookInfo.firstLinkText")}
+						<ExternalLinkIcon className="size-3.5 fill-green-800 duration-200" />
+					</Link>
+					<Link
+						className="group -mt-1 inline-flex items-center gap-2.5 pl-1 text-green-800"
+						target="_blank"
+						to={t("webhookInfo.secondLink")}
+					>
+						{t("webhookInfo.secondLinkText")}
+						<ExternalLinkIcon className="size-3.5 fill-green-800 duration-200" />
+					</Link>
+				</div>
 			</div>
 			<div className="flex flex-col gap-2">
 				<Input
