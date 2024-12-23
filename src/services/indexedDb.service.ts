6e7c19b950b42916fdcdfefb8bb9663359e71c77
Ronen Mars <ronen@autokitteh.com>
2024-12-23 23:18:08 +0200

    fix(UI-1121): fix db version

--- src/services/indexedDb.service.ts
+++ src/services/indexedDb.service.ts
@@ -14,7 +14,7 @@ export class IndexedDBService {
 
 	async InitDB() {
 		const storeName = this.storeName;
-		this.db = await openDB(this.dbName, 20, {
+		this.db = await openDB(this.dbName, 2, {
 			upgrade: async (db) => {
 				if (db.objectStoreNames.contains(storeName)) {
 					db.deleteObjectStore(storeName);
