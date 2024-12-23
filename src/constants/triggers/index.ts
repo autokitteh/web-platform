30801aa616d474d5961cd0039e51a36e251536aa
Ronen Mars <ronen@autokitteh.com>
2024-12-23 22:15:08 +0200

    feat(UI-1042): add missing ui fields in webhook trigger form (#804)

--- src/constants/triggers/index.ts
+++ src/constants/triggers/index.ts
@@ -1,1 +1,2 @@
 export { eventTypesPerIntegration } from "@constants/triggers/eventTypesPerIntegration.constants";
+export { httpMethodOptions, anyHttpMethod } from "@constants/triggers/webhookHttpMethods.constants";
