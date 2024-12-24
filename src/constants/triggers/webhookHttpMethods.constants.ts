d0e930f504b3a972a84ccbe143b4ea15ad0c1880
Ronen Mars <ronen@autokitteh.com>
2024-12-24 15:07:09 +0200

    fix(UI-1136): in webhook trigger user cant change http method back to any

--- src/constants/triggers/webhookHttpMethods.constants.ts
+++ src/constants/triggers/webhookHttpMethods.constants.ts
@@ -1,3 +1,3 @@
 const httpMethods = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"];
-export const anyHttpMethod = { value: "", label: "Any" };
+export const anyHttpMethod = { value: " ", label: "Any" };
 export const httpMethodOptions = [anyHttpMethod, ...httpMethods.map((method) => ({ value: method, label: method }))];
