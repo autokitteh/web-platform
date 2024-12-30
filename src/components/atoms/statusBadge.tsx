c54ea2a1918064611803757d58bef09d6a1feb93
Ronen Mars <ronen@autokitteh.com>
2024-12-30 12:15:25 +0200

    feat(UI-1129): new status badge cosmetic optimization

--- src/components/atoms/statusBadge.tsx
+++ src/components/atoms/statusBadge.tsx
@@ -7,7 +7,7 @@ import { DeploymentStateVariant } from "@src/enums";
 import { cn } from "@utilities";
 
 export const StatusBadge = ({ className, deploymentStatus }: DeploymentStatusBadgeProps) => {
-	const baseClass = "flex items-center gap-2.5 font-medium capitalize text-gray-500 text-[10px] tracking-widest";
+	const baseClass = "flex items-center gap-2.5 capitalize text-gray-50";
 
 	const badgeClass = cn(
 		baseClass,
@@ -16,7 +16,7 @@ export const StatusBadge = ({ className, deploymentStatus }: DeploymentStatusBad
 		},
 		className
 	);
-	const circleClass = cn("size-3.5 rounded-full relative flex-shrink-0", {
+	const circleClass = cn("size-3 rounded-full relative mb-0.5 flex-shrink-0", {
 		"border-2 border-gray-500 before:absolute  before:rounded-b-lg before:h-1/2 before:bottom-0 before:left-0 before:w-full before:bg-gray-500":
 			deploymentStatus === DeploymentStateVariant.draining,
 		"bg-gray-500": deploymentStatus === DeploymentStateVariant.active,
