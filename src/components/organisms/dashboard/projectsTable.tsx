286f3ccb224945b10d8b775b3edc4b67d865ee47
Ronen Mars <ronen@autokitteh.com>
2025-01-05 14:43:48 +0200

    feat: erase all projects per user

--- src/components/organisms/dashboard/projectsTable.tsx
+++ src/components/organisms/dashboard/projectsTable.tsx
@@ -1,213 +1,68 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-
-import { LoggerService } from "@services";
-import { DeploymentsService } from "@services/deployments.service";
-import { namespaces } from "@src/constants";
+import { DeploymentsService } from "@services";
+import { ProjectsService } from "@services/projects.service";
 import { DeploymentStateVariant } from "@src/enums";
-import { ModalName } from "@src/enums/components";
-import { calculateDeploymentSessionsStats } from "@src/utilities";
-import { DashboardProjectWithStats, Project } from "@type/models";
+import { Project } from "@type/models";
 
-import { useProjectActions, useSort } from "@hooks";
-import { useModalStore, useProjectStore, useToastStore } from "@store";
-
-import { Loader, TBody, Table } from "@components/atoms";
-import { DashboardProjectsTableHeader } from "@components/organisms/dashboard/projectsTableHeader";
-import { DashboardProjectsTableRow } from "@components/organisms/dashboard/projectsTableRow";
-import {
-	DeleteProjectModal,
-	DeleteActiveDeploymentProjectModal,
-	DeleteDrainingDeploymentProjectModal,
-} from "@components/organisms/modals";
+import { useProjectStore } from "@store";
 
 export const DashboardProjectsTable = () => {
-	const { t } = useTranslation("dashboard", { keyPrefix: "projects" });
-	const { t: tDeployments } = useTranslation("deployments");
-	const { t: tProjects } = useTranslation("projects");
-	const { projectsList } = useProjectStore();
-	const navigate = useNavigate();
-	const [projectsStats, setProjectsStats] = useState<DashboardProjectWithStats[]>([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const addToast = useToastStore((state) => state.addToast);
-	const { closeModal, openModal } = useModalStore();
+	const { projectsList, getProjectsList } = useProjectStore();
 
-	const {
-		items: sortedProjectsStats,
-		requestSort,
-		sortConfig,
-	} = useSort<DashboardProjectWithStats>(projectsStats, "name");
-	const { deleteProject, downloadProjectExport, isDeleting, deactivateDeployment } = useProjectActions();
-	const [selectedProjectForDeletion, setSelectedProjectForDeletion] = useState<{
-		activeDeploymentId?: string;
-		projectId?: string;
-	}>({
-		projectId: undefined,
-		activeDeploymentId: undefined,
-	});
+	const [i, setI] = React.useState(0);
 
-	const loadProjectsData = async (projectsList: Project[]) => {
-		const projectsStats = {} as Record<string, DashboardProjectWithStats>;
-		setIsLoading(true);
-		for (const project of projectsList) {
-			const { data: deployments } = await DeploymentsService.list(project.id);
-			let projectStatus = DeploymentStateVariant.inactive;
-			let deploymentId = "";
-			const lastDeployed = deployments?.[deployments?.length - 1]?.createdAt;
-			const { sessionStats, totalDeployments } = calculateDeploymentSessionsStats(deployments || []);
-
-			deployments?.forEach((deployment) => {
-				if (deployment.state === DeploymentStateVariant.active) {
-					projectStatus = DeploymentStateVariant.active;
-					deploymentId = deployment.deploymentId;
-				} else if (
-					deployment.state === DeploymentStateVariant.draining &&
-					projectStatus !== DeploymentStateVariant.active
-				) {
-					projectStatus = DeploymentStateVariant.draining;
-				}
-			});
-
-			projectsStats[project.id] = {
-				id: project.id,
-				name: project.name,
-				totalDeployments,
-				...sessionStats,
-				status: projectStatus,
-				lastDeployed,
-				deploymentId,
-			};
-		}
-		setProjectsStats(Object.values(projectsStats));
-		setIsLoading(false);
-	};
-
-	const handelDeactivateDeployment = useCallback(
-		async (deploymentId: string) => {
-			const { error, deploymentById } = await deactivateDeployment(deploymentId);
-
+	const eraseProjects = async (projectsList: Project[]) => {
+		for (let i = 0; i < projectsList.length; i++) {
+			setI(i);
+			const project = projectsList[i];
+			const { error } = await ProjectsService.delete(project.id);
 			if (error) {
-				addToast({
-					message: tDeployments("deploymentDeactivatedFailed"),
-					type: "error",
-				});
-
-				return;
+				const deactivated = await deactivateProject(project.id);
+				if (deactivated) await ProjectsService.delete(project.id);
 			}
 
-			setProjectsStats((prevStats) =>
-				prevStats.map((project) =>
-					project.deploymentId === deploymentId
-						? { ...project, status: deploymentById?.state || DeploymentStateVariant.unspecified }
-						: project
-				)
+			// After every 10 projects, wait 30 seconds
+			if (i > 0 && i % 10 === 0) {
+				await new Promise((resolve) => setTimeout(resolve, 6000));
+			} else {
+				// Wait 5 seconds between each delete
+				await new Promise((resolve) => setTimeout(resolve, 2000));
+			}
+		}
+	};
+
+	const deactivateProject = async (projectId: string) => {
+		try {
+			const { data: deployments } = await DeploymentsService.list(projectId);
+			const activeDeployment = deployments?.find(
+				(deployment) => deployment.state === DeploymentStateVariant.active
 			);
 
-			addToast({
-				message: tDeployments("history.actions.deploymentDeactivatedSuccessfully"),
-				type: "success",
-			});
-			LoggerService.info(
-				namespaces.ui.deployments,
-				tDeployments("history.actions.deploymentDeactivatedSuccessfullyExtended", { deploymentId })
-			);
-		},
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-		[sortedProjectsStats]
-	);
+			if (activeDeployment) {
+				await DeploymentsService.deactivate(activeDeployment.deploymentId);
+			}
+			return true;
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error("Error deactivating project", projectId, error);
+			return false;
+		}
+	};
+
+	const deactivateAndDelete = async (projectsList: Project[]) => {
+		await getProjectsList();
+		await eraseProjects(projectsList);
+	};
 
 	useEffect(() => {
-		loadProjectsData(projectsList);
+		deactivateAndDelete(projectsList);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [projectsList]);
 
-	const handleProjectDelete = async () => {
-		try {
-			const { activeDeploymentId, projectId } = selectedProjectForDeletion;
-			if (activeDeploymentId) {
-				const { error: errorDeactivateProject } = await deactivateDeployment(activeDeploymentId);
-				if (errorDeactivateProject) {
-					addToast({
-						message: tDeployments("deploymentDeactivatedFailed"),
-						type: "error",
-					});
-					return;
-				}
-			}
-			const { error: errorDeleteProject } = await deleteProject(projectId!);
-			if (errorDeleteProject) {
-				addToast({
-					message: tProjects("errorDeletingProject"),
-					type: "error",
-				});
-				return;
-			}
-
-			addToast({
-				message: tProjects("deleteProjectSuccess"),
-				type: "success",
-			});
-		} catch {
-			addToast({
-				message: t("errorDeletingProject"),
-				type: "error",
-			});
-		} finally {
-			closeModal(ModalName.deleteWithActiveDeploymentProject);
-			closeModal(ModalName.deleteProject);
-			setSelectedProjectForDeletion({ activeDeploymentId: undefined, projectId: undefined });
-		}
-	};
-
-	const displayDeleteModal = async (
-		status: DeploymentStateVariant,
-		deploymentId: string,
-		projectId: string,
-		name: string
-	) => {
-		if (status === DeploymentStateVariant.active) {
-			setSelectedProjectForDeletion({ activeDeploymentId: deploymentId, projectId });
-			openModal(ModalName.deleteWithActiveDeploymentProject);
-			return;
-		}
-		if (status === DeploymentStateVariant.draining) {
-			openModal(ModalName.deleteWithDrainingDeploymentProject);
-			return;
-		}
-
-		setSelectedProjectForDeletion({ projectId });
-		openModal(ModalName.deleteProject, name);
-	};
-
-	return isLoading ? (
-		<Loader isCenter />
-	) : (
-		<div className="z-10 h-1/2 select-none pt-10 md:h-2/3 xl:h-3/4 3xl:h-4/5">
-			{sortedProjectsStats.length ? (
-				<Table className="mt-2.5 h-auto max-h-full rounded-t-20">
-					<DashboardProjectsTableHeader requestSort={requestSort} sortConfig={sortConfig} />
-
-					<TBody className="mr-0">
-						{sortedProjectsStats.map((project) => (
-							<DashboardProjectsTableRow
-								key={project.id}
-								{...project}
-								displayDeleteModal={displayDeleteModal}
-								downloadProjectExport={downloadProjectExport}
-								handelDeactivateDeployment={handelDeactivateDeployment}
-								navigate={navigate}
-							/>
-						))}
-					</TBody>
-				</Table>
-			) : (
-				<div>{t("table.noProjectsFound")}</div>
-			)}
-
-			<DeleteDrainingDeploymentProjectModal />
-			<DeleteActiveDeploymentProjectModal isDeleting={isDeleting} onDelete={handleProjectDelete} />
-			<DeleteProjectModal isDeleting={isDeleting} onDelete={handleProjectDelete} />
+	return (
+		<div className="flex h-screen w-screen items-center justify-center text-4xl text-black">
+			Deleting... {i} out of {projectsList.length}
 		</div>
 	);
 };
