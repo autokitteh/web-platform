22d4eda053db009fdf0ebf3e22171546d35184e7
Ronen Mars <ronen@autokitteh.com>
2025-01-06 16:44:29 +0200

    feat: deactivate and delete projects

--- src/components/organisms/dashboard/projectsTable.tsx
+++ src/components/organisms/dashboard/projectsTable.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 
+import { DeploymentsService } from "@services";
 import { ProjectsService } from "@services/projects.service";
+import { DeploymentStateVariant } from "@src/enums";
 import { Project } from "@type/models";
 
 import { useProjectStore } from "@store";
@@ -10,24 +12,35 @@ export const DashboardProjectsTable = () => {
 
 	const [i, setI] = React.useState(0);
 
-	const eraseProjects = async (projectsList: Project[]) => {
+	const deactivateProjects = async (projectsList: Project[]) => {
 		for (let i = 0; i < projectsList.length; i++) {
 			setI(i);
-			const project = projectsList[i];
-			await ProjectsService.delete(project.id);
+
+			// Get deployments for current project
+			const { data: deployments } = await DeploymentsService.list(projectsList[i].id);
+
+			// Find active deployment if exists
+			const activeDeployment = deployments?.find(
+				(deployment) => deployment.state === DeploymentStateVariant.active
+			);
+
+			if (activeDeployment) {
+				await DeploymentsService.deactivate(activeDeployment.deploymentId);
+				await ProjectsService.delete(projectsList[i].id);
+			}
 
 			// After every 10 projects, wait 30 seconds
 			if (i > 0 && i % 10 === 0) {
 				await new Promise((resolve) => setTimeout(resolve, 30000));
 			} else {
-				// Wait 5 seconds between each delete
+				// Wait 5 seconds between each project
 				await new Promise((resolve) => setTimeout(resolve, 5000));
 			}
 		}
 	};
 
 	useEffect(() => {
-		eraseProjects(projectsList);
+		deactivateProjects(projectsList);
 	}, [projectsList]);
 
 	return (
