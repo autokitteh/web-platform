08bbaa60aba066dcfa95060dd182008e4090b400
Ronen Mars <ronen@autokitteh.com>
2025-01-07 18:37:14 +0200

    feat(UI-1019): events drawer - fix leftovers after rebase

--- src/components/templates/eventsLayout.tsx
+++ src/components/templates/eventsLayout.tsx
@@ -16,21 +16,8 @@ export const EventsLayout = () => {
 			<div className="relative size-full overflow-hidden pt-1.5">
 				<Outlet />
 
-<<<<<<< HEAD
 				<div className="absolute -bottom-5 -right-5">
 					<LogoCatLarge />
-=======
-				<div className="flex flex-1 flex-col">
-					<TitleTopbar title={t("title")} />
-
-					<div className="relative my-2 flex size-full overflow-hidden rounded-2xl">
-						<Outlet />
-
-						<div className="absolute -bottom-5 -right-5">
-							<LogoCatLarge />
-						</div>
-					</div>
->>>>>>> 4d4d633d (feat(UI-1019): refactor events routing and enhance drawer functionality)
 				</div>
 			</div>
 		</SystemLogLayout>
