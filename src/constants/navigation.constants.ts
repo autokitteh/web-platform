6686d6434527cecbd8e6be4db40ffa39499d0074
Ronen Mars <ronen@autokitteh.com>
2025-01-08 12:39:51 +0200

    refactor(UI-1169): rename orgs users to members

--- src/constants/navigation.constants.ts
+++ src/constants/navigation.constants.ts
@@ -31,11 +31,7 @@ export const userMenuOrganizationItems: NavigationSettingsItem[] = [
 	{
 		icon: UserIcon,
 		href: "/organization-settings/members",
-<<<<<<< HEAD
 		label: "Members",
-=======
-		label: "Users",
->>>>>>> 765ef368 (refactor(UI-1169): rename orgs users to members)
 	},
 	{
 		icon: ReceiptIcon,
