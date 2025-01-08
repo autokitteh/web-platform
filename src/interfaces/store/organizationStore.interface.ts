917348315ced14c34848c625fc58c5dbf12f400b
Ronen Mars <ronen@autokitteh.com>
2025-01-08 16:46:58 +0200

    feat(UI-1167): organizations fetching - fix

--- src/interfaces/store/organizationStore.interface.ts
+++ src/interfaces/store/organizationStore.interface.ts
@@ -6,4 +6,5 @@ export interface OrganizationStore {
 	organizationsList?: Organization[];
 	currentOrganizationId?: string;
 	isLoadingOrganizations: boolean;
+	getOrganizationsList: () => ServiceResponseError;
 }
