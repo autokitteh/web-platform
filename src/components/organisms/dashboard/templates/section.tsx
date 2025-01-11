e7a95f9cfaa8f38d2154ce8d78464f595c5d977d
J1za <workinvs@gmail.com>
2025-01-11 18:44:14 +0200

    fix(UI-1175): adjust icon size in ProjectTemplatesSection

--- src/components/organisms/dashboard/templates/section.tsx
+++ src/components/organisms/dashboard/templates/section.tsx
@@ -16,7 +16,7 @@ export const ProjectTemplatesSection = () => {
 				className="mb-7 flex w-full select-none items-center gap-3 font-averta text-3xl font-semibold"
 				element="h2"
 			>
-				<IconSvg className="size-7.5 stroke-white" src={StartTemplateIcon} />
+				<IconSvg className="size-6 stroke-white" src={StartTemplateIcon} />
 
 				{t("title")}
 			</Typography>
