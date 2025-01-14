92a67ffb01c154adade78f227f488fa8492d3624
Ronen Mars <ronen@autokitteh.com>
2025-01-14 18:27:05 +0200

    refactor(UI-1176): optimize id-copy-button component when no id

--- src/components/molecules/idCopyButton.tsx
+++ src/components/molecules/idCopyButton.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 import { useTranslation } from "react-i18next";
 
+import { LoggerService } from "@services/logger.service";
+import { namespaces } from "@src/constants";
+import { ButtonVariant } from "@src/enums/components";
+import { useToastStore } from "@src/store";
+
+import { Button } from "@components/atoms";
 import { CopyButton } from "@components/molecules/copyButton";
+
+import { CopyIcon } from "@assets/image/icons";
 
 export const IdCopyButton = ({
 	displayFullLength,
@@ -14,8 +22,22 @@ export const IdCopyButton = ({
 	id: string;
 }) => {
 	const { t } = useTranslation("components", { keyPrefix: "buttons" });
+	const addToast = useToastStore((state) => state.addToast);
 
 	const successMessage = t("copied");
+
+	if (!id) {
+		const noIdButtonClick = () => {
+			LoggerService.error(namespaces.ui.idCopyButton, t("idCopyButtonNoId"));
+			addToast({ message: t("idCopyButtonNoId"), type: "error" });
+		};
+		return (
+			<Button onClick={noIdButtonClick} title={t("idCopyButtonNoId")} variant={ButtonVariant.flatText}>
+				<CopyIcon className="size-3 fill-white" />
+			</Button>
+		);
+	}
+
 	const idPrefix = id.split("_")[0];
 	const idSuffix = id.split("_")[1];
 	const idSuffixEnd = idSuffix.substring(idSuffix.length - 3, idSuffix.length);
