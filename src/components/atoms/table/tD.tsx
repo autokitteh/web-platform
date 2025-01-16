781cbc584c64cd61e50f191a9ad70850327e62e3
Ronen Mars <ronen@autokitteh.com>
2025-01-16 09:28:07 +0200

    feat(UI-1198): organizations table - refactor

--- src/components/atoms/table/tD.tsx
+++ src/components/atoms/table/tD.tsx
@@ -1,8 +1,9 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { KeyboardEvent } from "react";
 
 import { TableProps } from "@interfaces/components";
-import { cn } from "@src/utilities";
+import { cn } from "@utilities";
+
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 
 export const Td = ({ children, className, onClick, title }: TableProps) => {
 	const tdStyle = cn("flex h-9.5 w-full items-center overflow-hidden", className);
@@ -19,7 +20,7 @@ export const Td = ({ children, className, onClick, title }: TableProps) => {
 	return (
 		<div aria-label={cellTitle} className={tdStyle} role="cell" title={cellTitle}>
 			<div className="flex w-full items-center" onClick={onClick} onKeyDown={handleKeyDown}>
-				<div className="flex w-full truncate pr-2">{children}</div>
+				<div className="w-full truncate pr-2">{children}</div>
 			</div>
 		</div>
 	);
