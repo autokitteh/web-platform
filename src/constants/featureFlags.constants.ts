7127f805f6d416e94a5bad1675167dff7f2e7087
J1za <workinvs@gmail.com>
2025-01-22 16:27:06 +0200

    refactor(UI-1151): remove unused feature flags and clean up user menu

--- src/constants/featureFlags.constants.ts
+++ src/constants/featureFlags.constants.ts
@@ -1,5 +1,4 @@
 export const featureFlags = {
 	displayDiscordIntegration: import.meta.env.DISPLAY_DISCORD_INTEGRATION,
 	displaySlackSocketIntegration: import.meta.env.DISPLAY_SLACK_SOCKET_INTEGRATION,
-	enableNewOrgsAndUsersDesign: import.meta.env.ENABLE_NEW_ORGS_AND_USERS_DESIGN,
 };
