4d31678fb4df22179b6966c780214165c0ae5f22
J1za <workinvs@gmail.com>
2025-01-23 12:02:37 +0200

    fix(UI-1196): adjust active tab in sessions page

--- src/components/organisms/deployments/sessions/viewer.tsx
+++ src/components/organisms/deployments/sessions/viewer.tsx
@@ -94,13 +94,24 @@ export const SessionViewer = () => {
 	}, [sessionInfo, fetchSessionInfo, reloadOutputs, reloadActivities]);
 
 	useEffect(() => {
-		const activeTabIndex = location.pathname.split("/").filter(Boolean)[deploymentId ? 6 : 4] || defaultSessionTab;
-		setActiveTab(activeTabIndex);
-	}, [location, deploymentId]);
+		const pathSegments = location.pathname.split("/");
+		const lastSegment = pathSegments[pathSegments.length - 1];
+
+		const activeTabValue = sessionTabs.some((tab) => tab.value.toLowerCase() === lastSegment)
+			? lastSegment
+			: defaultSessionTab;
+
+		setActiveTab(activeTabValue);
+	}, [location.pathname]);
 
 	const goTo = useCallback(
 		(path: string) => {
-			navigate(path === defaultSessionTab ? "" : path.toLowerCase());
+			if (path === defaultSessionTab) {
+				navigate(".");
+				return;
+			}
+
+			navigate(path.toLowerCase());
 		},
 		[navigate]
 	);
