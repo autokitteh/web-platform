c186c2929153ed4a1c80a19570dac7152ae9a8f3
J1za <workinvs@gmail.com>
2025-01-29 13:28:15 +0200

    feat(UI-1242): add hook for delete organization

--- src/components/organisms/settings/user/organizations/table.tsx
+++ src/components/organisms/settings/user/organizations/table.tsx
@@ -1,39 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-import omit from "lodash/omit";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
-import { LoggerService, ProjectsService } from "@services";
-import { namespaces } from "@src/constants";
 import { MemberRole } from "@src/enums";
-import { ModalName } from "@src/enums/components";
-import { useModalStore, useOrganizationStore, useToastStore } from "@src/store";
+import { useDeleteOrganization } from "@src/hooks";
+import { useOrganizationStore, useToastStore } from "@src/store";
 import { EnrichedOrganization } from "@src/types/models";
 
 import { Button, Typography, IconButton, TBody, THead, Table, Td, Th, Tr, Spinner } from "@components/atoms";
 import { DeleteOrganizationModal } from "@components/organisms/settings/organization";
-import { WarningDeleteOrganizationModal } from "@components/organisms/settings/user/organizations/warningDeleteModal";
+import { WarningDeleteOrganizationModal } from "@components/organisms/settings/user/organizations";
 
 import { TrashIcon } from "@assets/image/icons";
 
 export const UserOrganizationsTable = () => {
 	const { t } = useTranslation("settings", { keyPrefix: "userOrganizations" });
-	const { closeModal, openModal } = useModalStore();
-	const {
-		organizations,
-		getOrganizations,
-		getEnrichedOrganizations,
-		currentOrganization,
-		user,
-		deleteOrganization,
-		isLoading,
-		logoutFunction,
-	} = useOrganizationStore();
+	const { organizations, getOrganizations, getEnrichedOrganizations, user, isLoading } = useOrganizationStore();
 	const addToast = useToastStore((state) => state.addToast);
 	const navigate = useNavigate();
 	const [organizationsList, setOrganizationsList] = useState<EnrichedOrganization[]>();
-	const [organizationIdInDeletion, setOrganizationIdInDeletion] = useState<string>();
+	const { onDelete, organizationIdInDeletion, handleDeleteOrganization } = useDeleteOrganization();
 
 	useEffect(() => {
 		getOrganizations();
@@ -53,40 +40,6 @@ export const UserOrganizationsTable = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [organizations]);
 
-	const onDelete = async (organization: EnrichedOrganization) => {
-		const deletingCurrentOrganization = organization.id === currentOrganization?.id;
-
-		const { error } = await deleteOrganization(omit(organization, "currentMember"));
-		closeModal(ModalName.deleteOrganization);
-		if (error) {
-			addToast({
-				message: t("errors.deleteFailed", {
-					name: organization?.displayName,
-					organizationId: organization?.id,
-				}),
-				type: "error",
-			});
-		}
-
-		addToast({
-			message: t("table.messages.organizationDeleted", { name: organization.displayName }),
-			type: "success",
-		});
-
-		if (!deletingCurrentOrganization) return;
-		setTimeout(async () => {
-			if (!user?.defaultOrganizationId) {
-				LoggerService.error(
-					namespaces.ui.organizationTableUserSettings,
-					t("errors.defaultOrganizationIdMissing", { userId: user?.id })
-				);
-				logoutFunction(true);
-				return;
-			}
-			navigate(`/switch-organization/${user.defaultOrganizationId}`);
-		}, 3000);
-	};
-
 	const isNameInputDisabled = (organizationId: string, organizationRole?: MemberRole): boolean =>
 		!!(
 			isLoading.updatingOrganization ||
@@ -94,30 +47,6 @@ export const UserOrganizationsTable = () => {
 			organizationRole !== MemberRole.admin ||
 			organizationIdInDeletion
 		);
-
-	const handleDeleteOrganization = async (organization: EnrichedOrganization) => {
-		setOrganizationIdInDeletion(organization.id);
-		const { data: orgProjectList, error } = await ProjectsService.list(organization.id);
-		if (error || !orgProjectList) {
-			addToast({
-				message: t("errors.deleteFailed", {
-					name: organization?.displayName,
-					organizationId: organization?.id,
-				}),
-				type: "error",
-			});
-			return;
-		}
-		const hasProjects = orgProjectList?.length > 0;
-		setOrganizationIdInDeletion(undefined);
-
-		if (!hasProjects) {
-			openModal(ModalName.deleteOrganization, organization);
-			return;
-		}
-
-		openModal(ModalName.warningDeleteOrganization, { name: organization.displayName });
-	};
 
 	return (
 		<div className="w-3/4">
