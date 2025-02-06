26b79d9928c020784f19271231e9799f368b57c7
J1za <workinvs@gmail.com>
2025-02-06 18:03:22 +0200

    feat(UI-1232): add functionality in user feedback form

--- src/validations/userFeedback.schema.ts
+++ src/validations/userFeedback.schema.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
 export const userFeedbackSchema = z.object({
-	name: z.string().min(1, "Name is required"),
-	email: z.string().nonempty("Email is required").email("Email is invalid"),
 	message: z.string().min(1, "Message is required"),
 });
