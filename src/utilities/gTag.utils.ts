6cc02787d729b80e637ebe5c8d5f337f1e0db9a0
Ronen Mars <ronen@autokitteh.com>
2025-02-10 11:57:25 +0200

    fix(UI-1316): gtm

--- src/utilities/gTag.utils.ts
+++ src/utilities/gTag.utils.ts
@@ -1,5 +1,8 @@
 export const gTagEvent = (eventName: string, eventParams: object) => {
 	window.dataLayer = window.dataLayer || [];
+	console.log("gTagEvent", eventName, eventParams);
+	console.log("window.dataLayer", window.dataLayer);
+
 	window.dataLayer.push({
 		event: eventName,
 		eventParams,
