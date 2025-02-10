7ca52cee61a3c2faccb82350bfb7ce1c37c6db64
Ronen Mars <ronen@autokitteh.com>
2025-02-10 18:37:22 +0200

    fix(UI-1316): fix the google tag manager script

--- src/utilities/gTag.utils.ts
+++ src/utilities/gTag.utils.ts
@@ -1,8 +1,5 @@
 export const gTagEvent = (eventName: string, eventParams: object) => {
 	window.dataLayer = window.dataLayer || [];
-	console.log("gTagEvent", eventName, eventParams);
-	console.log("window.dataLayer", window.dataLayer);
-
 	window.dataLayer.push({
 		event: eventName,
 		eventParams,
