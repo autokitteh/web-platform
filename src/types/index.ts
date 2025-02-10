f8bb0b6cbb00acdfb8a9536a69b5dc7412234257
Ronen Mars <ronen@autokitteh.com>
2025-02-10 11:48:48 +0200

    fix(UI-1316): gtm

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,3 @@ export type { ServiceResponse, ServiceResponseError } from "@type/services.types
 export type { SortConfig } from "@type/sortConfig.type";
 export type { StartSessionArgsType } from "@type/startSessionArgs.type";
 export type { ColorSchemes, SystemSizes, TextSizes } from "@type/theme.type";
-
-declare global {
-	interface Window {
-		gtag: (type: string, action: string, options: object) => void;
-	}
-}
