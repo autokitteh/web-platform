9527f7f72f84c0211e251d72dfd88d3173dff9dd
J1za <workinvs@gmail.com>
2025-02-18 13:00:59 +0200

    feat(UI-1347): refactor Slack OAuth form

--- src/components/organisms/connections/integrations/slack/authMethods/oauthPrivate.tsx
+++ src/components/organisms/connections/integrations/slack/authMethods/oauthPrivate.tsx
@@ -5,107 +5,79 @@ import { useTranslation } from "react-i18next";
 
 import { Button, ErrorMessage, Input, SecretInput, Spinner } from "@components/atoms";
 
-export const SlackOauthPrivateForm = ({
+const initialLockState: Record<string, boolean> = {
+	clientSecret: true,
+	webhookSecret: true,
+	signingSecret: true,
+};
+
+const formFields = [
+	{ name: "client_id", translate: "clientId", requiresSecret: false },
+	{ name: "client_secret", translate: "clientSecret", requiresSecret: true },
+	{ name: "signing_secret", translate: "signingSecret", requiresSecret: true },
+] as const;
+
+interface SlackOauthPrivateFormProps {
+	control: any;
+	errors: FieldErrors<any>;
+	isLoading: boolean;
+	mode: "create" | "edit";
+	register: UseFormRegister<{ [key: string]: any }>;
+	setValue: (name: string, value: any) => void;
+}
+
+export const SlackOauthPrivateForm: React.FC<SlackOauthPrivateFormProps> = ({
 	control,
 	errors,
 	isLoading,
 	mode,
 	register,
 	setValue,
-}: {
-	control: any;
-	errors: FieldErrors<any>;
-	isLoading: boolean;
-	mode: "create" | "edit";
-	register: UseFormRegister<{ [x: string]: any }>;
-	setValue: any;
 }) => {
-	const [lockState, setLockState] = useState<{
-		clientSecret: boolean;
-		signingSecret: boolean;
-		webhookSecret: boolean;
-	}>({
-		clientSecret: true,
-		webhookSecret: true,
-		signingSecret: true,
-	});
+	const [lockState, setLockState] = useState(initialLockState);
 	const { t } = useTranslation("integrations");
-
-	const clientId = useWatch({ control, name: "client_id" });
-	const clientSecret = useWatch({ control, name: "client_secret" });
-	const signingSecret = useWatch({ control, name: "signing_secret" });
-
 	const isEditMode = mode === "edit";
+
+	const values = useWatch({
+		control,
+		name: formFields.map((f) => f.name),
+	});
+
+	const handleLockAction = (fieldName: string, newLockState: boolean) => {
+		setLockState((prev) => ({ ...prev, [fieldName]: newLockState }));
+	};
 
 	return (
 		<>
-			<div className="relative">
-				<Input
-					{...register("client_id")}
-					aria-label={t("slack.placeholders.clientId")}
-					isError={!!errors.client_id}
-					isRequired
-					label={t("slack.placeholders.clientId")}
-					value={clientId}
-				/>
-				<ErrorMessage>{errors.client_id?.message as string}</ErrorMessage>
-			</div>
-			<div className="relative">
-				{isEditMode ? (
-					<SecretInput
-						type="password"
-						{...register("client_secret")}
-						aria-label={t("slack.placeholders.clientSecret")}
-						handleInputChange={(newValue) => setValue("client_secret", newValue)}
-						handleLockAction={(newLockState) =>
-							setLockState((prevState) => ({ ...prevState, clientSecret: newLockState }))
-						}
-						isError={!!errors.client_secret}
-						isLocked={lockState.clientSecret}
-						isRequired
-						label={t("slack.placeholders.clientSecret")}
-						value={clientSecret}
-					/>
-				) : (
-					<Input
-						{...register("client_secret")}
-						aria-label={t("slack.placeholders.clientSecret")}
-						isError={!!errors.client_secret}
-						isRequired
-						label={t("slack.placeholders.clientSecret")}
-						value={clientSecret}
-					/>
-				)}
-				<ErrorMessage>{errors.client_secret?.message as string}</ErrorMessage>
-			</div>
-			<div className="relative">
-				{isEditMode ? (
-					<SecretInput
-						type="password"
-						{...register("signing_secret")}
-						aria-label={t("slack.placeholders.signingSecret")}
-						handleInputChange={(newValue) => setValue("signing_secret", newValue)}
-						handleLockAction={(newLockState) =>
-							setLockState((prevState) => ({ ...prevState, signingSecret: newLockState }))
-						}
-						isError={!!errors.signing_secret}
-						isLocked={lockState.signingSecret}
-						isRequired
-						label={t("slack.placeholders.signingSecret")}
-						value={signingSecret}
-					/>
-				) : (
-					<Input
-						{...register("signing_secret")}
-						aria-label={t("slack.placeholders.signingSecret")}
-						isError={!!errors.signing_secret}
-						isRequired
-						label={t("slack.placeholders.signingSecret")}
-						value={signingSecret}
-					/>
-				)}
-				<ErrorMessage>{errors.signing_secret?.message as string}</ErrorMessage>
-			</div>
+			{formFields.map(({ name, translate, requiresSecret }) => {
+				const label = t(`slack.placeholders.${translate}`);
+				const error = errors[name]?.message as string;
+				const commonProps = {
+					...register(name),
+					"aria-label": label,
+					"isError": !!errors[name],
+					"isRequired": true,
+					label,
+					"value": values[name as any],
+				};
+
+				return (
+					<div className="relative" key={name}>
+						{isEditMode && requiresSecret ? (
+							<SecretInput
+								type="password"
+								{...commonProps}
+								handleInputChange={(newValue) => setValue(name, newValue)}
+								handleLockAction={(newLockState) => handleLockAction(name, newLockState)}
+								isLocked={lockState[name]}
+							/>
+						) : (
+							<Input {...commonProps} />
+						)}
+						<ErrorMessage>{error}</ErrorMessage>
+					</div>
+				);
+			})}
 
 			<Button
 				aria-label={t("buttons.startPrivateOAuthFlow")}
