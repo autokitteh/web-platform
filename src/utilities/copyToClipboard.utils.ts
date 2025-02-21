9f92d3647ef6e551215fd3cd4760cf7851381efa
Ronen Mars <ronen@autokitteh.com>
2025-02-21 13:52:05 +0200

    fix(UI-1387): fix copy function

--- src/utilities/copyToClipboard.utils.ts
+++ src/utilities/copyToClipboard.utils.ts
@@ -1,23 +1,34 @@
 import i18n from "i18next";
 
-export const copyToClipboard = async (text: string): Promise<{ isError: boolean; message: string }> => {
+const retryCopyToClipboard = (text: string): { isError: boolean; message: string } => {
 	try {
-		if (navigator.clipboard && window.isSecureContext) {
-			await navigator.clipboard.writeText(text);
-		} else {
-			const textArea = document.createElement("textarea");
-			textArea.value = text;
-			textArea.style.position = "fixed";
-			textArea.style.left = "-999999px";
-			document.body.appendChild(textArea);
-			textArea.select();
-			document.execCommand("copy");
-			document.body.removeChild(textArea);
-		}
+		const textArea = document.createElement("textarea");
+		textArea.value = text;
+		textArea.style.position = "fixed";
+		textArea.style.left = "-999999px";
+		document.body.appendChild(textArea);
+		textArea.select();
+		document.execCommand("copy");
+		document.body.removeChild(textArea);
 
 		return { isError: false, message: i18n.t("copySuccess", { ns: "global" }) };
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (error) {
 		return { isError: true, message: i18n.t("copyFailure", { ns: "global" }) };
+	}
+};
+
+export const copyToClipboard = async (text: string): Promise<{ isError: boolean; message: string }> => {
+	try {
+		if (navigator.clipboard && window.isSecureContext) {
+			await navigator.clipboard.writeText(text);
+		} else {
+			return retryCopyToClipboard(text);
+		}
+
+		return { isError: false, message: i18n.t("copySuccess", { ns: "global" }) };
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		return retryCopyToClipboard(text);
 	}
 };
