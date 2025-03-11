490bc674ee33c5c2f740da4a831aa6a68a07c30b
J1za <workinvs@gmail.com>
2025-03-11 18:31:36 +0200

    feat(UI-1405): fix issues

--- src/components/organisms/dashboard/templates/catalog.tsx
+++ src/components/organisms/dashboard/templates/catalog.tsx
@@ -8,6 +8,7 @@ import { integrationTypes } from "@constants/lists";
 import { ModalName } from "@src/enums/components";
 import { TemplateMetadata } from "@src/interfaces/store";
 import { useModalStore, useTemplatesStore } from "@src/store";
+import { cn } from "@src/utilities";
 
 import { Frame, IconSvg, Loader, Typography } from "@components/atoms";
 import {
@@ -15,112 +16,112 @@ import {
 	MultiplePopoverSelect,
 	ProjectTemplateCreateModalContainer,
 } from "@components/organisms/dashboard/templates/tabs";
+
 import { StartTemplateIcon } from "@assets/image/icons";
-import { cn } from "@src/utilities";
 
 export const TemplatesCatalog = ({ fullScreen }: { fullScreen?: boolean }) => {
 	const { t } = useTranslation("dashboard", { keyPrefix: "templates" });
 	const [selectedTemplate, setSelectedTemplate] = useState<TemplateMetadata>();
 
-		const { openModal } = useModalStore();
-		const [parent] = useAutoAnimate();
-		const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-		const [selectedIntegrations, setSelectedIntegrations] = useState<string[]>([]);
+	const { openModal } = useModalStore();
+	const [parent] = useAutoAnimate();
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+	const [selectedIntegrations, setSelectedIntegrations] = useState<string[]>([]);
 	const { error, fetchTemplates, isLoading, sortedCategories: categories } = useTemplatesStore();
 
 	useEffect(() => {
-			fetchTemplates();
-		}, [fetchTemplates]);
-	
-		const handleCardCreateClick = useCallback(
-			(template: TemplateMetadata) => {
-				setSelectedTemplate(template);
-				openModal(ModalName.templateCreateProject);
-			},
-			[openModal]
+		fetchTemplates();
+	}, [fetchTemplates]);
+
+	const handleCardCreateClick = useCallback(
+		(template: TemplateMetadata) => {
+			setSelectedTemplate(template);
+			openModal(ModalName.templateCreateProject);
+		},
+		[openModal]
+	);
+
+	const allTemplates = useMemo(() => {
+		if (!categories) return [];
+		return categories.flatMap((cat) => cat.templates);
+	}, [categories]);
+
+	const isDefaultSelected = (list: string[]) => {
+		return list.length === 0 || list.includes(defaultSelectedMultipleSelect);
+	};
+
+	const templatesByCategory = useMemo(() => {
+		if (isDefaultSelected(selectedCategories)) {
+			return allTemplates;
+		}
+		return allTemplates.filter((template) => selectedCategories.includes(template.category));
+	}, [allTemplates, selectedCategories]);
+
+	const filteredTemplates = useMemo(() => {
+		if (isDefaultSelected(selectedIntegrations)) {
+			return templatesByCategory;
+		}
+		return templatesByCategory.filter((template) =>
+			selectedIntegrations.every((integration) => template.integrations.includes(integration))
 		);
-	
-		const allTemplates = useMemo(() => {
-			if (!categories) return [];
-			return categories.flatMap((cat) => cat.templates);
-		}, [categories]);
-	
-		const isDefaultSelected = (list: string[]) => {
-			return list.length === 0 || list.includes(defaultSelectedMultipleSelect);
-		};
-	
-		const templatesByCategory = useMemo(() => {
-			if (isDefaultSelected(selectedCategories)) {
-				return allTemplates;
-			}
-			return allTemplates.filter((template) => selectedCategories.includes(template.category));
-		}, [allTemplates, selectedCategories]);
-	
-		const filteredTemplates = useMemo(() => {
-			if (isDefaultSelected(selectedIntegrations)) {
-				return templatesByCategory;
-			}
-			return templatesByCategory.filter((template) =>
-				selectedIntegrations.every((integration) => template.integrations.includes(integration))
-			);
-		}, [templatesByCategory, selectedIntegrations]);
-	
-		const filteredCategories = useMemo(() => {
-			if (!categories) return [];
-	
-			if (isDefaultSelected(selectedIntegrations)) {
-				return categories.map((category) => ({
-					...category,
-					count: category.templates.length,
-					isVisible: true,
-				}));
-			}
-	
-			const filteredCategoryNames = new Set(filteredTemplates.map((template) => template.category));
-	
+	}, [templatesByCategory, selectedIntegrations]);
+
+	const filteredCategories = useMemo(() => {
+		if (!categories) return [];
+
+		if (isDefaultSelected(selectedIntegrations)) {
 			return categories.map((category) => ({
 				...category,
-				count: filteredTemplates.filter((template) => template.category === category.name).length,
-				isVisible: filteredCategoryNames.has(category.name),
+				count: category.templates.length,
+				isVisible: true,
 			}));
-		}, [categories, filteredTemplates, selectedIntegrations]);
-	
-		const filteredIntegrations = useMemo(() => {
-			if (!integrationTypes) return [];
-	
-			const filteredIntegrationNames = new Set(filteredTemplates.flatMap((template) => template.integrations));
-	
-			return integrationTypes.map((integration) => ({
-				...integration,
-				count: filteredTemplates.filter((template) => template.integrations.includes(integration.value)).length,
-				isVisible: filteredIntegrationNames.has(integration.value),
-			}));
-		}, [filteredTemplates]);
-	
-		const popoverCategoriesItems = useMemo(
-			() =>
-				filteredCategories
-					.filter((category) => category.isVisible)
-					.map(({ name, count }) => ({
-						id: name,
-						label: name,
-						count,
-					})),
-			[filteredCategories]
-		);
-	
-		const popoverIntegrationsItems = useMemo(
-			() =>
-				filteredIntegrations
-					.filter((integration) => integration.isVisible)
-					.map(({ icon, label, value, count }) => ({
-						id: value,
-						label,
-						icon,
-						count,
-					})),
-			[filteredIntegrations]
-		);
+		}
+
+		const filteredCategoryNames = new Set(filteredTemplates.map((template) => template.category));
+
+		return categories.map((category) => ({
+			...category,
+			count: filteredTemplates.filter((template) => template.category === category.name).length,
+			isVisible: filteredCategoryNames.has(category.name),
+		}));
+	}, [categories, filteredTemplates, selectedIntegrations]);
+
+	const filteredIntegrations = useMemo(() => {
+		if (!integrationTypes) return [];
+
+		const filteredIntegrationNames = new Set(filteredTemplates.flatMap((template) => template.integrations));
+
+		return integrationTypes.map((integration) => ({
+			...integration,
+			count: filteredTemplates.filter((template) => template.integrations.includes(integration.value)).length,
+			isVisible: filteredIntegrationNames.has(integration.value),
+		}));
+	}, [filteredTemplates]);
+
+	const popoverCategoriesItems = useMemo(
+		() =>
+			filteredCategories
+				.filter((category) => category.isVisible)
+				.map(({ name, count }) => ({
+					id: name,
+					label: name,
+					count,
+				})),
+		[filteredCategories]
+	);
+
+	const popoverIntegrationsItems = useMemo(
+		() =>
+			filteredIntegrations
+				.filter((integration) => integration.isVisible)
+				.map(({ icon, label, value, count }) => ({
+					id: value,
+					label,
+					icon,
+					count,
+				})),
+		[filteredIntegrations]
+	);
 
 	const frameClass = cn("h-full rounded-none border-l border-l-gray-750 bg-gray-1250", {
 		"mt-1.5 rounded-2xl": fullScreen,
@@ -133,7 +134,6 @@ export const TemplatesCatalog = ({ fullScreen }: { fullScreen?: boolean }) => {
 				element="h2"
 			>
 				<IconSvg className="size-6 fill-white" src={StartTemplateIcon} />
-
 				{t("title")}
 			</Typography>
 
@@ -143,33 +143,33 @@ export const TemplatesCatalog = ({ fullScreen }: { fullScreen?: boolean }) => {
 					<Loader isCenter />
 				) : (
 					<>
-					<div className="grid grid-cols-2 gap-4">
-											<MultiplePopoverSelect
-												defaultSelectedItems={[defaultSelectedMultipleSelect]}
-												emptyListMessage={t("noCategories")}
-												items={popoverCategoriesItems}
-												label={t("categories")}
-												onItemsSelected={setSelectedCategories}
-											/>
-											<MultiplePopoverSelect
-												defaultSelectedItems={[defaultSelectedMultipleSelect]}
-												emptyListMessage={t("noIntegrations")}
-												items={popoverIntegrationsItems}
-												label={t("integrations")}
-												onItemsSelected={setSelectedIntegrations}
-											/>
-										</div>
-					
-										<div className="mt-4 grid grid-cols-auto-fit-248 gap-x-4 gap-y-5 pb-5 text-black" ref={parent}>
-											{filteredTemplates.map((template) => (
-												<ProjectTemplateCard
-													category={template.category}
-													key={template.title}
-													onCreateClick={() => handleCardCreateClick(template)}
-													template={template}
-												/>
-											))}
-										</div>
+						<div className="grid grid-cols-2 gap-4">
+							<MultiplePopoverSelect
+								defaultSelectedItems={[defaultSelectedMultipleSelect]}
+								emptyListMessage={t("noCategories")}
+								items={popoverCategoriesItems}
+								label={t("categories")}
+								onItemsSelected={setSelectedCategories}
+							/>
+							<MultiplePopoverSelect
+								defaultSelectedItems={[defaultSelectedMultipleSelect]}
+								emptyListMessage={t("noIntegrations")}
+								items={popoverIntegrationsItems}
+								label={t("integrations")}
+								onItemsSelected={setSelectedIntegrations}
+							/>
+						</div>
+
+						<div className="mt-4 grid grid-cols-auto-fit-248 gap-x-4 gap-y-5 pb-5 text-black" ref={parent}>
+							{filteredTemplates.map((template) => (
+								<ProjectTemplateCard
+									category={template.category}
+									key={template.title}
+									onCreateClick={() => handleCardCreateClick(template)}
+									template={template}
+								/>
+							))}
+						</div>
 					</>
 				)}
 				{selectedTemplate ? <ProjectTemplateCreateModalContainer template={selectedTemplate} /> : null}
--- src/components/organisms/dashboard/templates/tabs/index.ts
+++ src/components/organisms/dashboard/templates/tabs/index.ts
@@ -1,7 +1,6 @@
 export { ProjectTemplateCard } from "@components/organisms/dashboard/templates/tabs/card";
 export { ProjectTemplateCreateModal } from "@components/organisms/dashboard/templates/tabs/createModal";
 export { ProjectTemplateCreateModalContainer } from "@components/organisms/dashboard/templates/tabs/createModalWrapper";
-export { ProjectTemplatesTabs } from "@components/organisms/dashboard/templates/tabs/tabs";
 export { MultipleLabelPopoverItem } from "@components/organisms/dashboard/templates/tabs/multipleLabelPopoverItem";
 export { IntegrationsMenuPopoverItem } from "@components/organisms/dashboard/templates/tabs/integrationsMenuPopoverItem";
 export { MultiplePopoverSelect } from "@components/organisms/dashboard/templates/tabs/multiplePopoverSelect";
