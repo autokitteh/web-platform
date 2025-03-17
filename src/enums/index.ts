91c70cc48f11e72d4e574bece3e82343192bc78a
Ronen Mars <ronen@autokitteh.com>
2025-03-17 13:57:00 +0200

    feat(UI-1433): create onboarding guided tour

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -17,3 +17,4 @@ export { StoreName } from "@enums/storeName.enum";
 export { TriggerType, TriggerTypes } from "@enums/trigger.enum";
 export { UserStatus, UserStatusType } from "@enums/userStatus.enum";
 export { EventListenerName } from "@enums/eventListenerNames.enum";
+export { TourId } from "@enums/tour.enum";
