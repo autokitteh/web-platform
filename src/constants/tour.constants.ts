773c05684c63a81b7632052a51f7b3638531fa12
Ronen Mars <ronen@autokitteh.com>
2025-03-20 16:02:20 +0200

    feat(UI-1433): create onboarding guided tour - fine tunning

--- src/constants/tour.constants.ts
+++ src/constants/tour.constants.ts
@@ -1,39 +1,40 @@
+import { t } from "i18next";
 import { TourId } from "@enums";
 import { Tour } from "@src/interfaces/store";
 
 export const tours: Record<string, Tour> = {
-	[TourId.onboarding]: {
-		id: TourId.onboarding,
-		name: "Onboarding Tour",
-		navigateOnComplete: "/projects/:projectId/sessions",
-		steps: [
-			{
-				id: "tourDeployButton",
-				targetElementId: "tourDeployButton",
-				title: "Deploy Project",
-				content: "Click to deploy your project's code",
-				placement: "bottom",
-				highlight: true,
-				actionElementId: "tourDeployButton",
-			},
-			{
-				id: "tourManualRunButton",
-				targetElementId: "tourManualRunButton",
-				title: "Manual Run",
-				content: "Manually run your deployed project to test it",
-				placement: "bottom",
-				highlight: true,
-				actionElementId: "tourManualRunButton",
-			},
-			{
-				id: "tourSessionsTopNav",
-				targetElementId: "tourSessionsTopNav",
-				title: "View Sessions",
-				content: "View all sessions for your project here",
-				placement: "bottom",
-				highlight: true,
-				actionElementId: "tourSessionsTopNav",
-			},
-		],
-	},
+    [TourId.onboarding]: {
+        id: TourId.onboarding,
+        name: t("onboarding.name", { ns: "tour" }),
+        navigateOnComplete: "/projects/:projectId/sessions",
+        steps: [
+            {
+                id: "tourDeployButton",
+                targetElementId: "tourDeployButton",
+                title: t("onboarding.steps.deployButton.title", { ns: "tour" }),
+                content: t("onboarding.steps.deployButton.content", { ns: "tour" }),
+                placement: "bottom",
+                highlight: true,
+                actionElementId: "tourDeployButton",
+            },
+            {
+                id: "tourManualRunButton",
+                targetElementId: "tourManualRunButton",
+                title: t("onboarding.steps.manualRunButton.title", { ns: "tour" }),
+                content: t("onboarding.steps.manualRunButton.content", { ns: "tour" }),
+                placement: "bottom",
+                highlight: true,
+                actionElementId: "tourManualRunButton",
+            },
+            {
+                id: "tourSessionsTopNav",
+                targetElementId: "tourSessionsTopNav",
+                title: t("onboarding.steps.sessionsTopNav.title", { ns: "tour" }),
+                content: t("onboarding.steps.sessionsTopNav.content", { ns: "tour" }),
+                placement: "bottom",
+                highlight: true,
+                actionElementId: "tourSessionsTopNav",
+            },
+        ],
+    },
 };
