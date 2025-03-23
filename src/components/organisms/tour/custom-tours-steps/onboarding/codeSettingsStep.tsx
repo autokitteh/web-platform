748453ee742d77f3dc8421dae3a23803533ba41a
Ronen Mars <ronen@autokitteh.com>
2025-03-23 23:27:48 +0200

    feat(UI-1433): create onboarding guided tour - arrows

--- src/components/organisms/tour/custom-tours-steps/onboarding/codeSettingsStep.tsx
+++ src/components/organisms/tour/custom-tours-steps/onboarding/codeSettingsStep.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { useTranslation } from "react-i18next";
 
-import { ChevronLeftIcon, ChevronRightIcon } from "@src/assets/image/icons";
-
 import { Typography } from "@components/atoms";
 
 export const CodeSettingsStep = () => {
@@ -11,6 +9,32 @@ export const CodeSettingsStep = () => {
 
 	return (
 		<div className="flex w-full flex-col gap-6">
+			<div className="flex w-full items-center justify-between">
+				<div className="w-full flex-1">
+					<Typography className="font-semibold text-white" element="h4" size="large">
+						{t("onboarding.steps.projectSettings.title")}
+					</Typography>
+					<Typography className="mt-1 text-gray-300" element="p" size="small">
+						{t("onboarding.steps.projectSettings.content")}
+
+						<div
+							className="dot absolute left-[-30vw] z-10 sm:left-[-25vw] md:left-[-20vw] lg:left-[-17vw] lg:top-[-26.5vh]"
+							style={
+								{
+									transform: "rotate(90deg) scaleX(-1) scaleY(-1)",
+									"--c": "#BCF870",
+									width: "13vw",
+									height: "36vh",
+									"--r": "7px",
+								} as React.CSSProperties
+							}
+						/>
+					</Typography>
+				</div>
+			</div>
+
+			<div className="h-px w-full bg-gray-700" />
+
 			<div className="flex items-center justify-between">
 				<div className="flex-1">
 					<Typography className="font-semibold text-white" element="h4" size="xl">
@@ -18,21 +42,19 @@ export const CodeSettingsStep = () => {
 					</Typography>
 					<Typography className="mt-1 text-gray-300" element="p" size="small">
 						{t("onboarding.steps.projectCode.content")}
-					</Typography>
-				</div>
-				<ChevronRightIcon className="ml-4 size-7 shrink-0 fill-white" />
-			</div>
 
-			<div className="h-px w-full bg-gray-700" />
-
-			<div className="flex w-full items-center justify-between">
-				<ChevronLeftIcon className="mr-4 size-7 shrink-0 fill-white" />
-				<div className="w-full flex-1">
-					<Typography className="font-semibold text-white" element="h4" size="large">
-						{t("onboarding.steps.projectSettings.title")}
-					</Typography>
-					<Typography className="mt-1 text-gray-300" element="p" size="small">
-						{t("onboarding.steps.projectSettings.content")}
+						<div
+							className="dot absolute right-[-20vw] z-10 sm:right-[-25vw] md:right-[-20vw] lg:right-[-6vw] lg:top-[4vh]"
+							style={
+								{
+									transform: "rotate(90deg) scaleX(-1)",
+									"--c": "#BCF870",
+									width: "6vw",
+									height: "8vh",
+									"--r": "7px",
+								} as React.CSSProperties
+							}
+						/>
 					</Typography>
 				</div>
 			</div>
