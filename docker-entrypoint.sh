0e477429fe333cd50ad9e894c464b9dff27fe025
Ronen Mars <ronen@autokitteh.com>
2025-03-26 16:39:54 +0200

    feat: backend proxy feature

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,19 +7,20 @@ if [ -n "$NGNIX_PROXY_API_URL" ]; then
         echo "API_URL must be provided when NGNIX_PROXY_API_URL is enabled."
         exit 1
     fi
-    if [ "${API_URL: -1}" != "/" ]; then
-        API_URL="${API_URL}/"
-        echo "Adding trailing slash to API_URL: $API_URL"
+    if [ "${NGNIX_PROXY_API_URL: -1}" != "/" ]; then
+        NGNIX_PROXY_API_URL="${NGNIX_PROXY_API_URL}/"
+        echo "Adding trailing slash to NGNIX_PROXY_API_URL: $NGNIX_PROXY_API_URL"
     fi
 
-    echo "Adding NGINX API routing configuration for $API_URL"
-    sed -i 's|api.autokitteh.cloud|'${API_URL}'|' /app/nginx.conf.template > /etc/nginx/conf.d/default.conf
+    echo "Adding NGINX API routing configuration for $NGNIX_PROXY_API_URL"
+    sed -i 's|https://api.autokitteh.cloud|'"${NGNIX_PROXY_API_URL}"'|' /app/nginx.conf.template
+    cp /app/nginx.conf.template /etc/nginx/conf.d/default.conf
 
     echo "Adding appBaseUrl=/api configuration for react app"
-    sed -i 's/appBaseUrl = ""/appBaseUrl = "'/api'"/' /usr/share/nginx/html/index.html
+    sed -i 's|appBaseUrl=""|appBaseUrl="/api"|' /usr/share/nginx/html/index.html
 elif [ -n "$API_URL" ]; then
-    echo "Adding appBaseUrl=/api configuration for react app"
-    sed -i 's/appBaseUrl = ""/appBaseUrl = "'"$API_URL"'"/' /usr/share/nginx/html/index.html
+    echo "Adding appBaseUrl="$API_URL" configuration for react app"
+    sed -i 's|appBaseUrl=""|appBaseUrl="'"$API_URL"'"|' /usr/share/nginx/html/index.html
 fi
 
 
