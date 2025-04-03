202ed326398840a4b09a8fec3cce8e1d1c6795b5
J1za <workinvs@gmail.com>
2025-04-03 12:21:51 +0300

    refactor(UI-1431): improve style

--- src/components/organisms/deployments/sessions/tabs/activities/singleActivityInfo.tsx
+++ src/components/organisms/deployments/sessions/tabs/activities/singleActivityInfo.tsx
@@ -49,32 +49,12 @@ export const SingleActivityInfo = ({
 					<div className="mb-4 mt-8 font-bold">{t("arguments")}:</div>
 
 					{activity?.args?.length ? (
-						<div className="rounded-md border-2 border-dashed border-gray-400 p-2 pt-0">
-							<svg
-								className="stroke-gray-400"
-								fill="none"
-								height="24"
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								viewBox="0 0 24 24"
-								width="24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M0 0h24v24H0z" fill="none" stroke="none" />
-								<path d="M20 16v-8l3 8v-8" />
-								<path d="M15 8a2 2 0 0 1 2 2v4a2 2 0 1 1 -4 0v-4a2 2 0 0 1 2 -2z" />
-								<path d="M1 8h3v6.5a1.5 1.5 0 0 1 -3 0v-.5" />
-								<path d="M7 15a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-2a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1h1a1 1 0 0 1 1 1" />
-							</svg>
-							<JsonView
-								className="scrollbar max-h-96 overflow-auto !bg-transparent"
-								collapsed={true}
-								style={githubDarkTheme}
-								value={mappedArguments}
-							/>
-						</div>
+						<JsonView
+							className="scrollbar max-h-96 overflow-auto rounded-md border border-gray-1000 !bg-transparent p-2"
+							collapsed={true}
+							style={githubDarkTheme}
+							value={mappedArguments}
+						/>
 					) : (
 						<div>{t("noArgumentsFound")}</div>
 					)}
@@ -83,7 +63,7 @@ export const SingleActivityInfo = ({
 
 					{activity.kwargs && !!Object.keys(activity.kwargs).length ? (
 						<JsonView
-							className="scrollbar max-h-96 overflow-auto rounded-md border-2 border-dashed border-gray-400 !bg-transparent p-2"
+							className="scrollbar max-h-96 overflow-auto rounded-md border border-gray-1000 !bg-transparent p-2"
 							collapsed={true}
 							style={githubDarkTheme}
 							value={activity.kwargs}
