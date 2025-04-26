28788cd63fc385061a35717ef8a4ca2e9950d299
Ronen Mars <ronen@autokitteh.com>
2025-04-26 15:58:50 +0300

    feat: limits - try to fix headless

--- src/errorBoundaries.tsx
+++ src/errorBoundaries.tsx
@@ -20,7 +20,6 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 	static getDerivedStateFromError(error: Error) {
 		return { hasError: true, error };
 	}
-
 	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
 		// eslint-disable-next-line no-console
 		console.error("React Error Boundary caught an error:", error, errorInfo);
@@ -33,7 +32,11 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 				<div>
 					<h2>Something went wrong</h2>
 					<details style={{ whiteSpace: "pre-wrap" }}>
-						{this.state.error ? this.state.error.toString() : null}
+						{this.state.error
+							? typeof this.state.error.toString === "function"
+								? this.state.error.toString()
+								: JSON.stringify(this.state.error, null, 2)
+							: null}
 						<br />
 						{this.state.errorInfo?.componentStack}
 					</details>
