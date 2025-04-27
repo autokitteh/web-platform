0a8878222ba8f852cb3cf3e5523721a32e8f8329
Ronen Mars <ronen@autokitteh.com>
2025-04-27 11:15:58 +0300

    feat: limits 20

--- src/components/templates/appProvider.tsx
+++ src/components/templates/appProvider.tsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect } from "react";
 
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import { supportEmail } from "@src/constants";
 import { tours } from "@src/constants/tour.constants";
-import { EventListenerName } from "@src/enums";
 import { ModalName } from "@src/enums/components";
-import { useEventListener, useRateLimitHandler } from "@src/hooks";
 import { AppProviderProps } from "@src/interfaces/components";
 import { useModalStore, useProjectStore, useToastStore, useTourStore } from "@src/store";
 import { shouldShowStepOnPath } from "@src/utilities";
 
 import { Toast } from "@components/molecules";
 import { TourManager } from "@components/organisms";
-import { QuotaLimitModal, RateLimitModal } from "@components/organisms/modals";
 import { ContinueTourModal } from "@components/organisms/tour/continueTourModal";
 
 export const AppProvider = ({ children }: AppProviderProps) => {
@@ -26,17 +23,13 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 		setPopoverVisible,
 		tourProjectId,
 	} = useTourStore();
-	const { openModal, closeModal, closeAllModals } = useModalStore();
+	const { openModal, closeModal } = useModalStore();
 	const { projectsList } = useProjectStore();
 	const navigate = useNavigate();
 	const { addToast } = useToastStore();
 	const { t } = useTranslation("tour", { keyPrefix: "general" });
-	const { isRetrying, onRetryClick } = useRateLimitHandler();
-	const [rateLimitModalDisplayed, setRateLimitModalDisplayed] = useState(false);
-	const [quotaLimitModalDisplayed, setQuotaLimitModalDisplayed] = useState(false);
-	const location = useLocation();
 
-	const continueTour = () => {
+	const continueTour = async () => {
 		closeModal(ModalName.continueTour);
 		closeModal(ModalName.toursProgress);
 		setPopoverVisible(false);
@@ -59,61 +52,61 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 		stopTour();
 	};
 
-	const displayRateLimitModal =
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		(_: CustomEvent) => {
-			if (!rateLimitModalDisplayed) {
-				openModal(ModalName.rateLimit);
-				setRateLimitModalDisplayed(true);
-			}
-		};
+	// useEffect(() => {
+	// 	if (!activeTour.tourId || !activeStep) return;
+	// 	const currentTour = tours[activeTour.tourId];
+	// 	const configStep = currentTour.steps.find((step) => step.id === activeStep.id);
+	// 	const tourProjectExists = projectsList.find((project) => project.id === tourProjectId);
+	// 	if (configStep && !shouldShowStepOnPath(configStep, location.pathname) && !!tourProjectExists) {
+	// 		openModal(ModalName.continueTour, { name: tours?.[activeTour?.tourId]?.name });
+	// 	}
+	// }, [activeStep, activeTour.tourId, location.pathname, tourProjectId]);
 
-	const displayQuotaLimitModal = ({
-		detail: { limit, resourceName, used },
-	}: CustomEvent<{ limit: string; resourceName: string; used: string }>) => {
-		if (!quotaLimitModalDisplayed) {
-			closeAllModals();
-			openModal(ModalName.quotaLimit, { limit, resource: resourceName, used });
-			setQuotaLimitModalDisplayed(true);
-		}
-	};
+	// const displayRateLimitModal =
+	// 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	// 	(_: CustomEvent) => {
+	// 		if (!rateLimitModalDisplayed) {
+	// 			openModal(ModalName.rateLimit);
+	// 			setRateLimitModalDisplayed(true);
+	// 		}
+	// 	};
 
-	const hideRateLimitModal = () => {
-		closeModal(ModalName.rateLimit);
-		setRateLimitModalDisplayed(false);
-	};
+	// const displayQuotaLimitModal = ({
+	// 	detail: { limit, resourceName, used },
+	// }: CustomEvent<{ limit: string; resourceName: string; used: string }>) => {
+	// 	if (!quotaLimitModalDisplayed) {
+	// 		closeAllModals();
+	// 		openModal(ModalName.quotaLimit, { limit, resource: resourceName, used });
+	// 		setQuotaLimitModalDisplayed(true);
+	// 	}
+	// };
 
-	const hideQuotaLimitModal = () => {
-		closeModal(ModalName.quotaLimit);
-		setQuotaLimitModalDisplayed(false);
-	};
+	// const hideRateLimitModal = () => {
+	// 	closeModal(ModalName.rateLimit);
+	// 	setRateLimitModalDisplayed(false);
+	// };
 
-	useEventListener(EventListenerName.displayRateLimitModal, displayRateLimitModal);
-	useEventListener(EventListenerName.displayQuotaLimitModal, displayQuotaLimitModal);
-	useEventListener(EventListenerName.hideRateLimitModal, hideRateLimitModal);
-	useEventListener(EventListenerName.hideQuotaLimitModal, hideQuotaLimitModal);
+	// const hideQuotaLimitModal = () => {
+	// 	closeModal(ModalName.quotaLimit);
+	// 	setQuotaLimitModalDisplayed(false);
+	// };
 
-	useEffect(() => {
-		if (!activeTour.tourId || !activeStep) return;
-		const currentTour = tours[activeTour.tourId];
-		const configStep = currentTour.steps.find((step) => step.id === activeStep.id);
-		const tourProjectExists = projectsList.find((project) => project.id === tourProjectId);
-		if (configStep && !shouldShowStepOnPath(configStep, location.pathname) && !!tourProjectExists) {
-			openModal(ModalName.continueTour, { name: tours?.[activeTour?.tourId]?.name });
-		}
-	}, [activeStep, activeTour.tourId, location.pathname, openModal, projectsList, tourProjectId]);
+	// useEventListener(EventListenerName.displayRateLimitModal, displayRateLimitModal);
+	// useEventListener(EventListenerName.displayQuotaLimitModal, displayQuotaLimitModal);
+	// useEventListener(EventListenerName.hideRateLimitModal, hideRateLimitModal);
+	// useEventListener(EventListenerName.hideQuotaLimitModal, hideQuotaLimitModal);
 
-	const onContactSupportClick = () => {
-		try {
-			window.open(`mailto:${supportEmail}`, "_blank");
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		} catch (error) {
-			const mailtoLink = document.createElement("a");
-			mailtoLink.href = `mailto:${supportEmail}`;
-			mailtoLink.target = "_blank";
-			mailtoLink.click();
-		}
-	};
+	// const onContactSupportClick = () => {
+	// 	try {
+	// 		window.open(`mailto:${supportEmail}`, "_blank");
+	// 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	// 	} catch (error) {
+	// 		const mailtoLink = document.createElement("a");
+	// 		mailtoLink.href = `mailto:${supportEmail}`;
+	// 		mailtoLink.target = "_blank";
+	// 		mailtoLink.click();
+	// 	}
+	// };
 
 	return (
 		<>
@@ -121,8 +114,6 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 			<Toast />
 			<TourManager />
 			<ContinueTourModal onCancel={cancelTour} onContinue={continueTour} />
-			{/* <RateLimitModal isRetrying={isRetrying} onRetryClick={onRetryClick} />
-			<QuotaLimitModal onContactSupportClick={onContactSupportClick} /> */}
 		</>
 	);
 };
