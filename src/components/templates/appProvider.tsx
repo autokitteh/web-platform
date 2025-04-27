0115dc439627e4285714dda7a43a649069b1e6f3
Ronen Mars <ronen@autokitteh.com>
2025-04-27 08:44:40 +0300

    feat: limits 13

--- src/components/templates/appProvider.tsx
+++ src/components/templates/appProvider.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
-import { tours } from "@src/constants/tour.constants";
-import { ModalName } from "@src/enums/components";
+import { supportEmail, tours } from "@src/constants";
+import { EventListenerName, ModalName } from "@src/enums";
 import { AppProviderProps } from "@src/interfaces/components";
 import { useModalStore, useProjectStore, useToastStore, useTourStore } from "@src/store";
 import { shouldShowStepOnPath } from "@src/utilities";
 
+import { useEventListener, useRateLimitHandler } from "@hooks";
+
 import { Toast } from "@components/molecules";
 import { TourManager } from "@components/organisms";
-import { ContinueTourModal } from "@components/organisms/tour/continueTourModal";
+import { RateLimitModal, QuotaLimitModal } from "@components/organisms/modals";
+import { ContinueTourModal } from "@components/organisms/tour";
 
 export const AppProvider = ({ children }: AppProviderProps) => {
 	const {
@@ -22,13 +25,16 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 		setPopoverVisible,
 		tourProjectId,
 	} = useTourStore();
-	const { openModal, closeModal } = useModalStore();
+	const { openModal, closeModal, closeAllModals } = useModalStore();
 	const { projectsList } = useProjectStore();
 	const navigate = useNavigate();
 	const { addToast } = useToastStore();
 	const { t } = useTranslation("tour", { keyPrefix: "general" });
+	const { isRetrying, onRetryClick } = useRateLimitHandler();
+	const [rateLimitModalDisplayed, setRateLimitModalDisplayed] = useState(false);
+	const [quotaLimitModalDisplayed, setQuotaLimitModalDisplayed] = useState(false);
 
-	const continueTour = async () => {
+	const continueTour = useCallback(async () => {
 		closeModal(ModalName.continueTour);
 		closeModal(ModalName.toursProgress);
 		setPopoverVisible(false);
@@ -44,12 +50,51 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 		if (!activeStep) return;
 
 		navigate(lastTourStepUrl, { state: { startAbandonedTour: true } });
-	};
+	}, [closeModal, setPopoverVisible, getLastStepUrl, addToast, t, stopTour, activeStep, navigate]);
 
-	const cancelTour = () => {
+	const cancelTour = useCallback(() => {
 		closeModal(ModalName.continueTour);
 		stopTour();
-	};
+	}, [closeModal, stopTour]);
+
+	const displayRateLimitModal = useCallback(
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		(_: CustomEvent) => {
+			if (!rateLimitModalDisplayed) {
+				openModal(ModalName.rateLimit);
+				setRateLimitModalDisplayed(true);
+			}
+		},
+		[rateLimitModalDisplayed, openModal]
+	);
+
+	const displayQuotaLimitModal = useCallback(
+		({
+			detail: { limit, resourceName, used },
+		}: CustomEvent<{ limit: string; resourceName: string; used: string }>) => {
+			if (!quotaLimitModalDisplayed) {
+				closeAllModals();
+				openModal(ModalName.quotaLimit, { limit, resource: resourceName, used });
+				setQuotaLimitModalDisplayed(true);
+			}
+		},
+		[quotaLimitModalDisplayed, closeAllModals, openModal]
+	);
+
+	const hideRateLimitModal = useCallback(() => {
+		closeModal(ModalName.rateLimit);
+		setRateLimitModalDisplayed(false);
+	}, [closeModal]);
+
+	const hideQuotaLimitModal = useCallback(() => {
+		closeModal(ModalName.quotaLimit);
+		setQuotaLimitModalDisplayed(false);
+	}, [closeModal]);
+
+	useEventListener(EventListenerName.displayRateLimitModal, displayRateLimitModal);
+	useEventListener(EventListenerName.displayQuotaLimitModal, displayQuotaLimitModal);
+	useEventListener(EventListenerName.hideRateLimitModal, hideRateLimitModal);
+	useEventListener(EventListenerName.hideQuotaLimitModal, hideQuotaLimitModal);
 
 	useEffect(() => {
 		if (!activeTour.tourId || !activeStep) return;
@@ -59,7 +104,18 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 		if (configStep && !shouldShowStepOnPath(configStep, location.pathname) && !!tourProjectExists) {
 			openModal(ModalName.continueTour, { name: tours?.[activeTour?.tourId]?.name });
 		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [activeTour.tourId, activeStep, projectsList, tourProjectId, openModal]);
+
+	const onContactSupportClick = useCallback(() => {
+		try {
+			window.open(`mailto:${supportEmail}`, "_blank");
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (error) {
+			const mailtoLink = document.createElement("a");
+			mailtoLink.href = `mailto:${supportEmail}`;
+			mailtoLink.target = "_blank";
+			mailtoLink.click();
+		}
 	}, []);
 
 	return (
@@ -68,6 +124,8 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 			<Toast />
 			<TourManager />
 			<ContinueTourModal onCancel={cancelTour} onContinue={continueTour} />
+			<RateLimitModal isRetrying={isRetrying} onRetryClick={onRetryClick} />
+			<QuotaLimitModal onContactSupportClick={onContactSupportClick} />
 		</>
 	);
 };
