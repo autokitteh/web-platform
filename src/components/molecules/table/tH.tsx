aca0a400a0b5b5f49977c2253d38463a4c208c23
J1za <workinvs@gmail.com>
2025-05-02 21:12:46 +0300

    refactor(UI-905): table sort

--- src/components/molecules/table/tH.tsx
+++ src/components/molecules/table/tH.tsx
@@ -1,8 +1,11 @@
 import React, { useId, useCallback } from "react";
 
 import { flexRender } from "@tanstack/react-table";
+import { useTranslation } from "react-i18next";
 
 import { FilterVariantColumnTable, ThTanstackProps } from "@interfaces/components";
+import { SortDirectionVariant } from "@src/enums/components";
+import { SortDirection as SortDirectionType } from "@src/types/components";
 import { cn } from "@src/utilities";
 
 import { ResizeButton } from "@components/atoms";
@@ -10,6 +13,7 @@ import { SortButton } from "@components/molecules";
 import { FilterTableTanstack } from "@components/molecules/table";
 
 export const ThTanstack = <TData,>({ header, className }: ThTanstackProps<TData>) => {
+	const { t } = useTranslation("table", { keyPrefix: "tableActions" });
 	const { filterVariant } = (header.column.columnDef.meta || {}) as FilterVariantColumnTable;
 	const resizeId = useId();
 
@@ -18,11 +22,11 @@ export const ThTanstack = <TData,>({ header, className }: ThTanstackProps<TData>
 	const enableResize = header.column.getCanResize();
 	const canSort = header.column.getCanSort();
 	const isSorted = header.column.getIsSorted() as boolean;
-	const sortDirection = header.column.getIsSorted() as "asc" | "desc";
+	const sortDirection = header.column.getIsSorted() as SortDirectionType;
 
 	const handleSort = useCallback(() => {
 		if (!canSort) return;
-		header.column.toggleSorting(sortDirection === "asc");
+		header.column.toggleSorting(sortDirection === SortDirectionVariant.ASC);
 	}, [canSort, header.column, sortDirection]);
 
 	const thClassName = cn(
@@ -37,7 +41,7 @@ export const ThTanstack = <TData,>({ header, className }: ThTanstackProps<TData>
 				{flexRender(header.column.columnDef.header, header.getContext())}
 				{canSort ? (
 					<SortButton
-						ariaLabel={`Sort by ${header.column.columnDef.header}`}
+						ariaLabel={`${t("sortBy")} ${header.column.columnDef.header}`}
 						className="opacity-0 group-hover:opacity-100"
 						isActive={isSorted}
 						sortDirection={sortDirection}
