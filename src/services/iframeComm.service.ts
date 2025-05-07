d966f061b301c693d8814b936974a345452020c9
Ronen Mars <ronen@autokitteh.com>
2025-05-07 17:20:03 +0300

    feat: ai fix build

--- src/services/iframeComm.service.ts
+++ src/services/iframeComm.service.ts
@@ -10,7 +10,7 @@ import {
 	MessageTypes,
 	HandshakeMessage,
 	ErrorMessage,
-} from "@type/iframe-communication.type";
+} from "@src/types/iframeCommunication.type";
 
 // Configuration
 const CONFIG = {
