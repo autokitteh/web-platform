4ffc5885f4ff8ed6491e987a0e5c8b983f5d1354
J1za <workinvs@gmail.com>
2025-05-08 16:56:20 +0300

    feat(UI-1469): improve event filters

--- src/interfaces/components/events.interface.ts
+++ src/interfaces/components/events.interface.ts
@@ -10,8 +10,7 @@ export interface EventsDrawerContextType {
 export interface EventFiltersProps {
 	projectOptions: SelectOption[];
 	onProjectChange: (projectId: string) => void;
-	onSourceNameChange: (projectId: string, sourceId: string, integrationId: string) => void;
-	onIntegrationChange: (projectId: string, sourceId: string, integrationId: string) => void;
-	onRefresh: (projectId: string, sourceId: string, integrationId: string) => Promise<void>;
+	onIntegrationChange: (projectId: string, integrationId: string) => void;
+	onRefresh: (projectId: string, integrationId: string) => Promise<void>;
 	isLoading: boolean;
 }
