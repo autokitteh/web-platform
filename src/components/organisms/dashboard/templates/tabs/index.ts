6f544ed969b3486c9fc9d642a77369c1b600a6f3
J1za <workinvs@gmail.com>
2025-05-08 16:54:44 +0300

    feat(UI-1469): add events filters and refactor code

--- src/components/organisms/dashboard/templates/tabs/index.ts
+++ src/components/organisms/dashboard/templates/tabs/index.ts
@@ -1,6 +1,4 @@
 export { ProjectTemplateCard } from "@components/organisms/dashboard/templates/tabs/card";
 export { ProjectTemplateCreateModal } from "@components/organisms/dashboard/templates/tabs/createModal";
 export { ProjectTemplateCreateModalContainer } from "@components/organisms/dashboard/templates/tabs/createModalWrapper";
-export { MultipleLabelPopoverItem } from "@components/organisms/dashboard/templates/tabs/multipleLabelPopoverItem";
 export { IntegrationsMenuPopoverItem } from "@components/organisms/dashboard/templates/tabs/integrationsMenuPopoverItem";
-export { MultiplePopoverSelect } from "@components/organisms/dashboard/templates/tabs/multiplePopoverSelect";
