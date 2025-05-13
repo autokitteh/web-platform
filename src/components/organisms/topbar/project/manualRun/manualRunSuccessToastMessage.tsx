5ca1fae14fd237cf867f10ba70dd70f2acfede19
J1za <workinvs@gmail.com>
2025-05-13 20:53:50 +0300

    fix(UI-1394): adjust toast

--- src/components/organisms/topbar/project/manualRun/manualRunSuccessToastMessage.tsx
+++ src/components/organisms/topbar/project/manualRun/manualRunSuccessToastMessage.tsx
@@ -5,23 +5,15 @@ import { useNavigate } from "react-router-dom";
 
 import { FileArrowRightIcon } from "@assets/image/icons";
 
-export const ManualRunSuccessToastMessage = ({
-	deploymentId,
-	projectId,
-	sessionId,
-}: {
-	deploymentId?: string;
-	projectId?: string;
-	sessionId?: string;
-}) => {
+export const ManualRunSuccessToastMessage = ({ projectId, sessionId }: { projectId?: string; sessionId?: string }) => {
 	const { t } = useTranslation("deployments", { keyPrefix: "history.manualRun" });
 	const navigate = useNavigate();
 
 	return (
 		<button
 			aria-label={t("executionSucceed")}
-			className="cursor-pointer px-4 py-3"
-			onClick={() => navigate(`/projects/${projectId}/deployments/${deploymentId}/sessions/${sessionId}`)}
+			className="px-4 py-3 cursor-pointer"
+			onClick={() => navigate(`/projects/${projectId}/sessions/${sessionId}`)}
 		>
 			<div className="flex flex-col">
 				<span className="font-semibold text-green-800">{t("executionSucceed")}</span>
