afbb28a992faa9152f052edc1fccfeba05017ece
Ronen Mars <ronen@autokitteh.com>
2025-05-26 14:01:04 +0300

    feat: linter - refactor

--- src/enums/projectActions.enum.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectActionType } from "@src/types/components";
-
-export const ProjectActions: Record<ProjectActionType, ProjectActionType> = {
-	build: "build",
-	deploy: "deploy",
-	manualRun: "manualRun",
-};
