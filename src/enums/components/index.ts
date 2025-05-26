c51c6e316015b8852e2c8c1c16499de90dbca0c7
Ronen Mars <ronen@autokitteh.com>
2025-05-26 18:56:38 +0300

    chore: remove redundant enum read-only file

--- src/enums/components/index.ts
+++ src/enums/components/index.ts
@@ -15,7 +15,6 @@ export { DrawerName } from "@enums/components/drawer.enum";
 export { InputVariant } from "@enums/components/input.enum";
 export { SidebarHrefMenu, SidebarMenu } from "@enums/components/menu.enum";
 export { ModalName } from "@enums/components/modal.enum";
-export { ReadOnlyFile } from "@enums/components/readOnlyFile.enum";
 export { SortDirectionVariant } from "@enums/components/table.enum";
 export { OutputTabsVariants } from "@enums/components/tabs.enum";
 export { TriggerFormIds, TriggerFormType } from "@enums/components/trigger.enum";
