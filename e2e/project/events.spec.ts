4349d4db7cea05539d010cc6fc9a74dbe02d458d
J1za <workinvs@gmail.com>
2025-05-28 16:46:08 +0300

    test(UI-1469): add new test

---
+++ e2e/project/events.spec.ts
@@ -0,0 +1,33 @@
+import randomatic from "randomatic";
+
+import { test, expect } from "../fixtures";
+import { DashboardPage } from "e2e/pages";
+import { waitForLoadingOverlayGone } from "e2e/utils/waitForLoadingOverlayToDisappear";
+
+const projectName = `test_${randomatic("Aa", 4)}`;
+
+test.describe("Events Suite", () => {
+	test("Create new event from template: HTTP", async ({ page }) => {
+		await waitForLoadingOverlayGone(page);
+		const dashboardPage = new DashboardPage(page);
+		await dashboardPage.createProjectFromTemplate(projectName);
+
+		await page.getByRole("tab", { name: "Triggers" }).click();
+		await page.getByLabel("Modify receive_http_get_or_head").click();
+		const webhookUrl = await page.getByLabel("Webhook URL").inputValue();
+
+		const newPage = await page.context().newPage();
+		await newPage.goto(webhookUrl, {
+			waitUntil: "domcontentloaded",
+		});
+		await expect(newPage).toHaveURL(webhookUrl);
+		await newPage.close();
+
+		await page.getByLabel("Events").click();
+		await page.getByLabel("Project name").click();
+		await page.getByRole("option", { name: projectName }).click();
+		await page.locator("body").click({ position: { x: 0, y: 0 } });
+
+		await expect(page.getByRole("rowgroup").getByRole("row")).toHaveCount(2);
+	});
+});
