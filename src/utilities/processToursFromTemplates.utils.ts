8d036df258bb3eaee400b15682ff7729b7f0ef66
Ronen Mars <ronen@autokitteh.com>
2025-05-30 01:55:17 +0300

    fix(UI-1562): fix tour templates loader

--- src/utilities/processToursFromTemplates.utils.ts
+++ src/utilities/processToursFromTemplates.utils.ts
@@ -12,17 +12,18 @@ export const processToursFromTemplates = async (structure?: Record<string, any>)
 	const toursDir = "walkthroughs";
 	const storagePromises: Promise<void>[] = [];
 
-	const processDirectory = (dirStructure: Record<string, any>, currentPath: string = "") => {
+	const processDirectory = async (dirStructure: Record<string, any>, currentPath: string = "") => {
 		for (const [name, node] of Object.entries(dirStructure)) {
 			if (!node) continue;
 
 			const fullPath = currentPath ? `${currentPath}/${name}` : name;
 
 			if (!isDirectoryNode(node)) {
-				return;
+				continue;
 			}
-			if (!currentPath.startsWith(toursDir)) {
-				processDirectory(node.children, fullPath);
+
+			if (currentPath !== toursDir && !currentPath.startsWith(`${toursDir}/`)) {
+				await processDirectory(node.children, fullPath);
 				continue;
 			}
 
@@ -37,14 +38,14 @@ export const processToursFromTemplates = async (structure?: Record<string, any>)
 			});
 
 			if (Object.keys(files).length <= 0) {
-				return;
+				continue;
 			}
 
 			storagePromises.push(tourStorage.storeTourFiles(tourId, files));
 		}
 	};
 
-	processDirectory(structure);
+	await processDirectory(structure);
 	await Promise.all(storagePromises);
 
 	return tours;
