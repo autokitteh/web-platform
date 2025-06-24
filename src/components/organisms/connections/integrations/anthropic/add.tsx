bfd6c9ff5341af21184f682270968c57dc6075d7
J1za <workinvs@gmail.com>
2025-06-24 12:09:27 +0300

    feat(UI-1585): add support for anthropic integration

---
+++ src/components/organisms/connections/integrations/anthropic/add.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+import { ConnectionAuthType } from "@src/enums";
+import { Integrations } from "@src/enums/components";
+import { useConnectionForm } from "@src/hooks";
+import { anthropicIntegrationSchema } from "@validations";
+
+import { Button, ErrorMessage, Input, Spinner } from "@components/atoms";
+import { Accordion } from "@components/molecules";
+
+import { ExternalLinkIcon, FloppyDiskIcon } from "@assets/image/icons";
+
+export const AnthropicIntegrationAddForm = ({
+	connectionId,
+	triggerParentFormSubmit,
+}: {
+	connectionId?: string;
+	triggerParentFormSubmit: () => void;
+}) => {
+	const { t } = useTranslation("integrations");
+
+	const { createConnection, errors, handleSubmit, isLoading, register } = useConnectionForm(
+		anthropicIntegrationSchema,
+		"create"
+	);
+
+	useEffect(() => {
+		if (connectionId) {
+			createConnection(connectionId, ConnectionAuthType.ApiKey, Integrations.anthropic);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [connectionId]);
+
+	return (
+		<form className="flex flex-col gap-6" onSubmit={handleSubmit(triggerParentFormSubmit)}>
+			<div className="relative">
+				<Input
+					{...register("api_key")}
+					aria-label={t("anthropic.placeholders.apiKey")}
+					disabled={isLoading}
+					isError={!!errors.api_key}
+					isRequired
+					label={t("anthropic.placeholders.apiKey")}
+				/>
+
+				<ErrorMessage>{errors.api_key?.message as string}</ErrorMessage>
+			</div>
+
+			<Accordion title={t("information")}>
+				<Link
+					className="group inline-flex items-center gap-2.5 text-green-800"
+					target="_blank"
+					to="https://docs.anthropic.com/claude/docs/getting-access-to-claude"
+				>
+					{t("anthropic.information.devPlatform")}
+
+					<ExternalLinkIcon className="size-3.5 fill-green-800 duration-200" />
+				</Link>
+			</Accordion>
+
+			<Button
+				aria-label={t("buttons.saveConnection")}
+				className="ml-auto w-fit border-white px-3 font-medium text-white hover:bg-black"
+				disabled={isLoading}
+				type="submit"
+				variant="outline"
+			>
+				{isLoading ? <Spinner /> : <FloppyDiskIcon className="size-5 fill-white transition" />}
+
+				{t("buttons.saveConnection")}
+			</Button>
+		</form>
+	);
+};
