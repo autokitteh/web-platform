78166928ed2e3a732a61b910013e1d293ada2f01
Ronen Mars <ronen@autokitteh.com>
2025-07-31 14:08:04 +0300

    feat: fix chatbot state memory between projects

--- src/components/templates/projectWrapper.tsx
+++ src/components/templates/projectWrapper.tsx
@@ -31,30 +31,6 @@ export const ProjectWrapper = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [projectId]);
 
-	// Update stored mode when chatbotMode store changes for current project
-	useEffect(() => {
-		if (projectId && chatbotMode[projectId] !== undefined) {
-			const storedMode = chatbotMode[projectId];
-			const expectedConfigMode = !storedMode;
-			if (chatbotConfigMode !== expectedConfigMode) {
-				setChatbotConfigMode(expectedConfigMode);
-			}
-		}
-	}, [chatbotMode, projectId, chatbotConfigMode]);
-
-	// Save chatbot mode when it changes (but not during initial restoration)
-	useEffect(() => {
-		if (projectId && chatbotConfigMode !== undefined) {
-			// Only save if the mode actually differs from stored mode to avoid infinite loops
-			const storedMode = chatbotMode[projectId];
-			const currentMode = !chatbotConfigMode; // configMode false = AI Assistant (true), configMode true = Status Mode (false)
-
-			if (storedMode !== currentMode) {
-				setChatbotMode(projectId, currentMode);
-			}
-		}
-	}, [projectId, chatbotConfigMode, setChatbotMode, chatbotMode]);
-
 	useEventListener(EventListenerName.displayProjectAiAssistantSidebar, () => {
 		setChatbotConfigMode(false);
 		openDrawer("chatbot");
