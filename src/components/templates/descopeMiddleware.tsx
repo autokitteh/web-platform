b4e040d94798b36e5976a7ad75bdf11b0cdf6c05
Ronen Mars <ronen@autokitteh.com>
2025-08-28 14:23:04 +0300

    fix(UI-1771): display login buttons if the login failed

--- src/components/templates/descopeMiddleware.tsx
+++ src/components/templates/descopeMiddleware.tsx
@@ -63,10 +63,6 @@ export const DescopeMiddleware = ({ children }: { children: ReactNode }) => {
 
 	useEffect(() => {
 		refreshCookie();
-
-		if (playwrightTestsAuthBearer && !isLoggingIn && !user) {
-			attemptLogin();
-		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
@@ -93,6 +89,13 @@ export const DescopeMiddleware = ({ children }: { children: ReactNode }) => {
 			if (startParam) paramsToKeep.start = startParam;
 			setSearchParams(paramsToKeep, { replace: true });
 
+			attemptLogin();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	useEffect(() => {
+		if (playwrightTestsAuthBearer && !isLoggingIn && !user) {
 			attemptLogin();
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
