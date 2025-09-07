5ed596729abda2a642aa903451b46e9a807197ef
Ronen Mars <ronen@autokitteh.com>
2025-09-07 12:50:26 +0300

    fix(UI-1822): fixed broken popover

--- src/components/molecules/popover/index.ts
+++ src/components/molecules/popover/index.ts
@@ -1,5 +1,6 @@
 export { PopoverWrapper } from "@components/molecules/popover/popover";
-export { PopoverContent, PopoverTrigger } from "@components/molecules/popover/popoverContent";
+export { PopoverContent } from "@components/molecules/popover/popoverContent";
+export { PopoverTrigger } from "@components/molecules/popover/popoverTrigger";
 export { PopoverContentBase } from "@components/molecules/popover/popoverContentBase";
 export { PopoverListWrapper } from "@components/molecules/popover/popoverList";
 export { PopoverListContent } from "@components/molecules/popover/popoverListContent";
