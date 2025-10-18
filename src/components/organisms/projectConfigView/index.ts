579520d8301042c06132ca97c94121717827b8a1
Ronen Mars <ronen@autokitteh.com>
2025-10-18 17:34:57 +0300

    feat: project config view

---
+++ src/components/organisms/projectConfigView/index.ts
@@ -0,0 +1,5 @@
+export { ProjectConfigConnections } from "./projectConfigConnections";
+export { ProjectConfigVariables } from "./projectConfigVariables";
+export { ProjectConfigTriggers } from "./projectConfigTriggers";
+export { ProjectConfigView } from "./projectConfigView";
+export { ProjectConfigViewDrawer } from "./projectConfigViewDrawer";
