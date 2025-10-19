a9d899137ad66dfd74d5d089c1f9bdb5205f0477
Ronen Mars <ronen@autokitteh.com>
2025-10-19 06:57:31 +0300

    feat: project config view

--- src/components/organisms/projectConfigView/projectConfigView.tsx
+++ src/components/organisms/projectConfigView/projectConfigView.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import { DocumentationModal } from "./documentationModal";
 import { ProjectConfigConnections } from "./projectConfigConnections";
+import { ProjectConfigDeleteView } from "./projectConfigDeleteView";
 import { ProjectConfigDocumentation } from "./projectConfigDocumentation";
 import { ProjectConfigFiles } from "./projectConfigFiles";
 import { ProjectConfigTriggers } from "./projectConfigTriggers";
@@ -11,9 +12,11 @@ import { ProjectConfigVariables } from "./projectConfigVariables";
 import { ProjectConfigViewProps } from "@interfaces/components";
 
 import { ActiveIndicator } from "@components/molecules";
+import { AddFileModal } from "@components/organisms/code/addModal";
 
 export const ProjectConfigView = ({ hasActiveDeployment }: ProjectConfigViewProps) => {
 	const { t } = useTranslation("project-configuration-view");
+
 	return (
 		<>
 			<div className="mx-auto flex size-full flex-col gap-2 overflow-y-auto p-6">
@@ -31,6 +34,8 @@ export const ProjectConfigView = ({ hasActiveDeployment }: ProjectConfigViewProp
 				<ProjectConfigTriggers />
 			</div>
 			<DocumentationModal />
+			<ProjectConfigDeleteView />
+			<AddFileModal />
 		</>
 	);
 };
