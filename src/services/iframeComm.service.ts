c9dee3278c7d94ba9e5bdb15a3f6b8b2eb60cb3c
Ronen Mars <ronen@autokitteh.com>
2025-10-19 20:09:59 +0300

    feat: project config view

--- src/services/iframeComm.service.ts
+++ src/services/iframeComm.service.ts
@@ -594,6 +594,7 @@ class IframeCommService {
 	}
 
 	private getTargetOrigin(): string {
+		// Get the actual iframe origin from its src URL
 		if (this.iframeRef && this.iframeRef.src) {
 			try {
 				const iframeOrigin = new URL(this.iframeRef.src).origin;
