78738642cd2dd250674d4341b6ea3d608f886bb5
Ronen Mars <ronen@autokitteh.com>
2025-10-19 07:08:44 +0300

    feat: project config view

--- src/components/organisms/projectConfigView/documentationModal.tsx
+++ src/components/organisms/projectConfigView/documentationModal.tsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from "react";
 
-import { useTranslation } from "react-i18next";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { remarkAlert } from "remark-github-blockquote-alert";
@@ -12,7 +11,6 @@ import { MermaidDiagram } from "@components/atoms";
 import { Modal } from "@components/molecules";
 
 export const DocumentationModal = () => {
-	const { t } = useTranslation("project-configuration-view", { keyPrefix: "documentation" });
 	const { getModalData } = useModalStore();
 	const readmeContent = getModalData<string>(ModalName.documentationModal);
 
@@ -39,9 +37,7 @@ export const DocumentationModal = () => {
 	return (
 		<Modal className="h-[90vh] w-[90vw] max-w-none bg-gray-1250" name={ModalName.documentationModal}>
 			<div className="mx-6 flex h-full flex-col">
-				<h3 className="mb-5 text-xl font-bold">{t("modalTitle")}</h3>
-
-				<div className="scrollbar markdown-dark markdown-body mb-6 flex-1 overflow-y-auto bg-transparent text-sm text-white">
+				<div className="scrollbar-visible markdown-dark markdown-body mb-6 flex-1 overflow-y-auto bg-transparent text-gray-400">
 					<Markdown components={markdownComponents} remarkPlugins={[remarkGfm, remarkAlert]}>
 						{readmeContent || ""}
 					</Markdown>
