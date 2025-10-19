1b6dc738fe9c31f402ac55c0e72a7d0ef2ff2f22
Ronen Mars <ronen@autokitteh.com>
2025-10-19 06:12:04 +0300

    feat: project config view

--- src/components/organisms/projectConfigView/projectConfigView.tsx
+++ src/components/organisms/projectConfigView/projectConfigView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useTranslation } from "react-i18next";
 
+import { DocumentationModal } from "./documentationModal";
 import { ProjectConfigConnections } from "./projectConfigConnections";
 import { ProjectConfigDocumentation } from "./projectConfigDocumentation";
 import { ProjectConfigFiles } from "./projectConfigFiles";
@@ -14,19 +15,22 @@ import { ActiveIndicator } from "@components/molecules";
 export const ProjectConfigView = ({ hasActiveDeployment }: ProjectConfigViewProps) => {
 	const { t } = useTranslation("project-configuration-view");
 	return (
-		<div className="mx-auto flex size-full flex-col gap-2 overflow-y-auto p-6">
-			{hasActiveDeployment ? (
-				<div className="mb-6">
-					<ActiveIndicator indicatorText={t("activeDeployment")} />
-				</div>
-			) : null}
+		<>
+			<div className="mx-auto flex size-full flex-col gap-2 overflow-y-auto p-6">
+				{hasActiveDeployment ? (
+					<div className="mb-6">
+						<ActiveIndicator indicatorText={t("activeDeployment")} />
+					</div>
+				) : null}
 
-			<ProjectConfigDocumentation />
-			<ProjectConfigFiles />
+				<ProjectConfigDocumentation />
+				<ProjectConfigFiles />
 
-			<ProjectConfigConnections />
-			<ProjectConfigVariables />
-			<ProjectConfigTriggers />
-		</div>
+				<ProjectConfigConnections />
+				<ProjectConfigVariables />
+				<ProjectConfigTriggers />
+			</div>
+			<DocumentationModal />
+		</>
 	);
 };
