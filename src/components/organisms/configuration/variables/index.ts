8d5b733c99acec64bd3fa7445872a5253a982ae9
Ronen Mars <ronen@autokitteh.com>
2025-11-07 06:03:32 +0200

    refactor: project config

---
+++ src/components/organisms/configuration/variables/index.ts
@@ -0,0 +1,5 @@
+export { ProjectSettingsVariables } from "./projectSettingsVariables";
+export { ProjectSettingsVariableAdd } from "./projectSettingsVariableAdd";
+export { ProjectSettingsVariableEdit } from "./projectSettingsVariableEdit";
+export { ProjectSettingsVariableDelete } from "./projectSettingsVariableDelete";
+export { ProjectSettingsVariableDeleteView } from "./projectSettingsVariableDeleteView";
