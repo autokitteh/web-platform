113d45b9cba214afc0032b98703ae6b6553b1df7
Ronen Mars <ronen@autokitteh.com>
2025-11-07 06:03:32 +0200

    refactor: project config

---
+++ src/components/organisms/configuration/triggers/projectSettingsTriggerEditWrapper.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { useNavigate, useParams } from "react-router-dom";
+
+import { ProjectSettingsTriggerEdit } from "./projectSettingsTriggerEdit";
+
+export const ProjectSettingsTriggerEditWrapper = () => {
+	const navigate = useNavigate();
+	const { id } = useParams<{ id: string }>();
+
+	const handleBack = () => {
+		navigate("..");
+	};
+
+	if (!id) {
+		return null;
+	}
+
+	return <ProjectSettingsTriggerEdit onBack={handleBack} triggerId={id} />;
+};
