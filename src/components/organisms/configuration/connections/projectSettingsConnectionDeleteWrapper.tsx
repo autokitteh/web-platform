295fbd2a6b192d9009a64f61b09d841736e15857
Ronen Mars <ronen@autokitteh.com>
2025-11-07 06:03:32 +0200

    refactor: project config

---
+++ src/components/organisms/configuration/connections/projectSettingsConnectionDeleteWrapper.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { useNavigate, useParams } from "react-router-dom";
+
+import { ProjectSettingsConnectionDelete } from "./projectSettingsConnectionDelete";
+
+export const ProjectSettingsConnectionDeleteWrapper = () => {
+	const navigate = useNavigate();
+	const { id } = useParams<{ id: string }>();
+
+	const handleBack = () => {
+		navigate("..");
+	};
+
+	const handleDelete = () => {
+		navigate("..");
+	};
+
+	if (!id) {
+		return null;
+	}
+
+	return <ProjectSettingsConnectionDelete connectionId={id} onBack={handleBack} onDelete={handleDelete} />;
+};
