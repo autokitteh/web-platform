f265b736ea3f6d669a027db84d916b1efb03b310
Ronen Mars <ronen@autokitteh.com>
2025-11-10 17:54:39 +0200

    feat: project screen

---
+++ src/components/molecules/configListAddButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@components/atoms/buttons/button";
+
+import { CirclePlusIcon } from "@assets/image/icons";
+
+export const AddButton = ({
+	addButtonLabel,
+	isLoading,
+	onAdd,
+	title,
+}: {
+	addButtonLabel: string;
+	isLoading?: boolean;
+	onAdd: () => void;
+	title: string;
+}) => {
+	return (
+		<Button
+			ariaLabel={`Add ${title}`}
+			className="group flex items-center gap-2 !p-0 hover:bg-transparent hover:font-semibold"
+			disabled={isLoading}
+			onClick={onAdd}
+		>
+			<CirclePlusIcon className="size-4 stroke-green-800 stroke-[2] transition-all group-hover:stroke-[2]" />
+			<span className="text-sm text-green-800">{addButtonLabel}</span>
+		</Button>
+	);
+};
