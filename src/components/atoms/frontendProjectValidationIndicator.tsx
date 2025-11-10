ee8ff26917145e68a37a349ed30cb33d998ab8d1
Ronen Mars <ronen@autokitteh.com>
2025-11-10 18:33:07 +0200

    feat: project screen

--- src/components/atoms/frontendProjectValidationIndicator.tsx
+++ src/components/atoms/frontendProjectValidationIndicator.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 
-import { FrontendProjectValidationProps } from "@interfaces/components";
+import { FrontendProjectValidationIndicatorProps } from "@interfaces/components";
 
 import { IconSvg } from "@components/atoms";
 
 import { WarningTriangleIcon } from "@assets/image/icons";
 
-export const FrontendProjectValidationIndicator = ({ level, message }: FrontendProjectValidationProps) => {
+export const FrontendProjectValidationIndicator = ({ level, message }: FrontendProjectValidationIndicatorProps) => {
 	return (
-		<div className="flex h-6 w-4 items-center justify-center">
-			{level && message ? (
-				level === "error" ? (
-					<div className="mb-0.5 size-2 rounded-full bg-error" />
-				) : (
-					<IconSvg className="mb-1 size-3 fill-yellow-500" src={WarningTriangleIcon} />
-				)
+		<div aria-label={message} className="flex h-6 w-4 items-center justify-center" role="img" title={message}>
+			{level === "error" ? (
+				<div className="mb-0.5 size-2 rounded-full bg-error" />
 			) : (
-				<div className="mb-0.5 size-2 rounded-full bg-green-500" />
+				<IconSvg className="mb-1 size-3 fill-yellow-500" src={WarningTriangleIcon} />
 			)}
 		</div>
 	);
