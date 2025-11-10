5cd0229342ab90accd5aa7e139280186db90c169
Ronen Mars <ronen@autokitteh.com>
2025-11-10 15:38:15 +0200

    feat: project screen

--- src/components/molecules/navigationButton.tsx
+++ src/components/molecules/navigationButton.tsx
@@ -9,7 +9,9 @@ import { Button, IconSvg } from "@components/atoms";
 
 export const NavigationButton = ({
 	ariaLabel,
+	customIconClassName,
 	disabled,
+	hasActiveIndicator = false,
 	icon,
 	id,
 	isEventsButton = false,
@@ -23,17 +25,22 @@ export const NavigationButton = ({
 		"group relative size-full gap-2 whitespace-nowrap rounded-none bg-transparent p-3.5 text-gray-1500 hover:bg-gray-1050",
 		{
 			"bg-black font-semibold active text-white": isSelected,
-		}
-	);
-	const iconClassName = cn(
-		"group-hover:stroke-green-200 group-hover:text-green-200 group-active:text-green-800",
-		{
-			"text-green-200": isSelected,
 		},
 		{
-			"stroke-white group-hover:stroke-green-200 h-[1.15rem] w-[1.15rem]": isEventsButton,
+			"hover:text-white": !isSelected,
 		}
 	);
+	const iconClassName = customIconClassName
+		? customIconClassName
+		: cn(
+				"group-hover:stroke-green-200 group-hover:text-green-200 group-active:text-green-800",
+				{
+					"text-green-200": isSelected,
+				},
+				{
+					"stroke-white group-hover:stroke-green-200 h-[1.15rem] w-[1.15rem]": isEventsButton,
+				}
+			);
 	return (
 		<Button
 			ariaLabel={ariaLabel}
@@ -47,6 +54,7 @@ export const NavigationButton = ({
 			variant="filledGray"
 		>
 			<IconSvg className={iconClassName} size="lg" src={icon} />
+			{hasActiveIndicator ? <div className="absolute right-2 top-2 size-2 rounded-full bg-green-500" /> : null}
 			<span className="group-hover:text-white">{label}</span>
 			{isSelected && showUnderline ? (
 				<motion.div
--- src/components/organisms/topbar/project/navigation.tsx
+++ src/components/organisms/topbar/project/navigation.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-import { motion } from "motion/react";
 import { useLocation, useParams } from "react-router-dom";
 
 import { featureFlags, tourStepsHTMLIds } from "@src/constants";
 import { EventListenerName } from "@src/enums";
 import { triggerEvent } from "@src/hooks";
 import { useCacheStore, useHasActiveDeployments, useSharedBetweenProjectsStore } from "@src/store";
-import { cn, useNavigateWithSettings } from "@src/utilities";
+import { useNavigateWithSettings } from "@src/utilities/navigation";
 
-import { Button, IconSvg } from "@components/atoms";
+import { NavigationButton } from "@components/molecules";
 
 import { AssetsIcon, DeploymentsIcon, EventsFlag, SessionsIcon, SettingsIcon } from "@assets/image/icons";
 import MagicAiIcon from "@assets/image/icons/ai";
@@ -27,25 +26,6 @@ export const ProjectTopbarNavigation = () => {
 	const isExplorerSelected = pathname.indexOf("explorer") > -1;
 	const isSessionsSelected = pathname.indexOf("sessions") > -1;
 	const isDeploymentsSelected = pathname.indexOf("deployments") > -1 && !isSessionsSelected;
-
-	const getButtonClassName = (isSelected: boolean) =>
-		cn(
-			"group relative size-full gap-2 whitespace-nowrap rounded-none bg-transparent p-3.5 text-gray-1500 hover:bg-gray-1050",
-			{
-				"bg-black font-semibold active text-white": isSelected,
-			}
-		);
-
-	const getIconClassName = (isSelected: boolean, isEvents: boolean = false) =>
-		cn(
-			"group-hover:stroke-green-200 group-hover:text-green-200 group-active:text-green-800",
-			{
-				"text-green-200": isSelected,
-			},
-			{
-				"stroke-white group-hover:stroke-green-200 h-[1.15rem] w-[1.15rem]": isEvents,
-			}
-		);
 
 	const handleOpenAiAssistant = () => {
 		triggerEvent(EventListenerName.displayProjectAiAssistantSidebar);
@@ -68,127 +48,71 @@ export const ProjectTopbarNavigation = () => {
 
 	return (
 		<div className="ml-50 mr-auto flex items-stretch divide-x divide-gray-750 border-x border-gray-750">
-			<Button
+			<NavigationButton
 				ariaLabel="Explorer"
-				className={getButtonClassName(isExplorerSelected)}
-				key="explorer"
+				icon={AssetsIcon}
+				isSelected={isExplorerSelected}
+				keyName="explorer"
+				label="Explorer"
 				onClick={handleExplorerClick}
-				role="navigation"
-				title="Explorer"
-				variant="filledGray"
-			>
-				<IconSvg className={getIconClassName(isExplorerSelected)} size="lg" src={AssetsIcon} />
-				<span className="group-hover:text-white">Explorer</span>
+			/>
 
-				{isExplorerSelected ? (
-					<motion.div
-						className="absolute inset-x-0 -bottom-2 h-2 bg-gray-750"
-						layoutId="underline"
-						transition={{ type: "spring", stiffness: 300, damping: 30 }}
-					/>
-				) : null}
-			</Button>
-
-			<Button
+			<NavigationButton
 				ariaLabel="Deployments"
-				className={getButtonClassName(isDeploymentsSelected)}
-				key="deployments"
+				hasActiveIndicator={hasActiveDeployment}
+				icon={DeploymentsIcon}
+				isSelected={isDeploymentsSelected}
+				keyName="deployments"
+				label="Deployments"
 				onClick={() => navigateWithSettings(`/projects/${projectId!}/deployments`)}
-				role="navigation"
-				title="Deployments"
-				variant="filledGray"
-			>
-				<IconSvg className={getIconClassName(isDeploymentsSelected)} size="lg" src={DeploymentsIcon} />
-				{hasActiveDeployment ? (
-					<div className="absolute right-2 top-2 size-2 rounded-full bg-green-500" />
-				) : null}
-				<span className="group-hover:text-white">Deployments</span>
+			/>
 
-				{isDeploymentsSelected ? (
-					<motion.div
-						className="absolute inset-x-0 -bottom-2 h-2 bg-gray-750"
-						layoutId="underline"
-						transition={{ type: "spring", stiffness: 300, damping: 30 }}
-					/>
-				) : null}
-			</Button>
-
-			<Button
+			<NavigationButton
 				ariaLabel="Sessions"
-				className={getButtonClassName(isSessionsSelected)}
 				disabled={!deployments?.length}
+				icon={SessionsIcon}
 				id={tourStepsHTMLIds.sessionsTopNav}
-				key="sessions"
+				isSelected={isSessionsSelected}
+				keyName="sessions"
+				label="Sessions"
 				onClick={() => navigateWithSettings(`/projects/${projectId!}/sessions`)}
-				role="navigation"
-				title="Sessions"
-				variant="filledGray"
-			>
-				<IconSvg className={getIconClassName(isSessionsSelected)} size="lg" src={SessionsIcon} />
-				<span className="group-hover:text-white">Sessions</span>
+			/>
 
-				{isSessionsSelected ? (
-					<motion.div
-						className="absolute inset-x-0 -bottom-2 h-2 bg-gray-750"
-						layoutId="underline"
-						transition={{ type: "spring", stiffness: 300, damping: 30 }}
-					/>
-				) : null}
-			</Button>
-
-			<Button
+			<NavigationButton
 				ariaLabel="Config"
-				className="group relative size-full gap-2 whitespace-nowrap rounded-none bg-transparent p-3.5 text-gray-1500 hover:bg-gray-1050 hover:text-white"
+				customIconClassName="size-5 fill-green-200 text-green-200 transition group-hover:text-green-200 group-active:text-green-800"
 				disabled={settingsSidebarOpen}
+				icon={SettingsIcon}
 				id={tourStepsHTMLIds.projectConfig}
-				key="settings"
+				isSelected={false}
+				keyName="settings"
+				label="Config"
 				onClick={handleOpenConfigSidebar}
-				role="navigation"
-				title="Config"
-				variant="filledGray"
-			>
-				<IconSvg
-					className="size-5 fill-green-200 text-green-200 transition group-hover:text-green-200 group-active:text-green-800"
-					size="lg"
-					src={SettingsIcon}
-				/>
-				<span className="group-hover:text-white">Config</span>
-			</Button>
+				showUnderline={false}
+			/>
 
-			<Button
+			<NavigationButton
 				ariaLabel="Events"
-				className="group relative size-full gap-2 whitespace-nowrap rounded-none bg-transparent p-3.5 text-gray-1500 hover:bg-gray-1050 hover:text-white"
-				key="events"
+				icon={EventsFlag}
+				isEventsButton={true}
+				isSelected={false}
+				keyName="events"
+				label="Events"
 				onClick={handleOpenEventsSidebar}
-				role="navigation"
-				title="Events"
-				variant="filledGray"
-			>
-				<IconSvg
-					className="size-[1.15rem] fill-none stroke-white text-green-200 transition group-hover:stroke-green-200 group-hover:text-green-200 group-active:text-green-800"
-					size="lg"
-					src={EventsFlag}
-				/>
-				<span className="group-hover:text-white">Events</span>
-			</Button>
+				showUnderline={false}
+			/>
 
 			{featureFlags.displayChatbot ? (
-				<Button
+				<NavigationButton
 					ariaLabel="AI"
-					className="group relative size-full gap-2 whitespace-nowrap rounded-none bg-transparent p-3.5 text-gray-1500 hover:bg-gray-1050 hover:text-white"
-					key="chatbot"
+					customIconClassName="size-5 fill-green-200 text-green-200 transition group-hover:text-green-200 group-active:text-green-800"
+					icon={MagicAiIcon}
+					isSelected={false}
+					keyName="chatbot"
+					label="AI"
 					onClick={handleOpenAiAssistant}
-					role="navigation"
-					title="AI"
-					variant="filledGray"
-				>
-					<IconSvg
-						className="size-5 fill-green-200 text-green-200 transition group-hover:text-green-200 group-active:text-green-800"
-						size="lg"
-						src={MagicAiIcon}
-					/>
-					<span className="group-hover:text-white">AI</span>
-				</Button>
+					showUnderline={false}
+				/>
 			) : null}
 		</div>
 	);
