10c150b1b1db3ce65d75e4635bb36d4d203c4f90
Ronen Mars <ronen@autokitteh.com>
2025-11-12 11:48:45 +0200

    feat: project screen wip

--- src/components/molecules/copyButton.tsx
+++ src/components/molecules/copyButton.tsx
@@ -22,70 +22,91 @@ export const CopyButton = forwardRef<
 		buttonText?: string;
 		className?: string;
 		dataTestId?: string;
+		iconClassName?: string;
 		size?: Extract<SystemSizes, "xs" | "sm" | "md">;
 		successMessage?: string;
 		tabIndex?: number;
 		text: string;
 		title?: string;
 	}
->(({ ariaLabel, className, dataTestId, size = "xs", successMessage, tabIndex = 0, text, title, buttonText }, ref) => {
-	const { t } = useTranslation("components", { keyPrefix: "buttons" });
-	const addToast = useToastStore((state) => state.addToast);
-
-	const copyTextToClipboardRef = useRef(
-		debounce(async (text: string) => {
-			const { isError, message } = await copyToClipboard(text);
-
-			addToast({
-				message: successMessage && !isError ? successMessage : message,
-				type: isError ? "error" : "success",
-			});
-		}, 300)
-	);
-
-	const copyTextToClipboard = copyTextToClipboardRef.current;
-
-	useImperativeHandle(ref, () => ({
-		copy: () => copyTextToClipboard(text),
-	}));
-
-	const copyButtonStyle = cn(
-		"m-0 flex items-center justify-center bg-transparent p-0 hover:bg-gray-900",
+>(
+	(
 		{
-			"size-12": size === "md",
-			"size-8": size === "sm",
-			"size-6": size === "xs",
-			"gap-2": buttonText,
+			ariaLabel,
+			className,
+			dataTestId,
+			iconClassName,
+			size = "xs",
+			successMessage,
+			tabIndex = 0,
+			text,
+			title,
+			buttonText,
 		},
-		className
-	);
-	const copyButtonIconStyle = cn("fill-white", {
-		"size-5": size === "md",
-		"size-4": size === "sm",
-		"size-3": size === "xs",
-	});
+		ref
+	) => {
+		const { t } = useTranslation("components", { keyPrefix: "buttons" });
+		const addToast = useToastStore((state) => state.addToast);
 
-	const ariaLabelText = ariaLabel || t("copyButtonText", { text: ariaLabel }) || "";
-	const titleText = t("copyButtonTextTitle", { text: title }) || ariaLabelText;
-	return (
-		<Button
-			ariaLabel={ariaLabelText}
-			className={copyButtonStyle}
-			data-testid={dataTestId}
-			onClick={(event) => {
-				event.stopPropagation();
-				copyTextToClipboard(text);
-			}}
-			onKeyPressed={() => copyTextToClipboard(text)}
-			tabIndex={tabIndex}
-			title={titleText}
-			type="button"
-			valueText={text}
-		>
-			<CopyIcon className={copyButtonIconStyle} />
-			{buttonText ? <div className="text-white">{buttonText}</div> : null}
-		</Button>
-	);
-});
+		const copyTextToClipboardRef = useRef(
+			debounce(async (text: string) => {
+				const { isError, message } = await copyToClipboard(text);
+
+				addToast({
+					message: successMessage && !isError ? successMessage : message,
+					type: isError ? "error" : "success",
+				});
+			}, 300)
+		);
+
+		const copyTextToClipboard = copyTextToClipboardRef.current;
+
+		useImperativeHandle(ref, () => ({
+			copy: () => copyTextToClipboard(text),
+		}));
+
+		const copyButtonStyle = cn(
+			"m-0 flex items-center justify-center bg-transparent p-0 hover:bg-gray-900",
+			{
+				"size-12": size === "md",
+				"size-8": size === "sm",
+				"size-6": size === "xs",
+				"gap-2": buttonText,
+			},
+			className
+		);
+		const copyButtonIconStyle = cn(
+			"fill-white",
+			{
+				"size-5": size === "md",
+				"size-4": size === "sm",
+				"size-3": size === "xs",
+			},
+			iconClassName
+		);
+
+		const ariaLabelText = ariaLabel || t("copyButtonText", { text: ariaLabel }) || "";
+		const titleText = t("copyButtonTextTitle", { text: title }) || ariaLabelText;
+		return (
+			<Button
+				ariaLabel={ariaLabelText}
+				className={copyButtonStyle}
+				data-testid={dataTestId}
+				onClick={(event) => {
+					event.stopPropagation();
+					copyTextToClipboard(text);
+				}}
+				onKeyPressed={() => copyTextToClipboard(text)}
+				tabIndex={tabIndex}
+				title={titleText}
+				type="button"
+				valueText={text}
+			>
+				<CopyIcon className={copyButtonIconStyle} />
+				{buttonText ? <div className="text-white">{buttonText}</div> : null}
+			</Button>
+		);
+	}
+);
 
 CopyButton.displayName = "CopyButton";
