8d75126f3b9d5bdc085b72e30f5bba28ed0086d2
Ronen Mars <ronen@autokitteh.com>
2025-11-15 23:19:01 +0200

    feat: e2e - fix e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:8000",
+		baseURL: "http://localhost:8000?e2e=true",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on",
