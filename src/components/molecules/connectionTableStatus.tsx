e994fccce0b317db426c2eebbeef623c47a070bc
Ronen Mars <ronen@autokitteh.com>
2025-12-05 09:49:54 +0200

    feat: connection init required status

--- src/components/molecules/connectionTableStatus.tsx
+++ src/components/molecules/connectionTableStatus.tsx
@@ -13,6 +13,7 @@ export const ConnectionTableStatus = ({ status }: { status: ConnectionStatusType
 		[ConnectionStatus.ok]: "text-green-800 hover:bg-green-800/10",
 		[ConnectionStatus.unspecified]: "text-blue-500 hover:bg-blue-500/10",
 		[ConnectionStatus.warning]: "text-yellow-500 hover:bg-yellow-500/10",
+		[ConnectionStatus.init_required]: "text-orange-500 hover:bg-orange-500/10",
 	};
 	const baseClass = cn(
 		"flex w-[6.8rem] items-center justify-center rounded-md border border-gray-800 px-2 py-0.5 text-xs text-white",
