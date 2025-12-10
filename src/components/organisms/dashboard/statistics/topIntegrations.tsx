ffb312bedd15708becbf263254a469cd9902ff79
Ronen Mars <ronen@autokitteh.com>
2025-12-10 13:46:42 +0200

    feat: introduce new dashboard statistics components and integrate them into the dashboard page and layout

---
+++ src/components/organisms/dashboard/statistics/topIntegrations.tsx
@@ -0,0 +1,14 @@
+import { IntegrationUsageData } from "@utilities/fakeDashboardData";
+
+import { ChartContainer, IntegrationUsageChart } from "@components/molecules/charts";
+
+interface TopIntegrationsProps {
+	data: IntegrationUsageData[];
+	isLoading?: boolean;
+}
+
+export const TopIntegrations = ({ data, isLoading = false }: TopIntegrationsProps) => (
+	<ChartContainer isLoading={isLoading} subtitle="Most used integrations" title="Top Integrations">
+		<IntegrationUsageChart className="h-64" data={data} />
+	</ChartContainer>
+);
