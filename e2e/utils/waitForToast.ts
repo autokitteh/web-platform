fc283d0bec6956c0a4df2a3657c965ed0dfb8974
Ronen Mars <ronen@autokitteh.com>
2025-12-13 01:04:57 +0200

    test: improve waitForToast function

--- e2e/utils/waitForToast.ts
+++ e2e/utils/waitForToast.ts
@@ -1,25 +1,12 @@
 import type { Page } from "@playwright/test";
 
 export const waitForToast = async (page: Page, toastMessage: string, timeout = 10000) => {
-	const start = Date.now();
-	while (Date.now() - start < timeout) {
-		const toast = page.locator(`[role="alert"]`, { hasText: toastMessage }).last();
-		if (await toast.isVisible()) {
-			return toast;
-		}
-	}
-	throw new Error(`Toast with message "${toastMessage}" not found`);
+	const toast = page.locator(`[role="alert"]`, { hasText: toastMessage }).last();
+	await toast.waitFor({ state: "visible", timeout });
+	return toast;
 };
 
 export const waitForToastToBeRemoved = async (page: Page, toastMessage: string, timeout = 10000) => {
-	const timeToDisplayToast = timeout / 2;
-	await waitForToast(page, toastMessage, timeToDisplayToast);
-	const start = Date.now();
-	while (Date.now() - start < timeout) {
-		const toast = page.locator(`[role="alert"]`, { hasText: toastMessage }).last();
-		if (!(await toast.isVisible())) {
-			return;
-		}
-	}
-	throw new Error(`Toast with message "${toastMessage}" not removed`);
+	const toast = await waitForToast(page, toastMessage, timeout / 2);
+	await toast.waitFor({ state: "hidden", timeout });
 };
