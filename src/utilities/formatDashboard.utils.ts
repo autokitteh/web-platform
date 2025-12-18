8f7cf5121da37d602f5635c3b18fb667a30cdbbf
Ronen Mars <ronen@autokitteh.com>
2025-12-18 23:10:02 +0200

    refactor: optimize utilities

--- src/utilities/formatDashboard.utils.ts
+++ src/utilities/formatDashboard.utils.ts
@@ -1,29 +1,6 @@
-export const formatNumber = (num: number): string => {
-	if (num >= 1000000) {
-		return `${(num / 1000000).toFixed(1)}M`;
-	}
-	if (num >= 1000) {
-		return `${(num / 1000).toFixed(1)}K`;
-	}
+import prettyMs from "pretty-ms";
 
-	return num.toLocaleString();
-};
+export const formatNumber = (num: number): string =>
+	new Intl.NumberFormat("en", { notation: "compact", maximumFractionDigits: 1 }).format(num);
 
-export const formatDuration = (ms: number): string => {
-	if (ms < 1000) {
-		return `${ms}ms`;
-	}
-	if (ms < 60000) {
-		return `${(ms / 1000).toFixed(1)}s`;
-	}
-	if (ms < 3600000) {
-		const minutes = Math.floor(ms / 60000);
-		const seconds = Math.floor((ms % 60000) / 1000);
-
-		return `${minutes}m ${seconds}s`;
-	}
-	const hours = Math.floor(ms / 3600000);
-	const minutes = Math.floor((ms % 3600000) / 60000);
-
-	return `${hours}h ${minutes}m`;
-};
+export const formatDuration = (ms: number): string => prettyMs(ms);
