127cf6e4a389e630ade4901722adfebbcf7b7a86
Ronen Mars <ronen@autokitteh.com>
2025-12-23 13:24:28 +0200

    refactor: adjust e2e toast wait timeouts to be based on `closeToastDuration`

--- e2e/utils/waitForToast.ts
+++ e2e/utils/waitForToast.ts
@@ -1,12 +1,14 @@
 import type { Page } from "@playwright/test";
 
-export const waitForToast = async (page: Page, toastMessage: string, timeout = 10000) => {
+import { closeToastDuration } from "@src/constants";
+
+export const waitForToast = async (page: Page, toastMessage: string, timeout = closeToastDuration * 1.5) => {
 	const toast = page.locator(`[role="alert"]`, { hasText: toastMessage }).last();
 	await toast.waitFor({ state: "visible", timeout });
 	return toast;
 };
 
-export const waitForToastToBeRemoved = async (page: Page, toastMessage: string, timeout = 10000) => {
+export const waitForToastToBeRemoved = async (page: Page, toastMessage: string, timeout = closeToastDuration * 4) => {
 	const toast = await waitForToast(page, toastMessage, timeout);
 	await toast.waitFor({ state: "hidden", timeout });
 };
